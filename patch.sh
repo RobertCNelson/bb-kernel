9f5c5ac4d91e12aaaf6e317fcbd859bb2c537df9
Robert Nelson <robertcnelson@gmail.com>
2014-06-18 11:15:33 -0500

    sgx: drop gpio reset driver
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -98,12 +98,11 @@ saucy () {
 
 sgx () {
 	echo "dir: sgx"
-	${git} "${DIR}/patches/sgx/0001-reset-Add-driver-for-gpio-controlled-reset-pins.patch"
-	${git} "${DIR}/patches/sgx/0002-prcm-port-from-ti-linux-3.12.y.patch"
-	${git} "${DIR}/patches/sgx/0003-ARM-DTS-AM335x-Add-SGX-DT-node.patch"
-	${git} "${DIR}/patches/sgx/0004-arm-Export-cache-flush-management-symbols-when-MULTI.patch"
-	${git} "${DIR}/patches/sgx/0005-hack-port-da8xx-changes-from-ti-3.12-repo.patch"
-	${git} "${DIR}/patches/sgx/0006-Changes-according-to-TI-for-SGX-support.patch"
+	${git} "${DIR}/patches/sgx/0001-prcm-port-from-ti-linux-3.12.y.patch"
+	${git} "${DIR}/patches/sgx/0002-ARM-DTS-AM335x-Add-SGX-DT-node.patch"
+	${git} "${DIR}/patches/sgx/0003-arm-Export-cache-flush-management-symbols-when-MULTI.patch"
+	${git} "${DIR}/patches/sgx/0004-hack-port-da8xx-changes-from-ti-3.12-repo.patch"
+	${git} "${DIR}/patches/sgx/0004-Changes-according-to-TI-for-SGX-support.patch"
 }
 
 rt () {
