f18cf8188ebfe59eee85af8c816a45409451a095
Robert Nelson <robertcnelson@gmail.com>
2014-07-21 14:35:33 -0500

    config: nfs, fix argus usb
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -26,8 +26,8 @@ toolchain="gcc_linaro_gnueabihf_4_9"
 
 #Kernel/Build
 KERNEL_REL=3.16
-KERNEL_TAG=${KERNEL_REL}-rc5
-BUILD=bone0
+KERNEL_TAG=${KERNEL_REL}-rc6
+BUILD=bone0.1
 
 #v3.X-rcX + upto SHA
 #KERNEL_SHA=""
