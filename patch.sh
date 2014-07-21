fa1e8eb0feebf3a5f0ac50baacbc7ce554cbc25f
Robert Nelson <robertcnelson@gmail.com>
2014-07-21 14:54:37 -0500

    add audio cape
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -125,6 +125,7 @@ static_capes () {
 	echo "dir: static-capes"
 	${git} "${DIR}/patches/static-capes/0001-Added-Argus-UPS-cape-support.patch"
 	${git} "${DIR}/patches/static-capes/0002-Added-Argus-UPS-cape-support-BBW.patch"
+	${git} "${DIR}/patches/static-capes/0003-ARM-dts-am335x-boneblack-cape-audi.patch"
 }
 
 saucy () {
--- version.sh
+++ version.sh
@@ -27,7 +27,7 @@ toolchain="gcc_linaro_gnueabihf_4_9"
 #Kernel/Build
 KERNEL_REL=3.16
 KERNEL_TAG=${KERNEL_REL}-rc6
-BUILD=bone0.1
+BUILD=bone0.2
 
 #v3.X-rcX + upto SHA
 #KERNEL_SHA=""
