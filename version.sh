9360c37c52adaaf753e0db01e17b1deb827c5723
Robert Nelson <robertcnelson@gmail.com>
2014-08-25 18:43:25 -0500

    3.17-rc2-bone1 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -26,8 +26,8 @@ toolchain="gcc_linaro_gnueabihf_4_9"
 
 #Kernel/Build
 KERNEL_REL=3.17
-KERNEL_TAG=${KERNEL_REL}-rc1
-BUILD=bone0.8
+KERNEL_TAG=${KERNEL_REL}-rc2
+BUILD=bone1
 
 #v3.X-rcX + upto SHA
 #KERNEL_SHA=""
