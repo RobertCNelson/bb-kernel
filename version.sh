12690a27c034879e39fd6210bef9c0b83538c993
Robert Nelson <robertcnelson@gmail.com>
2015-04-29 09:47:00 -0500

    4.0.1-bone1 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -13,8 +13,8 @@ toolchain="gcc_linaro_gnueabihf_4_9"
 
 #Kernel/Build
 KERNEL_REL=4.0
-KERNEL_TAG=${KERNEL_REL}
-BUILD=bone0.4
+KERNEL_TAG=${KERNEL_REL}.1
+BUILD=bone1
 
 #v3.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
