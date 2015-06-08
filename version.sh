26020fdc8dae86f097c882b20c1a3ba919edbcbc
Robert Nelson <robertcnelson@gmail.com>
2015-06-08 09:25:33 -0500

    4.1-rc7-bone7 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -13,8 +13,8 @@ toolchain="gcc_linaro_gnueabihf_4_9"
 
 #Kernel/Build
 KERNEL_REL=4.1
-KERNEL_TAG=${KERNEL_REL}-rc6
-BUILD=bone6.6
+KERNEL_TAG=${KERNEL_REL}-rc7
+BUILD=bone7
 
 #v3.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
