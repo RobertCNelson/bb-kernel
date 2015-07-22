dea9718316939f1c605243181be86d26ac9919ef
Robert Nelson <robertcnelson@gmail.com>
2015-07-21 21:19:41 -0500

    4.1.3-bone13 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -13,8 +13,8 @@ toolchain="gcc_linaro_gnueabihf_4_9"
 
 #Kernel/Build
 KERNEL_REL=4.1
-KERNEL_TAG=${KERNEL_REL}.2
-BUILD=bone12.1
+KERNEL_TAG=${KERNEL_REL}.3
+BUILD=bone13
 
 #v3.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
