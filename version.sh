60109e2379411aca62cb85c32e58c55fba6cb58c
Robert Nelson <robertcnelson@gmail.com>
2015-10-22 18:52:33 -0500

    scripts: sync with master of: https://github.com/RobertCNelson/stable-kernel.git
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -6,16 +6,18 @@ config="omap2plus_defconfig"
 
 #toolchain="gcc_linaro_eabi_4_8"
 #toolchain="gcc_linaro_eabi_4_9"
+#toolchain="gcc_linaro_eabi_5"
 #toolchain="gcc_linaro_gnueabi_4_6"
 #toolchain="gcc_linaro_gnueabihf_4_7"
 #toolchain="gcc_linaro_gnueabihf_4_8"
 toolchain="gcc_linaro_gnueabihf_4_9"
+#toolchain="gcc_linaro_gnueabihf_5"
 
 #Kernel/Build
 KERNEL_REL=4.1
 KERNEL_TAG=${KERNEL_REL}.10
 BUILD=bone16
-kernel_rt=".7-rt8"
+kernel_rt=".10-rt10"
 
 #v4.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
