6c5757bc9e57c4e4a9d4b18c086ee22f632567e8
Robert Nelson <robertcnelson@gmail.com>
2016-01-12 10:23:33 -0600

    add edma fix for overlays
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -21,7 +21,7 @@ toolchain="gcc_linaro_gnueabihf_4_9"
 #Kernel/Build
 KERNEL_REL=4.1
 KERNEL_TAG=${KERNEL_REL}.15
-BUILD=bone-rt-r17
+BUILD=bone-rt-r17.1
 kernel_rt=".13-rt15"
 
 #v4.X-rcX + upto SHA
