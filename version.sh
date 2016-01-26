886d69a001dc950918922b8d013e3fdc24e47178
Robert Nelson <robertcnelson@gmail.com>
2016-01-26 10:48:07 -0600

    fix: CONFIG_DRM_I2C_ADIHDMI
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -24,7 +24,7 @@ toolchain="gcc_linaro_gnueabihf_5"
 #Kernel/Build
 KERNEL_REL=4.5
 KERNEL_TAG=${KERNEL_REL}-rc1
-BUILD=${build_prefix}1.1
+BUILD=${build_prefix}1.2
 kernel_rt=".X-rtY"
 
 #v4.X-rcX + upto SHA
