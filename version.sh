f7d3a62b72a60d72b15a74d21084bfa085bbd4c1
Robert Nelson <robertcnelson@gmail.com>
2016-01-31 18:35:18 -0600

    4.4.1-bone-rt-r4 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -24,8 +24,8 @@ toolchain="gcc_linaro_gnueabihf_5"
 
 #Kernel/Build
 KERNEL_REL=4.4
-KERNEL_TAG=${KERNEL_REL}
-BUILD=${build_prefix}3.2
+KERNEL_TAG=${KERNEL_REL}.1
+BUILD=${build_prefix}4
 kernel_rt="-rt3"
 
 #v4.X-rcX + upto SHA
