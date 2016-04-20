0f1974d0e18b92d012c00f994978e5791f633377
Robert Nelson <robertcnelson@gmail.com>
2016-04-20 12:25:19 -0500

    config: disable CONFIG_ARM_PSCI (breaks build on non-smp)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -25,7 +25,7 @@ toolchain="gcc_linaro_gnueabihf_4_9"
 #Kernel/Build
 KERNEL_REL=4.1
 KERNEL_TAG=${KERNEL_REL}.22
-BUILD=${build_prefix}20
+BUILD=${build_prefix}20.1
 kernel_rt=".20-rt23"
 
 #v4.X-rcX + upto SHA
