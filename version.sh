63662fc8eef501b2b8d631b2333ba954175340d5
Robert Nelson <robertcnelson@gmail.com>
2016-05-11 16:50:20 -0500

    4.4.10-bone-rt-r10 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -24,9 +24,9 @@ toolchain="gcc_linaro_gnueabihf_5"
 
 #Kernel/Build
 KERNEL_REL=4.4
-KERNEL_TAG=${KERNEL_REL}.9
+KERNEL_TAG=${KERNEL_REL}.10
 BUILD=${build_prefix}10
-kernel_rt=".7-rt16"
+kernel_rt=".9-rt17"
 
 #v4.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
