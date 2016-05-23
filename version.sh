6188fa0514e557f195362481a0d5087c3891f4e6
Robert Nelson <robertcnelson@gmail.com>
2016-05-22 20:08:20 -0500

    config: CONFIG_PREEMPT_RT_FULL, crap, this got disabled in september, no one noticed...
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -25,7 +25,7 @@ toolchain="gcc_linaro_gnueabihf_4_9"
 #Kernel/Build
 KERNEL_REL=4.1
 KERNEL_TAG=${KERNEL_REL}.24
-BUILD=${build_prefix}21
+BUILD=${build_prefix}21.1
 kernel_rt=".20-rt23"
 
 #v4.X-rcX + upto SHA
