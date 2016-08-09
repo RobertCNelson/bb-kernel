556247c69d08edb5ddaae4acc92c1330c83f36b8
Robert Nelson <robertcnelson@gmail.com>
2016-08-09 09:25:35 -0500

    4.8-rc1-bone1 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -25,7 +25,7 @@ toolchain="gcc_linaro_gnueabihf_5"
 #Kernel/Build
 KERNEL_REL=4.8
 KERNEL_TAG=${KERNEL_REL}-rc1
-BUILD=${build_prefix}0
+BUILD=${build_prefix}1
 kernel_rt=""
 
 #v4.X-rcX + upto SHA
