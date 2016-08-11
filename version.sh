db17a33f179ff28bb573042741381bab48157507
Robert Nelson <robertcnelson@gmail.com>
2016-08-11 14:51:59 -0500

    fix deb, backport iio/touchscreen
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -25,7 +25,7 @@ toolchain="gcc_linaro_gnueabihf_5"
 #Kernel/Build
 KERNEL_REL=4.6
 KERNEL_TAG=${KERNEL_REL}.6
-BUILD=${build_prefix}3
+BUILD=${build_prefix}3.1
 kernel_rt=".5-rt10"
 
 #v4.X-rcX + upto SHA
