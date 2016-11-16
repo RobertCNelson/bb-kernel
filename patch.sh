754b053e534963324af0d7507784fd1cfc717ea2
Robert Nelson <robertcnelson@gmail.com>
2016-11-16 15:26:13 -0600

    4.4.32-bone-rt-r14 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -352,7 +352,7 @@ lts44_backports () {
 	patch_backports
 	${git} "${DIR}/patches/backports/${subsystem}/0002-kernel-time-timekeeping.c-get_monotonic_coarse64.patch"
 
-	backport_tag="v4.8.7"
+	backport_tag="v4.8.8"
 
 	subsystem="touchscreen"
 	#regenerate="enable"
