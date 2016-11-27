3b506e31301b2a616c06d5d942ec85d255001dd0
Robert Nelson <robertcnelson@gmail.com>
2016-11-27 16:10:57 -0600

    4.9-rc7-bone3 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -138,7 +138,7 @@ aufs4 () {
 		${git_bin} format-patch -4 -o ../patches/aufs4/
 
 		cd ../
-		if [ ! -f ./aufs4-standalone ] ; then
+		if [ ! -d ./aufs4-standalone ] ; then
 			${git_bin} clone https://github.com/sfjro/aufs4-standalone
 			cd ./aufs4-standalone
 			${git_bin} checkout origin/aufs${KERNEL_REL} -b tmp
@@ -208,6 +208,58 @@ rt () {
 	${git} "${DIR}/patches/rt/0001-merge-CONFIG_PREEMPT_RT-Patch-Set.patch"
 }
 
+tinydrm () {
+	echo "dir: tinydrm"
+	#regenerate="enable"
+	if [ "x${regenerate}" = "xenable" ] ; then
+		cd ../
+		if [ ! -d ./tinydrm ] ; then
+			${git_bin} clone https://github.com/notro/tinydrm
+			cd ./tinydrm
+			${git_bin} checkout origin/master -b tmp
+			cd ../
+		else
+			rm -rf ./tinydrm || true
+			${git_bin} clone https://github.com/notro/tinydrm
+			cd ./tinydrm
+			${git_bin} checkout origin/master -b tmp
+			cd ../
+		fi
+		cd ./KERNEL/
+
+		mkdir -p ./drivers/gpu/drm/tinydrm/core/
+		cp -v ../tinydrm/Kconfig ./drivers/gpu/drm/tinydrm/
+		cp -v ../tinydrm/Makefile ./drivers/gpu/drm/tinydrm/
+		cp -rv ../tinydrm/core/* ./drivers/gpu/drm/tinydrm/core/
+		cp -rv ../tinydrm/*.c ./drivers/gpu/drm/tinydrm/
+		mkdir -p ./include/drm/tinydrm
+		cp -v ../tinydrm/include/drm/tinydrm/*.h ./include/drm/tinydrm
+		cp -v ../tinydrm/include/uapi/drm/*.h ./include/uapi/drm
+
+		echo "obj-\$(CONFIG_DRM_TINYDRM)+= tinydrm/" >> ./drivers/gpu/drm/Makefile
+		echo "source \"drivers/gpu/drm/tinydrm/Kconfig\"" >> ./drivers/gpu/drm/Kconfig
+
+		${git_bin} add .
+		${git_bin} commit -a -m 'merge: tinydrm' -s
+		${git_bin} format-patch -1 -o ../patches/drivers/tinydrm/
+
+		exit 2
+	fi
+
+	#regenerate="enable"
+	if [ "x${regenerate}" = "xenable" ] ; then
+		start_cleanup
+	fi
+
+	${git} "${DIR}/patches/drivers/tinydrm/0001-merge-tinydrm.patch"
+
+	if [ "x${regenerate}" = "xenable" ] ; then
+		wdir="drivers/tinydrm"
+		number=1
+		cleanup
+	fi
+}
+
 local_patch () {
 	echo "dir: dir"
 	${git} "${DIR}/patches/dir/0001-patch.patch"
@@ -216,6 +268,7 @@ local_patch () {
 #external_git
 #aufs4
 #rt
+tinydrm
 #local_patch
 
 pre_backports () {
@@ -293,15 +346,13 @@ reverts () {
 	${git} "${DIR}/patches/reverts/0006-Revert-wlcore-sdio-Populate-config-firmware-data.patch"
 	${git} "${DIR}/patches/reverts/0007-Revert-wlcore-Prepare-family-to-fix-nvs-file-handlin.patch"
 
+	${git} "${DIR}/patches/reverts/0008-Revert-Fix-subtle-CONFIG_MODVERSIONS-problems.patch"
+
 	if [ "x${regenerate}" = "xenable" ] ; then
 		wdir="reverts"
-		number=7
+		number=8
 		cleanup
 	fi
-}
-
-build () {
-	dir 'build/kbuild'
 }
 
 drivers () {
@@ -592,7 +643,6 @@ beaglebone () {
 ###
 #backports
 reverts
-build
 drivers
 soc
 beaglebone
--- version.sh
+++ version.sh
@@ -27,7 +27,7 @@ toolchain="gcc_linaro_gnueabihf_6"
 
 #Kernel/Build
 KERNEL_REL=4.9
-KERNEL_TAG=${KERNEL_REL}-rc6
+KERNEL_TAG=${KERNEL_REL}-rc7
 BUILD=${build_prefix}3
 kernel_rt=".X-rtY"
 
