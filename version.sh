b47df976d18266e5076a41540ea78ad15476c15d
Robert Nelson <robertcnelson@gmail.com>
2017-03-12 18:45:22 -0500

    4.4.53-bone-rt-r16 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -27,9 +27,9 @@ toolchain="gcc_linaro_gnueabihf_5"
 
 #Kernel/Build
 KERNEL_REL=4.4
-KERNEL_TAG=${KERNEL_REL}.52
+KERNEL_TAG=${KERNEL_REL}.53
 BUILD=${build_prefix}16
-kernel_rt=".50-rt62"
+kernel_rt=".50-rt63"
 
 #v4.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
