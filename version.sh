21d7183a753937e08b53942d034522e437c47a09
Robert Nelson <robertcnelson@gmail.com>
2017-05-14 09:36:37 -0500

    4.4.68-bone17 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -27,9 +27,9 @@ toolchain="gcc_linaro_gnueabihf_5"
 
 #Kernel/Build
 KERNEL_REL=4.4
-KERNEL_TAG=${KERNEL_REL}.67
+KERNEL_TAG=${KERNEL_REL}.68
 BUILD=${build_prefix}17
-kernel_rt=".60-rt73"
+kernel_rt=".66-rt79"
 
 #v4.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
