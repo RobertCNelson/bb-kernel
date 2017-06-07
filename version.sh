a868d3b071991ad49bc852001669d01d79f74f0a
Robert Nelson <robertcnelson@gmail.com>
2017-06-07 09:41:55 -0500

    4.9.31-bone-rt-r4 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -14,22 +14,25 @@ KERNEL_ARCH=arm
 #toolchain="gcc_linaro_eabi_4_9"
 #toolchain="gcc_linaro_eabi_5"
 #toolchain="gcc_linaro_eabi_6"
+#toolchain="gcc_linaro_eabi_7"
 #toolchain="gcc_linaro_gnueabi_4_6"
 #toolchain="gcc_linaro_gnueabihf_4_7"
 #toolchain="gcc_linaro_gnueabihf_4_8"
 #toolchain="gcc_linaro_gnueabihf_4_9"
 #toolchain="gcc_linaro_gnueabihf_5"
 toolchain="gcc_linaro_gnueabihf_6"
+#toolchain="gcc_linaro_gnueabihf_7"
 #arm64
 #KERNEL_ARCH=arm64
 #toolchain="gcc_linaro_aarch64_gnu_5"
 #toolchain="gcc_linaro_aarch64_gnu_6"
+#toolchain="gcc_linaro_aarch64_gnu_7"
 
 #Kernel/Build
 KERNEL_REL=4.9
-KERNEL_TAG=${KERNEL_REL}.30
+KERNEL_TAG=${KERNEL_REL}.31
 BUILD=${build_prefix}4
-kernel_rt=".27-rt18"
+kernel_rt=".30-rt20"
 
 #v4.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
