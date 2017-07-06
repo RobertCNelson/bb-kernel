fc11d19a95789ad31e95e90fdcb198c10962e039
Robert Nelson <robertcnelson@gmail.com>
2017-07-06 11:33:01 -0500

    uio: import Matthijs fixes
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -31,7 +31,7 @@ toolchain="gcc_linaro_gnueabihf_6"
 #Kernel/Build
 KERNEL_REL=4.12
 KERNEL_TAG=${KERNEL_REL}
-BUILD=${build_prefix}1.1
+BUILD=${build_prefix}1.2
 kernel_rt=".X-rtY"
 
 #v4.X-rcX + upto SHA
