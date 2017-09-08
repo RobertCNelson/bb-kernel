4c71bd1935cd25b4e9410b7de4668ac3e416f5f6
Robert Nelson <robertcnelson@gmail.com>
2017-09-08 15:28:38 -0500

    4.4.87-bone19 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -28,11 +28,12 @@ toolchain="gcc_linaro_gnueabihf_5"
 #toolchain="gcc_linaro_aarch64_gnu_6"
 #toolchain="gcc_linaro_aarch64_gnu_7"
 
-#Kernel/Build
+#Kernel
 KERNEL_REL=4.4
-KERNEL_TAG=${KERNEL_REL}.86
+KERNEL_TAG=${KERNEL_REL}.87
+kernel_rt=".86-rt99"
+#Kernel Build
 BUILD=${build_prefix}19
-kernel_rt=".79-rt92"
 
 #v4.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
