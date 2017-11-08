9b1b5ed2de0b9d4f4a8e8baf16d132bc42dfe65c
Robert Nelson <robertcnelson@gmail.com>
2017-11-08 10:52:06 -0600

    4.9.61-bone-rt-r8 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -193,6 +193,7 @@ rt_cleanup () {
 rt () {
 	echo "dir: rt"
 
+	${git_bin} revert --no-edit df0c2d409e9d3159724b206da7e7e697fb5874eb
 	${git_bin} revert --no-edit 120ec1e4cdddfc16c31581c2c30511c6c16fe0fd
 
 	rt_patch="${KERNEL_REL}${kernel_rt}"
