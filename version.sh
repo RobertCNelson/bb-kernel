8b10e56b29c51f179bc55316325eb23a25ece990
Robert Nelson <robertcnelson@gmail.com>
2017-11-08 09:46:49 -0600

    4.4.97-bone-rt-r20 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -30,7 +30,7 @@ toolchain="gcc_linaro_gnueabihf_5"
 
 #Kernel
 KERNEL_REL=4.4
-KERNEL_TAG=${KERNEL_REL}.96
+KERNEL_TAG=${KERNEL_REL}.97
 kernel_rt=".86-rt99"
 #Kernel Build
 BUILD=${build_prefix}20
