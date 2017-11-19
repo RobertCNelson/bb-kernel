790a00c4e98d66cdba8d109cf5fbfe2f5bb12da3
Robert Nelson <robertcnelson@gmail.com>
2017-11-18 22:20:57 -0600

    4.4.99-bone20 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -30,7 +30,7 @@ toolchain="gcc_linaro_gnueabihf_5"
 
 #Kernel
 KERNEL_REL=4.4
-KERNEL_TAG=${KERNEL_REL}.98
+KERNEL_TAG=${KERNEL_REL}.99
 kernel_rt=".86-rt99"
 #Kernel Build
 BUILD=${build_prefix}20
