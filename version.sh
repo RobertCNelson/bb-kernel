dfae14f25ccd545d2695f9b421fea6cc156c6c8c
Robert Nelson <robertcnelson@gmail.com>
2018-01-17 10:41:04 -0600

    4.4.112-bone21 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -30,7 +30,7 @@ toolchain="gcc_linaro_gnueabihf_5"
 
 #Kernel
 KERNEL_REL=4.4
-KERNEL_TAG=${KERNEL_REL}.111
+KERNEL_TAG=${KERNEL_REL}.112
 kernel_rt=".110-rt125"
 #Kernel Build
 BUILD=${build_prefix}21
