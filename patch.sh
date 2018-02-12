b20348eac08f04c8dc2344c08c6a51fabc0544fa
Robert Nelson <robertcnelson@gmail.com>
2018-02-12 13:35:51 -0600

    4.16-rc1-bone0 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -379,7 +379,7 @@ reverts () {
 drivers () {
 	dir 'drivers/ar1021_i2c'
 	dir 'drivers/btrfs'
-	dir 'drivers/pwm'
+#	dir 'drivers/pwm'
 	dir 'drivers/spi'
 	dir 'drivers/ssd1306'
 	dir 'drivers/tsl2550'
@@ -451,7 +451,7 @@ soc () {
 	dir 'soc/ti'
 	dir 'soc/ti/bone_common'
 	dir 'soc/ti/uboot'
-	dir 'soc/ti/blue'
+#	dir 'soc/ti/blue'
 	dir 'soc/ti/sancloud'
 	dir 'soc/ti/abbbi'
 	dir 'soc/ti/am335x_olimex_som'
