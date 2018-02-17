f2db77d0fd593cf16858493307de05d5b989cdf0
Robert Nelson <robertcnelson@gmail.com>
2018-02-16 20:50:27 -0600

    4.4.116-bone-rt-r21 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -198,7 +198,7 @@ rt () {
 	echo "dir: rt"
 	rt_patch="${KERNEL_REL}${kernel_rt}"
 
-	${git_bin} revert --no-edit 360496cab53af2f1dd77dbe353b7b665bcfdc1e3
+	${git_bin} revert --no-edit 77f56f5d39e95a152a755d53b0d870b92fbf466b
 
 	#un-matched kernel
 	#regenerate="enable"
