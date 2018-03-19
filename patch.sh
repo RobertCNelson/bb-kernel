35be611671f7bc16a4ea388f6245a31b770b6199
Robert Nelson <robertcnelson@gmail.com>
2018-03-19 13:20:01 -0500

    4.14.28-bone-rt-r14 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -191,7 +191,7 @@ rt () {
 	echo "dir: rt"
 	rt_patch="${KERNEL_REL}${kernel_rt}"
 
-	#${git_bin} revert --no-edit xyz
+	${git_bin} revert --no-edit a80de288d6c5188f734e13302ca83141cd0211d6
 
 	#un-matched kernel
 	#regenerate="enable"
