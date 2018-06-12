04796d523a9306d7be70db0115d81b638724f82d
Robert Nelson <robertcnelson@gmail.com>
2018-06-11 19:27:32 -0500

    4.16.15-bone11 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -30,7 +30,7 @@ toolchain="gcc_linaro_gnueabihf_7"
 
 #Kernel
 KERNEL_REL=4.16
-KERNEL_TAG=${KERNEL_REL}.14
+KERNEL_TAG=${KERNEL_REL}.15
 kernel_rt=".12-rt5"
 #Kernel Build
 BUILD=${build_prefix}11
