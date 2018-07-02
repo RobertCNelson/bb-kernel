c26d25cb7074141865e1e679fc416cd9d72afc3a
Robert Nelson <robertcnelson@gmail.com>
2018-07-02 16:02:05 -0500

    4.14.52-bone-rt-r16 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -191,21 +191,8 @@ rt () {
 	echo "dir: rt"
 	rt_patch="${KERNEL_REL}${kernel_rt}"
 
-	#v4.14.51
-	${git_bin} revert --no-edit e7a65e899d521eba676a1e652f9bf48d67722a18
-	${git_bin} revert --no-edit 0742396317a0a0e41ab47e9de10cd6f16317f049
-
-	#v4.14.48
-	${git_bin} revert --no-edit c6a95f37d3a0539424b66c2e31b657b10aaa2b53
-	${git_bin} revert --no-edit 8441a0014a29e4230d492947e5599415f219d9a9
-
-	#v4.14.45
-	${git_bin} revert --no-edit 2565b271aed0199f5ff977429486e3b59e68e708
-	${git_bin} revert --no-edit 3623c1f37efaf6ab2eed458a2ad11e49ebc2e08b
-	${git_bin} revert --no-edit 1366b31d18297ff1879f0becf941425503408447
-
-	#v4.14.42
-	${git_bin} revert --no-edit ece94a76618e1df547ea99544fb69de5a5079633
+	#v4.14.x
+	#${git_bin} revert --no-edit xyz
 
 	#regenerate="enable"
 	if [ "x${regenerate}" = "xenable" ] ; then
