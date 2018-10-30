98b74b481d14dbb9115170f1db5b36db36fc9ef9
Robert Nelson <robertcnelson@gmail.com>
2018-10-30 13:00:15 -0500

    4.19-bone4 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -32,9 +32,9 @@ toolchain="gcc_arm_gnueabihf_8"
 #Kernel
 KERNEL_REL=4.19
 KERNEL_TAG=${KERNEL_REL}
-#kernel_rt="-rc8-rt1"
+kernel_rt="-rt1"
 #Kernel Build
-BUILD=${build_prefix}3
+BUILD=${build_prefix}4
 
 #v4.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
