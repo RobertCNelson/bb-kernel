02513cd34ccdf5c5b64494981ee40e6f0ec32906
Robert Nelson <robertcnelson@gmail.com>
2018-11-24 15:55:12 -0600

    4.19.4-bone-rt-r7 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -31,10 +31,10 @@ toolchain="gcc_arm_gnueabihf_8"
 
 #Kernel
 KERNEL_REL=4.19
-KERNEL_TAG=${KERNEL_REL}.2
+KERNEL_TAG=${KERNEL_REL}.4
 kernel_rt=".1-rt3"
 #Kernel Build
-BUILD=${build_prefix}6
+BUILD=${build_prefix}7
 
 #v4.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
