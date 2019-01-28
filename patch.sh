93d0cdbb9162858ef83f24a075fc9e0c3c3f05f9
Robert Nelson <robertcnelson@gmail.com>
2019-01-28 13:07:14 -0600

    staging: rtl8188eu: Add device code for D-Link DWA-121 rev B1
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -372,6 +372,7 @@ drivers () {
 	dir 'drivers/ar1021_i2c'
 	dir 'drivers/btrfs'
 	dir 'drivers/pwm'
+	dir 'drivers/rtl8188eu'
 	dir 'drivers/spi'
 	dir 'drivers/ssd1306'
 	dir 'drivers/tps65217'
--- version.sh
+++ version.sh
@@ -35,7 +35,7 @@ KERNEL_REL=4.20
 KERNEL_TAG=${KERNEL_REL}.4
 kernel_rt=".X-rtY"
 #Kernel Build
-BUILD=${build_prefix}4
+BUILD=${build_prefix}4.1
 
 #v4.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
