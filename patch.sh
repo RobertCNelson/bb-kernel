2aad67fac899757f2dae28b1dba329bd3fcbe4ca
Robert Nelson <robertcnelson@gmail.com>
2019-02-05 14:53:38 -0600

    4.19.19-bone21 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -112,6 +112,7 @@ aufs4 () {
 	echo "dir: aufs4"
 	#regenerate="enable"
 	if [ "x${regenerate}" = "xenable" ] ; then
+		KERNEL_REL=4.19.17+
 		wget https://raw.githubusercontent.com/sfjro/aufs4-standalone/aufs${KERNEL_REL}/aufs4-kbuild.patch
 		patch -p1 < aufs4-kbuild.patch || aufs_fail
 		rm -rf aufs4-kbuild.patch
@@ -146,6 +147,7 @@ aufs4 () {
 			${git_bin} clone -b aufs${KERNEL_REL} https://github.com/sfjro/aufs4-standalone --depth=1
 		fi
 		cd ./KERNEL/
+		KERNEL_REL=4.19
 
 		cp -v ../aufs4-standalone/Documentation/ABI/testing/*aufs ./Documentation/ABI/testing/
 		mkdir -p ./Documentation/filesystems/aufs/
@@ -333,7 +335,7 @@ patch_backports (){
 }
 
 backports () {
-	backport_tag="v4.20.4"
+	backport_tag="v4.20.6"
 
 	subsystem="remoteproc"
 	#regenerate="enable"
@@ -373,7 +375,6 @@ drivers () {
 	dir 'drivers/ar1021_i2c'
 	dir 'drivers/btrfs'
 	dir 'drivers/pwm'
-	dir 'drivers/rtl8188eu'
 	dir 'drivers/spi'
 	dir 'drivers/ssd1306'
 	dir 'drivers/tps65217'
