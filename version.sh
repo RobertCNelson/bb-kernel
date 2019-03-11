9169a2cd951b10c373a7bfb1201ea1833d901569
Robert Nelson <robertcnelson@gmail.com>
2019-03-11 16:19:12 -0500

    4.19.28-bone-rt-r28 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -32,10 +32,10 @@ toolchain="gcc_arm_gnueabihf_8"
 
 #Kernel
 KERNEL_REL=4.19
-KERNEL_TAG=${KERNEL_REL}.27
+KERNEL_TAG=${KERNEL_REL}.28
 kernel_rt=".25-rt16"
 #Kernel Build
-BUILD=${build_prefix}27
+BUILD=${build_prefix}28
 
 #v4.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
