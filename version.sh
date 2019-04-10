6edff016fcab307fac2c89eb211396b9bb912d7e
Robert Nelson <robertcnelson@gmail.com>
2019-04-10 16:30:24 -0500

    5.0.7-bone-rt-r9 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -32,10 +32,10 @@ toolchain="gcc_arm_gnueabihf_8"
 
 #Kernel
 KERNEL_REL=5.0
-KERNEL_TAG=${KERNEL_REL}.5
-kernel_rt=".5-rt3"
+KERNEL_TAG=${KERNEL_REL}.7
+kernel_rt=".7-rt4"
 #Kernel Build
-BUILD=${build_prefix}8
+BUILD=${build_prefix}9
 
 #v5.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
