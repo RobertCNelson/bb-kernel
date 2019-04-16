7cac39827b23e929feeb17cd0fc420e7d51ce205
Robert Nelson <robertcnelson@gmail.com>
2019-04-16 09:42:33 -0500

    4.14.109-bone-rt-r21 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -32,10 +32,10 @@ toolchain="gcc_linaro_gnueabihf_7"
 
 #Kernel
 KERNEL_REL=4.14
-KERNEL_TAG=${KERNEL_REL}.106
-kernel_rt=".106-rt56"
+KERNEL_TAG=${KERNEL_REL}.109
+kernel_rt=".109-rt58"
 #Kernel Build
-BUILD=${build_prefix}20
+BUILD=${build_prefix}21
 
 #v5.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
