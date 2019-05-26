66212bec4204a868d3f4cb181a75cb7f148dda10
Robert Nelson <robertcnelson@gmail.com>
2019-05-26 08:42:06 -0500

    5.1.5-bone5 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -32,10 +32,10 @@ toolchain="gcc_arm_gnueabihf_8"
 
 #Kernel
 KERNEL_REL=5.1
-KERNEL_TAG=${KERNEL_REL}.4
+KERNEL_TAG=${KERNEL_REL}.5
 kernel_rt=".X-rtY"
 #Kernel Build
-BUILD=${build_prefix}4
+BUILD=${build_prefix}5
 
 #v5.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
