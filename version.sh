b2d121e7e20ff1cc7f52398ca30d549051c9989e
Robert Nelson <robertcnelson@gmail.com>
2019-06-10 13:38:28 -0500

    5.1.8-bone7 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -32,10 +32,10 @@ toolchain="gcc_arm_gnueabihf_8"
 
 #Kernel
 KERNEL_REL=5.1
-KERNEL_TAG=${KERNEL_REL}.6
+KERNEL_TAG=${KERNEL_REL}.8
 kernel_rt=".X-rtY"
 #Kernel Build
-BUILD=${build_prefix}6
+BUILD=${build_prefix}7
 
 #v5.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
