6bb635ff148956cd85dda04a7c8e95d029316255
Robert Nelson <robertcnelson@gmail.com>
2019-07-14 18:33:44 -0500

    5.1.18-bone14 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -32,10 +32,10 @@ toolchain="gcc_arm_gnueabihf_8"
 
 #Kernel
 KERNEL_REL=5.1
-KERNEL_TAG=${KERNEL_REL}.17
+KERNEL_TAG=${KERNEL_REL}.18
 kernel_rt=".X-rtY"
 #Kernel Build
-BUILD=${build_prefix}13
+BUILD=${build_prefix}14
 
 #v5.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
