811857db3549bb441380adf354d6d88ccb80839d
Robert Nelson <robertcnelson@gmail.com>
2019-07-29 12:58:12 -0500

    add: can-isotp: https://github.com/hartkopp/can-isotp
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -35,7 +35,7 @@ KERNEL_REL=4.14
 KERNEL_TAG=${KERNEL_REL}.134
 kernel_rt=".134-rt63"
 #Kernel Build
-BUILD=${build_prefix}24.1
+BUILD=${build_prefix}24.2
 
 #v5.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
