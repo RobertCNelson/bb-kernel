39e23d4941611e3cede307fff355b69ee06c0fbc
Robert Nelson <robertcnelson@gmail.com>
2019-08-01 21:24:16 -0500

    kernel v5.2.5 rebase with rt: v5.2-rt1 aufs/wireguard/etc
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -345,7 +345,7 @@ local_patch () {
 }
 
 #external_git
-#aufs
+aufs
 #rt
 wireguard
 ti_pm_firmware
@@ -452,6 +452,7 @@ soc () {
 #reverts
 drivers
 soc
+dir 'fixes'
 
 packaging () {
 	echo "dir: packaging"
