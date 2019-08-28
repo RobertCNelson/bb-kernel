ae75252ed43c6b2aea32fdf12d2c93720bfdc0f7
Robert Nelson <robertcnelson@gmail.com>
2019-08-28 16:37:44 -0500

    sync: greybus, exfat
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -32,10 +32,10 @@ toolchain="gcc_linaro_gnueabihf_7"
 
 #Kernel
 KERNEL_REL=4.14
-KERNEL_TAG=${KERNEL_REL}.134
-kernel_rt=".134-rt63"
+KERNEL_TAG=${KERNEL_REL}.139
+kernel_rt=".139-rt66"
 #Kernel Build
-BUILD=${build_prefix}25
+BUILD=${build_prefix}25.1
 
 #v5.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
