fcf082078e3c778fccf9e7b9adf048c6ce013d32
Robert Nelson <robertcnelson@gmail.com>
2019-08-29 22:00:41 -0500

    kernel v5.2.11 rebase with rt: v5.2.10-rt5 aufs/wireguard/etc
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -32,10 +32,10 @@ toolchain="gcc_arm_gnueabihf_8"
 
 #Kernel
 KERNEL_REL=5.2
-KERNEL_TAG=${KERNEL_REL}.10
+KERNEL_TAG=${KERNEL_REL}.11
 kernel_rt=".10-rt5"
 #Kernel Build
-BUILD=${build_prefix}7
+BUILD=${build_prefix}7.1
 
 #v5.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
