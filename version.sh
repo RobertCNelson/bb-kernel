f13dae363431bfd927865825fd821cbadade8ebf
Robert Nelson <robertcnelson@gmail.com>
2019-08-29 23:16:36 -0500

    kernel v4.14.141 rebase with rt: v4.14.139-rt66 aufs/wireguard/etc
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -32,10 +32,10 @@ toolchain="gcc_linaro_gnueabihf_7"
 
 #Kernel
 KERNEL_REL=4.14
-KERNEL_TAG=${KERNEL_REL}.139
+KERNEL_TAG=${KERNEL_REL}.141
 kernel_rt=".139-rt66"
 #Kernel Build
-BUILD=${build_prefix}26
+BUILD=${build_prefix}26.1
 
 #v5.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
