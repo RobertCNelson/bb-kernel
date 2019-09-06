684989e2fbde37d7d208d3218409718eaf182d0a
Robert Nelson <robertcnelson@gmail.com>
2019-09-06 15:18:19 -0500

    kernel v4.19.71 rebase with rt: v4.19.59-rt24 aufs/wireguard/etc
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -32,10 +32,10 @@ toolchain="gcc_arm_gnueabihf_8"
 
 #Kernel
 KERNEL_REL=4.19
-KERNEL_TAG=${KERNEL_REL}.69
+KERNEL_TAG=${KERNEL_REL}.71
 kernel_rt=".59-rt24"
 #Kernel Build
-BUILD=${build_prefix}37
+BUILD=${build_prefix}37.1
 
 #v5.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
