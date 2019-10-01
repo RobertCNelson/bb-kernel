532a51e11f70963e8169aebce8fbdc79d23c0b79
Robert Nelson <robertcnelson@gmail.com>
2019-10-01 10:33:06 -0500

    kernel v5.3.2 rebase with rt: v5.3.X-rtY aufs/wireguard/etc
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -32,10 +32,10 @@ toolchain="gcc_arm_gnueabihf_8"
 
 #Kernel
 KERNEL_REL=5.3
-KERNEL_TAG=${KERNEL_REL}.1
+KERNEL_TAG=${KERNEL_REL}.2
 kernel_rt=".X-rtY"
 #Kernel Build
-BUILD=${build_prefix}9
+BUILD=${build_prefix}9.1
 
 #v5.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
