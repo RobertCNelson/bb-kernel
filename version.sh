012852e8fba2480c3f19a45d9d77fea8d307f8ca
Robert Nelson <robertcnelson@gmail.com>
2020-01-14 16:45:48 -0600

    kernel v5.5-rc6 rebase with rt: v5.5.X-rtY aufs/wireguard/etc BBDTBS: https://github.com/beagleboard/BeagleBoard-DeviceTrees/commit/7b27fa274d2143f7f9a5ea7b50b883d3a2838c1b CAN-ISOTP: https://github.com/hartkopp/can-isotp/commit/b31bce98d65f894aad6427bcf6f3f7822e261a59 TI_AMX3_CM3: http://git.ti.com/gitweb/?p=processor-firmware/ti-amx3-cm3-pm-firmware.git;a=commit;h=6a849767df85ce9399494f53fb5c753665396653 WIREGUARD: https://git.zx2c4.com/WireGuard/commit/edad0d6e99e5133b1e8e865d727a25fff6399cb4
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -23,19 +23,21 @@ KERNEL_ARCH=arm
 #toolchain="gcc_linaro_gnueabihf_6"
 #toolchain="gcc_linaro_gnueabihf_7"
 toolchain="gcc_arm_gnueabihf_8"
+#toolchain="gcc_arm_gnueabihf_9"
 #arm64
 #KERNEL_ARCH=arm64
 #toolchain="gcc_linaro_aarch64_gnu_5"
 #toolchain="gcc_linaro_aarch64_gnu_6"
 #toolchain="gcc_linaro_aarch64_gnu_7"
 #toolchain="gcc_arm_aarch64_gnu_8"
+#toolchain="gcc_arm_aarch64_gnu_9"
 
 #Kernel
 KERNEL_REL=5.5
-KERNEL_TAG=${KERNEL_REL}-rc4
+KERNEL_TAG=${KERNEL_REL}-rc6
 kernel_rt=".X-rtY"
 #Kernel Build
-BUILD=${build_prefix}3
+BUILD=${build_prefix}3.1
 
 #v5.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
