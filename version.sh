e8ea9749b7bf42fe07e9b71fb832946c07428bf3
Robert Nelson <robertcnelson@gmail.com>
2020-01-16 15:29:38 -0600

    kernel v4.19.94 rebase with rt: v4.19.94-rt38 wireguard/can_isotp/device-tree/etc AUFS: https://github.com/sfjro/aufs4-standalone/commit/6c582cc629cbfb4fac5bfc7d20db128c7d201da6 BBDTBS: https://github.com/beagleboard/BeagleBoard-DeviceTrees/commit/e107ff29ffe740ced5fc32c0f190063a3cf77cc4 CAN-ISOTP: https://github.com/hartkopp/can-isotp/commit/ced84cab120a1ddde2bd4561a18514707c8a6184 RT: patch-4.19.94-rt38.patch.xz TI_AMX3_CM3: http://git.ti.com/gitweb/?p=processor-firmware/ti-amx3-cm3-pm-firmware.git;a=commit;h=6a849767df85ce9399494f53fb5c753665396653
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -15,7 +15,8 @@ KERNEL_ARCH=arm
 #toolchain="gcc_linaro_eabi_5"
 #toolchain="gcc_linaro_eabi_6"
 #toolchain="gcc_linaro_eabi_7"
-#toolchain="gcc_linaro_gnueabi_4_6"
+#toolchain="gcc_arm_eabi_8"
+#toolchain="gcc_arm_eabi_9"
 #toolchain="gcc_linaro_gnueabihf_4_7"
 #toolchain="gcc_linaro_gnueabihf_4_8"
 #toolchain="gcc_linaro_gnueabihf_4_9"
@@ -34,10 +35,10 @@ toolchain="gcc_arm_gnueabihf_8"
 
 #Kernel
 KERNEL_REL=4.19
-KERNEL_TAG=${KERNEL_REL}.90
-kernel_rt=".90-rt35"
+KERNEL_TAG=${KERNEL_REL}.94
+kernel_rt=".94-rt38"
 #Kernel Build
-BUILD=${build_prefix}42
+BUILD=${build_prefix}42.1
 
 #v5.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
