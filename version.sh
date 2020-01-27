d20d7d38076f66962080e6137a353de7ff5f3bb5
Robert Nelson <robertcnelson@gmail.com>
2020-01-27 11:23:05 -0600

    kernel v4.19.94 rebase with: wireguard/can_isotp/device-tree/etc AUFS: https://github.com/sfjro/aufs4-standalone/commit/51e286ef080090bb6fd949b1ae55f94d24bf1687 BBDTBS: https://github.com/beagleboard/BeagleBoard-DeviceTrees/commit/e107ff29ffe740ced5fc32c0f190063a3cf77cc4 CAN-ISOTP: https://github.com/hartkopp/can-isotp/commit/ced84cab120a1ddde2bd4561a18514707c8a6184 TI_AMX3_CM3: http://git.ti.com/gitweb/?p=processor-firmware/ti-amx3-cm3-pm-firmware.git;a=commit;h=6a849767df85ce9399494f53fb5c753665396653 WIREGUARD: https://git.zx2c4.com/WireGuard/commit/edad0d6e99e5133b1e8e865d727a25fff6399cb4
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -36,9 +36,9 @@ toolchain="gcc_arm_gnueabihf_8"
 #Kernel
 KERNEL_REL=4.19
 KERNEL_TAG=${KERNEL_REL}.94
-kernel_rt=".94-rt38"
+kernel_rt=".94-rt39"
 #Kernel Build
-BUILD=${build_prefix}43
+BUILD=${build_prefix}43.1
 
 #v5.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
