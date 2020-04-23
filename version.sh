6e2dd54bed6b16e21ccec7c0311a2ea74612df0f
Robert Nelson <robertcnelson@gmail.com>
2020-04-23 14:05:28 -0500

    sync rt changes
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -36,9 +36,9 @@ toolchain="gcc_arm_gnueabihf_9"
 #Kernel
 KERNEL_REL=5.6
 KERNEL_TAG=${KERNEL_REL}.4
-kernel_rt=".X-rtY"
+kernel_rt=".4-rt3"
 #Kernel Build
-BUILD=${build_prefix}10
+BUILD=${build_prefix}10.1
 
 #v5.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
