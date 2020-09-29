9ed619ec83cbee03e59d5ea99446f97a1f8dbac2
Robert Nelson <robertcnelson@gmail.com>
2020-09-29 10:03:36 -0500

    kernel v5.9-rc7 rebase with: can_isotp/device-tree/etc BBDTBS: https://github.com/beagleboard/BeagleBoard-DeviceTrees/commit/c0adbe9b2fdfeaf3be021759add9684eff9350db CAN-ISOTP: https://github.com/hartkopp/can-isotp/commit/0cb92ae51d109788a350d7af856da44318f9d814 TI_AMX3_CM3: http://git.ti.com/gitweb/?p=processor-firmware/ti-amx3-cm3-pm-firmware.git;a=commit;h=6a849767df85ce9399494f53fb5c753665396653
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -35,10 +35,10 @@ toolchain="gcc_arm_gnueabihf_9"
 
 #Kernel
 KERNEL_REL=5.9
-KERNEL_TAG=${KERNEL_REL}-rc6
+KERNEL_TAG=${KERNEL_REL}-rc7
 kernel_rt=".X-rtY"
 #Kernel Build
-BUILD=${build_prefix}4
+BUILD=${build_prefix}4.1
 
 #v5.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
