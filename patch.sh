2fca53a3b40a2727b32142d027fc401e5a04043b
Robert Nelson <robertcnelson@gmail.com>
2020-12-14 18:17:28 -0600

    kernel v4.14.209 rebase with: wireguard/can_isotp/device-tree/etc AUFS: https://github.com/sfjro/aufs4-standalone/commit/df5c09adc49fd0d431c0f18d190130675c8bffb9 BBDTBS: https://github.com/beagleboard/BeagleBoard-DeviceTrees/commit/b2e543c6d0022e13dba7fe7e9a012c6e5c45407b CAN-ISOTP: https://github.com/hartkopp/can-isotp/commit/748f8efe84ab7c27496d8c8165f28892ba0e8868 TI_AMX3_CM3: http://git.ti.com/gitweb/?p=processor-firmware/ti-amx3-cm3-pm-firmware.git;a=commit;h=6a849767df85ce9399494f53fb5c753665396653 WIREGUARD: https://git.zx2c4.com/WireGuard/commit/edad0d6e99e5133b1e8e865d727a25fff6399cb4
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -483,7 +483,7 @@ patch_backports (){
 }
 
 backports () {
-	backport_tag="v4.19.146"
+	backport_tag="v4.19.163"
 
 	subsystem="greybus"
 	#regenerate="enable"
@@ -499,7 +499,7 @@ backports () {
 		${git} "${DIR}/patches/backports/greybus/0002-greybus-drivers-staging-greybus-module.c-no-struct_s.patch"
 	fi
 
-	backport_tag="v5.4.66"
+	backport_tag="v5.4.83"
 
 	subsystem="wiznet"
 	#regenerate="enable"
