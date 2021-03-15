a120a8cb04bca82c3b1f8c6edc666a1c7e3cb338
Robert Nelson <robertcnelson@gmail.com>
2021-03-15 13:02:03 -0500

    kernel v4.19.180 rebase with: wireguard/can_isotp/device-tree/etc AUFS: https://github.com/sfjro/aufs4-standalone/commit/1bb4caf8d7ee47acc49f05deb4da9d34884c44b2 BBDTBS: https://github.com/beagleboard/BeagleBoard-DeviceTrees/commit/27213f48061dff002c9b297ac3f851048c688073 CAN-ISOTP: https://github.com/hartkopp/can-isotp/commit/748f8efe84ab7c27496d8c8165f28892ba0e8868 TI_AMX3_CM3: http://git.ti.com/gitweb/?p=processor-firmware/ti-amx3-cm3-pm-firmware.git;a=commit;h=6a849767df85ce9399494f53fb5c753665396653 WIREGUARD: https://git.zx2c4.com/WireGuard/commit/edad0d6e99e5133b1e8e865d727a25fff6399cb4
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -45,10 +45,10 @@ toolchain="gcc_arm_gnueabihf_8"
 
 #Kernel
 KERNEL_REL=4.19
-KERNEL_TAG=${KERNEL_REL}.173
-kernel_rt=".173-rt72"
+KERNEL_TAG=${KERNEL_REL}.180
+kernel_rt=".180-rt73"
 #Kernel Build
-BUILD=${build_prefix}60
+BUILD=${build_prefix}60.1
 
 #v5.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
