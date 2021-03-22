7d7cbe2484933325f5bed7c79f280271bc05fd88
Robert Nelson <robertcnelson@gmail.com>
2021-03-22 14:18:33 -0500

    kernel v5.4.106 rebase with rt: v5.4.106-rt54 wireguard/can_isotp/device-tree/etc AUFS: https://github.com/sfjro/aufs5-standalone/commit/062b2393e4fae30efc066061087a8b9747528609 BBDTBS: https://github.com/beagleboard/BeagleBoard-DeviceTrees/commit/52e69fcb5f6e076f714af1196232a11fd420e2fb CAN-ISOTP: https://github.com/hartkopp/can-isotp/commit/b312dbd5e720833cb69bc65af3670a52dd1e93ca RT: patch-5.4.106-rt54.patch.xz TI_AMX3_CM3: http://git.ti.com/gitweb/?p=processor-firmware/ti-amx3-cm3-pm-firmware.git;a=commit;h=fb484c5e54f2e31cf0a338d2927a06a2870bcc2c WIREGUARD: https://git.zx2c4.com/WireGuard/commit/edad0d6e99e5133b1e8e865d727a25fff6399cb4
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -316,19 +316,19 @@ wireguard () {
 }
 
 ti_pm_firmware () {
-	#http://git.ti.com/gitweb/?p=processor-firmware/ti-amx3-cm3-pm-firmware.git;a=shortlog;h=refs/heads/ti-v4.1.y-next
+	#https://git.ti.com/gitweb?p=processor-firmware/ti-amx3-cm3-pm-firmware.git;a=shortlog;h=refs/heads/ti-v4.1.y
 	#regenerate="enable"
 	if [ "x${regenerate}" = "xenable" ] ; then
 
 		cd ../
 		if [ ! -d ./ti-amx3-cm3-pm-firmware ] ; then
-			${git_bin} clone -b ti-v4.1.y-next git://git.ti.com/processor-firmware/ti-amx3-cm3-pm-firmware.git --depth=1
+			${git_bin} clone -b ti-v4.1.y git://git.ti.com/processor-firmware/ti-amx3-cm3-pm-firmware.git --depth=1
 			cd ./ti-amx3-cm3-pm-firmware
 				ti_amx3_cm3_hash=$(git rev-parse HEAD)
 			cd -
 		else
 			rm -rf ./ti-amx3-cm3-pm-firmware || true
-			${git_bin} clone -b ti-v4.1.y-next git://git.ti.com/processor-firmware/ti-amx3-cm3-pm-firmware.git --depth=1
+			${git_bin} clone -b ti-v4.1.y git://git.ti.com/processor-firmware/ti-amx3-cm3-pm-firmware.git --depth=1
 			cd ./ti-amx3-cm3-pm-firmware
 				ti_amx3_cm3_hash=$(git rev-parse HEAD)
 			cd -
@@ -481,7 +481,7 @@ patch_backports (){
 }
 
 backports () {
-	backport_tag="v5.10.22"
+	backport_tag="v5.10.25"
 
 	subsystem="greybus"
 	#regenerate="enable"
@@ -578,6 +578,7 @@ reverts () {
 }
 
 drivers () {
+	#exit 2
 	dir 'RPi'
 	dir 'drivers/ar1021_i2c'
 	dir 'drivers/sound'
@@ -611,7 +612,7 @@ soc
 packaging () {
 	do_backport="enable"
 	if [ "x${do_backport}" = "xenable" ] ; then
-		backport_tag="v5.10.22"
+		backport_tag="v5.10.25"
 
 		subsystem="bindeb-pkg"
 		#regenerate="enable"
