da3d99982ed248c282a7156e661a09a7e62b30bc
Robert Nelson <robertcnelson@gmail.com>
2021-04-15 15:29:45 -0500

    kernel v5.10.27 rebase with rt: v5.10.27-rt36 device-tree/etc AUFS: https://github.com/sfjro/aufs5-standalone/commit/628a13259b5cabf165c4e5c340ceddaee587fd2d BBDTBS: https://github.com/beagleboard/BeagleBoard-DeviceTrees/commit/cbde1f53d0513bd1613aca7083049d3a3c42193f RT: patch-5.10.27-rt36.patch.xz TI_AMX3_CM3: http://git.ti.com/gitweb/?p=processor-firmware/ti-amx3-cm3-pm-firmware.git;a=commit;h=fb484c5e54f2e31cf0a338d2927a06a2870bcc2c WPANUSB: https://github.com/statropy/wpanusb/commit/251f0167545bf2dcaa3cad991a59dbf5ab05490a
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -109,6 +109,7 @@ aufs_fail () {
 }
 
 aufs () {
+	#https://github.com/sfjro/aufs5-standalone/tree/aufs5.10
 	aufs_prefix="aufs5-"
 	#regenerate="enable"
 	if [ "x${regenerate}" = "xenable" ] ; then
@@ -425,7 +426,7 @@ patch_backports (){
 }
 
 backports () {
-	backport_tag="v5.12-rc5"
+	backport_tag="v5.12-rc7"
 
 	subsystem="greybus"
 	#regenerate="enable"
@@ -441,7 +442,7 @@ backports () {
 		patch_backports
 	fi
 
-	backport_tag="v5.12-rc5"
+	backport_tag="v5.12-rc7"
 
 	subsystem="wlcore"
 	#regenerate="enable"
@@ -514,7 +515,7 @@ soc
 packaging () {
 	#do_backport="enable"
 	if [ "x${do_backport}" = "xenable" ] ; then
-		backport_tag="v5.10.11"
+		backport_tag="v5.10.30"
 
 		subsystem="bindeb-pkg"
 		#regenerate="enable"
--- version.sh
+++ version.sh
@@ -42,10 +42,10 @@ toolchain="gcc_arm_gnueabihf_10"
 
 #Kernel
 KERNEL_REL=5.10
-KERNEL_TAG=${KERNEL_REL}.25
-kernel_rt=".25-rt35"
+KERNEL_TAG=${KERNEL_REL}.27
+kernel_rt=".27-rt36"
 #Kernel Build
-BUILD=${build_prefix}29
+BUILD=${build_prefix}29.2
 
 #v5.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
