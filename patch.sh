6a6d7da5a878b737e0f2c0f7b0b8cadc587504bf
Robert Nelson <robertcnelson@gmail.com>
2021-04-23 15:51:41 -0500

    kernel v5.0.21 rebase with: wireguard/can_isotp/device-tree/etc AUFS: https://github.com/sfjro/aufs5-standalone/commit/25f304c2c8c866b551ba7dc0c26b8fc1bbac95fa BBDTBS: https://github.com/beagleboard/BeagleBoard-DeviceTrees/commit/7011307c0575ec8011463aba6be7a21c687d14ef CAN-ISOTP: https://github.com/hartkopp/can-isotp/commit/b312dbd5e720833cb69bc65af3670a52dd1e93ca TI_AMX3_CM3: http://git.ti.com/gitweb/?p=processor-firmware/ti-amx3-cm3-pm-firmware.git;a=commit;h=fb484c5e54f2e31cf0a338d2927a06a2870bcc2c WIREGUARD: https://git.zx2c4.com/WireGuard/commit/edad0d6e99e5133b1e8e865d727a25fff6399cb4
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -109,6 +109,7 @@ aufs_fail () {
 }
 
 aufs () {
+	#https://github.com/sfjro/aufs5-standalone/tree/aufs5.0
 	aufs_prefix="aufs5-"
 	#regenerate="enable"
 	if [ "x${regenerate}" = "xenable" ] ; then
@@ -465,6 +466,7 @@ post_backports () {
 		cd -
 	fi
 
+	rm -f arch/arm/boot/dts/overlays/*.dtbo || true
 	${git_bin} add .
 	${git_bin} commit -a -m "backports: ${subsystem}: from: linux.git" -m "Reference: ${backport_tag}" -s
 	if [ ! -d ../patches/backports/${subsystem}/ ] ; then
@@ -479,6 +481,21 @@ patch_backports (){
 }
 
 backports () {
+	backport_tag="v5.4.114"
+
+	subsystem="wiznet"
+	#regenerate="enable"
+	if [ "x${regenerate}" = "xenable" ] ; then
+		pre_backports
+
+		cp -v ~/linux-src/drivers/net/ethernet/wiznet/* ./drivers/net/ethernet/wiznet/
+
+		post_backports
+		exit 2
+	else
+		patch_backports
+	fi
+
 	backport_tag="v5.3.18"
 
 	subsystem="stmpe"
@@ -542,7 +559,6 @@ ti_rogerq_pruss () {
 }
 
 reverts () {
-	echo "dir: reverts"
 	#regenerate="enable"
 	if [ "x${regenerate}" = "xenable" ] ; then
 		start_cleanup
@@ -551,7 +567,7 @@ reverts () {
 	## notes
 	##git revert --no-edit xyz -s
 
-	#${git} "${DIR}/patches/reverts/0001-Revert-xyz.patch"
+	dir 'reverts'
 
 	if [ "x${regenerate}" = "xenable" ] ; then
 		wdir="reverts"
@@ -569,7 +585,6 @@ drivers () {
 	dir 'drivers/spi'
 	dir 'drivers/ssd1306'
 	dir 'drivers/tps65217'
-	dir 'drivers/wiznet'
 
 	dir 'drivers/ti/cpsw'
 	dir 'drivers/ti/etnaviv'
--- version.sh
+++ version.sh
@@ -48,7 +48,7 @@ KERNEL_REL=5.0
 KERNEL_TAG=${KERNEL_REL}.21
 kernel_rt=".X-rtY"
 #Kernel Build
-BUILD=${build_prefix}18
+BUILD=${build_prefix}18.1
 
 #v5.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
