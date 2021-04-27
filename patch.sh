a65272f1d390ce904f49a01cd4fb41854dc5f812
Robert Nelson <robertcnelson@gmail.com>
2021-04-27 09:34:58 -0500

    kernel v5.12 rebase with: device-tree/etc BBDTBS: https://github.com/beagleboard/BeagleBoard-DeviceTrees/commit/f9bbbcb253f50ba050235220cb4525162409d189 TI_AMX3_CM3: http://git.ti.com/gitweb/?p=processor-firmware/ti-amx3-cm3-pm-firmware.git;a=commit;h=fb484c5e54f2e31cf0a338d2927a06a2870bcc2c WPANUSB: https://github.com/statropy/wpanusb/commit/251f0167545bf2dcaa3cad991a59dbf5ab05490a
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -408,6 +408,7 @@ post_backports () {
 		cd -
 	fi
 
+	rm -f arch/arm/boot/dts/overlays/*.dtbo || true
 	${git_bin} add .
 	${git_bin} commit -a -m "backports: ${subsystem}: from: linux.git" -m "Reference: ${backport_tag}" -s
 	if [ ! -d ../patches/backports/${subsystem}/ ] ; then
@@ -422,7 +423,7 @@ patch_backports (){
 }
 
 backports () {
-	backport_tag="v5.12-rc8"
+	backport_tag="v5.12"
 
 	subsystem="greybus"
 	#regenerate="enable"
@@ -434,11 +435,11 @@ backports () {
 
 		post_backports
 		exit 2
-	else
-		patch_backports
+#	else
+#		patch_backports
 	fi
 
-	backport_tag="v5.12-rc8"
+	backport_tag="v5.12"
 
 	subsystem="wlcore"
 	#regenerate="enable"
@@ -449,9 +450,26 @@ backports () {
 
 		post_backports
 		exit 2
-	else
-		patch_backports
+#	else
+#		patch_backports
 	fi
+
+	backport_tag="v5.12"
+
+	subsystem="spidev"
+	#regenerate="enable"
+	if [ "x${regenerate}" = "xenable" ] ; then
+		pre_backports
+
+		cp -v ~/linux-src/drivers/spi/spidev.c ./drivers/spi/spidev.c
+
+		post_backports
+		exit 2
+#	else
+#		patch_backports
+	fi
+
+	${git} "${DIR}/patches/backports/spidev/0002-spidev-Add-Micron-SPI-NOR-Authenta-device-compatible.patch"
 }
 
 reverts () {
@@ -473,8 +491,9 @@ reverts () {
 }
 
 drivers () {
+	#https://github.com/raspberrypi/linux/branches
 	#exit 2
-#	dir 'RPi'
+	dir 'RPi'
 	dir 'drivers/ar1021_i2c'
 	dir 'drivers/spi'
 	dir 'drivers/tps65217'
@@ -499,7 +518,7 @@ soc () {
 }
 
 ###
-#backports
+backports
 #reverts
 drivers
 soc
--- version.sh
+++ version.sh
@@ -42,10 +42,10 @@ toolchain="gcc_arm_gnueabihf_10"
 
 #Kernel
 KERNEL_REL=5.12
-KERNEL_TAG=${KERNEL_REL}-rc8
+KERNEL_TAG=${KERNEL_REL}
 kernel_rt=".x-rty"
 #Kernel Build
-BUILD=${build_prefix}10
+BUILD=${build_prefix}10.1
 
 #v5.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
