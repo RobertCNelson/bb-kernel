4472f4e23efd12a75fe9b6ee00e8ca4145267c1f
Robert Nelson <robertcnelson@gmail.com>
2021-05-12 13:52:14 -0500

    kernel v5.11.20 rebase with: device-tree/etc AUFS: https://github.com/sfjro/aufs5-standalone/commit/082b34fe8001f43a8b0c56a6484d95f2a25f880a BBDTBS: https://github.com/beagleboard/BeagleBoard-DeviceTrees/commit/cb5cf4df2382cb55785a13a43b5a67fec7017736 TI_AMX3_CM3: http://git.ti.com/gitweb/?p=processor-firmware/ti-amx3-cm3-pm-firmware.git;a=commit;h=fb484c5e54f2e31cf0a338d2927a06a2870bcc2c WPANUSB: https://github.com/statropy/wpanusb/commit/251f0167545bf2dcaa3cad991a59dbf5ab05490a
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -25,7 +25,7 @@ DEBARCH=armhf
 #toolchain="gcc_linaro_gnueabihf_7"
 #toolchain="gcc_arm_gnueabihf_8"
 #toolchain="gcc_arm_gnueabihf_9"
-toolchain="gcc_arm_gnueabihf_10"
+toolchain="gcc_10_arm"
 #arm64
 #KERNEL_ARCH=arm64
 #DEBARCH=arm64
@@ -34,18 +34,20 @@ toolchain="gcc_arm_gnueabihf_10"
 #toolchain="gcc_linaro_aarch64_gnu_7"
 #toolchain="gcc_arm_aarch64_gnu_8"
 #toolchain="gcc_arm_aarch64_gnu_9"
-#toolchain="gcc_arm_aarch64_gnu_10"
+#toolchain="gcc_10_aarch64"
 #riscv64
 #KERNEL_ARCH=riscv
 #DEBARCH=riscv64
+#toolchain="gcc_8_riscv64"
+#toolchain="gcc_9_riscv64"
 #toolchain="gcc_10_riscv64"
 
 #Kernel
 KERNEL_REL=5.11
-KERNEL_TAG=${KERNEL_REL}.18
+KERNEL_TAG=${KERNEL_REL}.20
 kernel_rt=".X-rtY"
 #Kernel Build
-BUILD=${build_prefix}17
+BUILD=${build_prefix}17.1
 
 #v5.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
