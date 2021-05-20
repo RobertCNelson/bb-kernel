76e2c3b36386db76e633e526bb58d858c442265c
Robert Nelson <robertcnelson@gmail.com>
2021-05-20 15:26:36 -0500

    kernel v5.8.18 rebase with: can_isotp/device-tree/etc AUFS: https://github.com/sfjro/aufs5-standalone/commit/c3286f2ea36d198035e1038e7310c6edb537535a BBDTBS: https://github.com/beagleboard/BeagleBoard-DeviceTrees/commit/77541acbdbfc44bdefe8d4cf6d30c7d32101f1dc CAN-ISOTP: https://github.com/hartkopp/can-isotp/commit/b312dbd5e720833cb69bc65af3670a52dd1e93ca TI_AMX3_CM3: http://git.ti.com/gitweb/?p=processor-firmware/ti-amx3-cm3-pm-firmware.git;a=commit;h=fb484c5e54f2e31cf0a338d2927a06a2870bcc2c WPANUSB: https://github.com/statropy/wpanusb/commit/251f0167545bf2dcaa3cad991a59dbf5ab05490a WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/sforshee/wireless-regdb.git/commit/?id=04f3e5411b1cdc59292775e7b8e05a5d35ea4a80
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -17,16 +17,13 @@ DEBARCH=armhf
 #toolchain="gcc_linaro_eabi_5"
 #toolchain="gcc_linaro_eabi_6"
 #toolchain="gcc_linaro_eabi_7"
-#toolchain="gcc_arm_eabi_8"
-#toolchain="gcc_arm_eabi_9"
-#toolchain="gcc_arm_eabi_10"
 #toolchain="gcc_linaro_gnueabihf_4_8"
 #toolchain="gcc_linaro_gnueabihf_4_9"
 #toolchain="gcc_linaro_gnueabihf_5"
 #toolchain="gcc_linaro_gnueabihf_6"
 #toolchain="gcc_linaro_gnueabihf_7"
-#toolchain="gcc_arm_gnueabihf_8"
-toolchain="gcc_arm_gnueabihf_9"
+#toolchain="gcc_8_arm"
+toolchain="gcc_9_arm"
 #toolchain="gcc_10_arm"
 #arm64
 #KERNEL_ARCH=arm64
@@ -49,7 +46,7 @@ KERNEL_REL=5.8
 KERNEL_TAG=${KERNEL_REL}.18
 kernel_rt=".X-rtY"
 #Kernel Build
-BUILD=${build_prefix}33
+BUILD=${build_prefix}33.1
 
 #v5.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
