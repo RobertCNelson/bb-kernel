5d784d85ae87dea9504022447c6b859356119c4d
Robert Nelson <robertcnelson@gmail.com>
2021-05-20 17:11:24 -0500

    kernel v5.10.35 rebase with: device-tree/etc AUFS: https://github.com/sfjro/aufs5-standalone/commit/304f28d3c932b1b999393b04dcf380180418e409 BBDTBS: https://github.com/beagleboard/BeagleBoard-DeviceTrees/commit/289566e6f0377f1c77cd020f30134ce4fac735b2 TI_AMX3_CM3: http://git.ti.com/gitweb/?p=processor-firmware/ti-amx3-cm3-pm-firmware.git;a=commit;h=fb484c5e54f2e31cf0a338d2927a06a2870bcc2c WPANUSB: https://github.com/statropy/wpanusb/commit/251f0167545bf2dcaa3cad991a59dbf5ab05490a WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/sforshee/wireless-regdb.git/commit/?id=04f3e5411b1cdc59292775e7b8e05a5d35ea4a80
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- repo_maintenance/push-kernel-n-test.sh
+++ repo_maintenance/push-kernel-n-test.sh
@@ -25,6 +25,10 @@ cat_files () {
 	if [ -f ./patches/git/WPANUSB ] ; then
 		cat ./patches/git/WPANUSB >> ${wfile}
 	fi
+
+	if [ -f ./patches/git/WIRELESS_REGDB ] ; then
+		cat ./patches/git/WIRELESS_REGDB >> ${wfile}
+	fi
 }
 
 DIR=$PWD
