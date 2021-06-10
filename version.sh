36acac1ef0ada0d199c5960413adf082144842ad
Robert Nelson <robertcnelson@gmail.com>
2021-06-10 17:44:41 -0500

    kernel v5.4.123 rebase with: wireguard/can_isotp/device-tree/etc AUFS: https://github.com/sfjro/aufs5-standalone/commit/1d622ad812e528026bafd13b59d1ccdb185437df BBDTBS: https://github.com/beagleboard/BeagleBoard-DeviceTrees/commit/4fa8829467ea2c6c5611bc9eb3c38a845ced565d CAN-ISOTP: https://github.com/hartkopp/can-isotp/commit/593c79fb0f4d6e36f1cce5d1c880e26664bc574e TI_AMX3_CM3: http://git.ti.com/gitweb/?p=processor-firmware/ti-amx3-cm3-pm-firmware.git;a=commit;h=fb484c5e54f2e31cf0a338d2927a06a2870bcc2c WIREGUARD: https://git.zx2c4.com/WireGuard/commit/edad0d6e99e5133b1e8e865d727a25fff6399cb4 WPANUSB: https://github.com/statropy/wpanusb/commit/251f0167545bf2dcaa3cad991a59dbf5ab05490a WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/sforshee/wireless-regdb.git/commit/?id=04f3e5411b1cdc59292775e7b8e05a5d35ea4a80
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -17,6 +17,7 @@ DEBARCH=armhf
 toolchain="gcc_8_arm"
 #toolchain="gcc_9_arm"
 #toolchain="gcc_10_arm"
+#toolchain="gcc_11_arm"
 #arm64
 #KERNEL_ARCH=arm64
 #DEBARCH=arm64
@@ -25,6 +26,7 @@ toolchain="gcc_8_arm"
 #toolchain="gcc_8_aarch64"
 #toolchain="gcc_9_aarch64"
 #toolchain="gcc_10_aarch64"
+#toolchain="gcc_11_aarch64"
 #riscv64
 #KERNEL_ARCH=riscv
 #DEBARCH=riscv64
@@ -32,13 +34,14 @@ toolchain="gcc_8_arm"
 #toolchain="gcc_8_riscv64"
 #toolchain="gcc_9_riscv64"
 #toolchain="gcc_10_riscv64"
+#toolchain="gcc_11_riscv64"
 
 #Kernel
 KERNEL_REL=5.4
-KERNEL_TAG=${KERNEL_REL}.115
-kernel_rt=".115-rt57"
+KERNEL_TAG=${KERNEL_REL}.123
+kernel_rt=".123-rt59"
 #Kernel Build
-BUILD=${build_prefix}52
+BUILD=${build_prefix}52.1
 
 #v5.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
