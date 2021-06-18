c712f4b906c1767b6954bf0dc51a340d5f9a92bc
Robert Nelson <robertcnelson@gmail.com>
2021-06-18 17:47:45 -0500

    kernel v5.8.18 rebase with: can_isotp/device-tree/etc AUFS: https://github.com/sfjro/aufs5-standalone/commit/c3286f2ea36d198035e1038e7310c6edb537535a BBDTBS: https://github.com/beagleboard/BeagleBoard-DeviceTrees/commit/7f2f67f468b1c31710609eb8c74ecc5a0f5eedf2 CAN-ISOTP: https://github.com/hartkopp/can-isotp/commit/593c79fb0f4d6e36f1cce5d1c880e26664bc574e TI_AMX3_CM3: http://git.ti.com/gitweb/?p=processor-firmware/ti-amx3-cm3-pm-firmware.git;a=commit;h=fb484c5e54f2e31cf0a338d2927a06a2870bcc2c WPANUSB: https://github.com/statropy/wpanusb/commit/251f0167545bf2dcaa3cad991a59dbf5ab05490a WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/sforshee/wireless-regdb.git/commit/?id=04f3e5411b1cdc59292775e7b8e05a5d35ea4a80
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -503,7 +503,7 @@ patch_backports (){
 }
 
 backports () {
-	backport_tag="v5.12.7"
+	backport_tag="v5.12.12"
 
 	subsystem="greybus"
 	#regenerate="enable"
@@ -519,7 +519,7 @@ backports () {
 		patch_backports
 	fi
 
-	backport_tag="v5.12.7"
+	backport_tag="v5.12.12"
 
 	subsystem="wlcore"
 	#regenerate="enable"
@@ -699,14 +699,14 @@ backports () {
 		${git_bin} commit -a -m "cypress fmac patchset" -m "v5.4.18-2021_0114" -s
 		${git_bin} format-patch -1 -o ../patches/cypress/
 
-		exit 2
-#	else
-#		patch_backports
+	#	exit 2
+	#else
+		patch_backports
 	fi
 
-#	dir 'cypress'
+	#dir 'cypress'
 	#exit 2
-#	dir 'cypress/fixes'
+	#dir 'cypress/fixes'
 }
 
 reverts () {
@@ -741,7 +741,9 @@ drivers () {
 	dir 'drivers/ti/serial'
 	dir 'drivers/ti/tsc'
 	dir 'drivers/ti/gpio'
+#	dir 'drivers/ti/mmc'
 	dir 'drivers/greybus'
+	dir 'drivers/bluetooth'
 	dir 'drivers/mikrobus'
 	dir 'drivers/serdev'
 	dir 'drivers/iio'
@@ -767,7 +769,7 @@ soc
 packaging () {
 	do_backport="enable"
 	if [ "x${do_backport}" = "xenable" ] ; then
-		backport_tag="v5.10.40"
+		backport_tag="v5.10.45"
 
 		subsystem="bindeb-pkg"
 		#regenerate="enable"
