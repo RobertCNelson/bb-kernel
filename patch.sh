46a8d3a7f20729cf789bbb7f58c94442ffb1aadf
Robert Nelson <robertcnelson@gmail.com>
2021-07-14 14:26:13 -0500

    kernel v5.10.47 rebase with rt: v5.10.47-rt45 device-tree/etc AUFS: https://github.com/sfjro/aufs5-standalone/commit/304f28d3c932b1b999393b04dcf380180418e409 BBDTBS: https://github.com/beagleboard/BeagleBoard-DeviceTrees/commit/050224a18ae8d9ec58b58cd62e4d3e093c03dba7 RT: patch-5.10.47-rt45.patch.xz TI_AMX3_CM3: http://git.ti.com/gitweb/?p=processor-firmware/ti-amx3-cm3-pm-firmware.git;a=commit;h=fb484c5e54f2e31cf0a338d2927a06a2870bcc2c WPANUSB: https://github.com/statropy/wpanusb/commit/251f0167545bf2dcaa3cad991a59dbf5ab05490a WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/sforshee/wireless-regdb.git/commit/?id=a4468e8f1ff2710982d2a8042e974d801e3a7ba0
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -348,6 +348,12 @@ ti_pm_firmware () {
 	dir 'drivers/ti/firmware'
 }
 
+cleanup_dts_builds () {
+	rm -rf arch/arm/boot/dts/.*cmd || true
+	rm -rf arch/arm/boot/dts/.*tmp || true
+	rm -rf arch/arm/boot/dts/*dtb || true
+}
+
 dtb_makefile_append () {
 	sed -i -e 's:am335x-boneblack.dtb \\:am335x-boneblack.dtb \\\n\t'$device' \\:g' arch/arm/boot/dts/Makefile
 }
@@ -372,6 +378,9 @@ beagleboard_dtbs () {
 			cd -
 		fi
 		cd ./KERNEL/
+
+		cleanup_dts_builds
+		rm -rf arch/arm/boot/dts/overlays/ || true
 
 		mkdir -p arch/arm/boot/dts/overlays/
 		cp -vr ../${work_dir}/src/arm/* arch/arm/boot/dts/
@@ -463,7 +472,7 @@ patch_backports (){
 }
 
 backports () {
-	backport_tag="v5.12.13"
+	backport_tag="v5.12.17"
 
 	subsystem="greybus"
 	#regenerate="enable"
@@ -479,7 +488,7 @@ backports () {
 		patch_backports
 	fi
 
-	backport_tag="v5.12.13"
+	backport_tag="v5.12.17"
 
 	subsystem="wlcore"
 	#regenerate="enable"
@@ -494,7 +503,7 @@ backports () {
 		patch_backports
 	fi
 
-	backport_tag="v5.13-rc7"
+	backport_tag="v5.13.2"
 
 	subsystem="spidev"
 	#regenerate="enable"
@@ -509,7 +518,7 @@ backports () {
 		patch_backports
 	fi
 
-	backport_tag="v5.13-rc7"
+	backport_tag="v5.13.2"
 
 	subsystem="pru_rproc"
 	#regenerate="enable"
@@ -528,7 +537,7 @@ backports () {
 		patch_backports
 	fi
 
-	backport_tag="v5.10.46"
+	backport_tag="v5.10.50"
 
 	subsystem="iio"
 	#regenerate="enable"
@@ -577,12 +586,12 @@ drivers () {
 	dir 'drivers/ti/cpsw'
 	dir 'drivers/ti/serial'
 	dir 'drivers/ti/tsc'
+	dir 'drivers/ti/gpio'
 	dir 'drivers/greybus'
 	dir 'drivers/mikrobus'
 	dir 'drivers/serdev'
 	dir 'drivers/iio'
 	dir 'drivers/fb_ssd1306'
-	dir 'drivers/usb'
 	dir 'drivers/bluetooth'
 	dir 'fixes'
 }
@@ -605,7 +614,7 @@ soc
 packaging () {
 	#do_backport="enable"
 	if [ "x${do_backport}" = "xenable" ] ; then
-		backport_tag="v5.10.46"
+		backport_tag="v5.10.50"
 
 		subsystem="bindeb-pkg"
 		#regenerate="enable"
