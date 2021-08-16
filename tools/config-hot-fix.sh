0dd60651edce917b1313dfceaf4317e7d5aac74d
Robert Nelson <robertcnelson@gmail.com>
2021-08-16 16:57:12 -0500

    kernel v5.10.56 rebase with rt: v5.10.56-rt49 device-tree/etc AUFS: https://github.com/sfjro/aufs5-standalone/commit/95c58be95f8200c308d571070a913eed5ef5a3c2 BBDTBS: https://github.com/beagleboard/BeagleBoard-DeviceTrees/commit/0457cb0e9fdec2f1f99cb91ef7efe35d0b6d7c53 RT: patch-5.10.56-rt49.patch.xz TI_AMX3_CM3: http://git.ti.com/gitweb/?p=processor-firmware/ti-amx3-cm3-pm-firmware.git;a=commit;h=fb484c5e54f2e31cf0a338d2927a06a2870bcc2c WPANUSB: https://github.com/statropy/wpanusb/commit/251f0167545bf2dcaa3cad991a59dbf5ab05490a WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/sforshee/wireless-regdb.git/commit/?id=a0bcb88f614b58e708a4f456c3cb594a1bd1b00d
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/config-hot-fix.sh
+++ tools/config-hot-fix.sh
@@ -95,4 +95,54 @@ config="CONFIG_REMOTEPROC_CDEV" ; config_enable
 config="CONFIG_WKUP_M3_RPROC" ; config_enable
 config="CONFIG_PRU_REMOTEPROC" ; config_module
 
+#abi="5.13.0-trunk"
+#kernel="5.13.9-1~exp2"
+config="CONFIG_BPF_UNPRIV_DEFAULT_OFF" ; config_enable
+config="CONFIG_CGROUP_MISC" ; config_enable
+config="CONFIG_RESET_ATTACK_MITIGATION" ; config_enable
+
+#LIBCOMPOSITE built-in finally works... ;)
+config="CONFIG_USB_LIBCOMPOSITE" ; config_enable
+config="CONFIG_USB_F_ACM" ; config_enable
+config="CONFIG_USB_F_SS_LB" ; config_enable
+config="CONFIG_USB_U_SERIAL" ; config_enable
+config="CONFIG_USB_U_ETHER" ; config_enable
+config="CONFIG_USB_U_AUDIO" ; config_enable
+config="CONFIG_USB_F_SERIAL" ; config_enable
+config="CONFIG_USB_F_OBEX" ; config_enable
+config="CONFIG_USB_F_NCM" ; config_enable
+config="CONFIG_USB_F_ECM" ; config_enable
+config="CONFIG_USB_F_PHONET" ; config_module
+config="CONFIG_USB_F_EEM" ; config_enable
+config="CONFIG_USB_F_SUBSET" ; config_enable
+config="CONFIG_USB_F_RNDIS" ; config_enable
+config="CONFIG_USB_F_MASS_STORAGE" ; config_enable
+config="CONFIG_USB_F_FS" ; config_enable
+config="CONFIG_USB_F_UAC1" ; config_enable
+config="CONFIG_USB_F_UAC2" ; config_enable
+config="CONFIG_USB_F_UVC" ; config_module
+config="CONFIG_USB_F_MIDI" ; config_enable
+config="CONFIG_USB_F_HID" ; config_enable
+config="CONFIG_USB_F_PRINTER" ; config_enable
+config="CONFIG_USB_F_TCM" ; config_module
+config="CONFIG_USB_CONFIGFS" ; config_enable
+config="CONFIG_USB_CONFIGFS_SERIAL" ; config_enable
+config="CONFIG_USB_CONFIGFS_ACM" ; config_enable
+config="CONFIG_USB_CONFIGFS_OBEX" ; config_enable
+config="CONFIG_USB_CONFIGFS_NCM" ; config_enable
+config="CONFIG_USB_CONFIGFS_ECM" ; config_enable
+config="CONFIG_USB_CONFIGFS_ECM_SUBSET" ; config_enable
+config="CONFIG_USB_CONFIGFS_RNDIS" ; config_enable
+config="CONFIG_USB_CONFIGFS_EEM" ; config_enable
+config="CONFIG_USB_CONFIGFS_PHONET" ; config_enable
+config="CONFIG_USB_CONFIGFS_MASS_STORAGE" ; config_enable
+config="CONFIG_USB_CONFIGFS_F_LB_SS" ; config_enable
+config="CONFIG_USB_CONFIGFS_F_FS" ; config_enable
+config="CONFIG_USB_CONFIGFS_F_UAC1" ; config_enable
+config="CONFIG_USB_CONFIGFS_F_UAC2" ; config_enable
+config="CONFIG_USB_CONFIGFS_F_MIDI" ; config_enable
+config="CONFIG_USB_CONFIGFS_F_HID" ; config_enable
+config="CONFIG_USB_CONFIGFS_F_UVC" ; config_enable
+config="CONFIG_USB_CONFIGFS_F_PRINTER" ; config_enable
+
 cd ${DIR}/
