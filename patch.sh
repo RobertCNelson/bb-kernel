458ea1270a4088c26d5f9b3804a9658752d65412
Robert Nelson <robertcnelson@gmail.com>
2021-08-24 20:13:33 -0500

    kernel v5.12.19 rebase with: device-tree/etc AUFS: https://github.com/sfjro/aufs5-standalone/commit/870768284f808dc85c3cef530f86d35f17fa48dc BBDTBS: https://github.com/beagleboard/BeagleBoard-DeviceTrees/commit/c802f7e328ea8032489fd1bb4792a2f1e0c29131 TI_AMX3_CM3: http://git.ti.com/gitweb/?p=processor-firmware/ti-amx3-cm3-pm-firmware.git;a=commit;h=fb484c5e54f2e31cf0a338d2927a06a2870bcc2c WPANUSB: https://github.com/statropy/wpanusb/commit/251f0167545bf2dcaa3cad991a59dbf5ab05490a WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/sforshee/wireless-regdb.git/commit/?id=a0bcb88f614b58e708a4f456c3cb594a1bd1b00d
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -462,7 +462,7 @@ patch_backports (){
 }
 
 backports () {
-	backport_tag="v5.13.11"
+	backport_tag="v5.13.12"
 
 	subsystem="greybus"
 	#regenerate="enable"
@@ -478,7 +478,7 @@ backports () {
 		patch_backports
 	fi
 
-	backport_tag="v5.13.11"
+	backport_tag="v5.13.12"
 
 	subsystem="spidev"
 	#regenerate="enable"
@@ -493,7 +493,7 @@ backports () {
 		patch_backports
 	fi
 
-	backport_tag="v5.13.11"
+	backport_tag="v5.13.12"
 
 	subsystem="pinctrl"
 	#regenerate="enable"
@@ -502,25 +502,6 @@ backports () {
 
 		cp -rv ~/linux-src/drivers/pinctrl/* ./drivers/pinctrl/
 		cp -rv ~/linux-src/include/linux/pinctrl/* ./include/linux/pinctrl/
-
-		post_backports
-		exit 2
-	else
-		patch_backports
-	fi
-
-	backport_tag="v5.13.11"
-
-	subsystem="pru_rproc"
-	#regenerate="enable"
-	if [ "x${regenerate}" = "xenable" ] ; then
-		pre_backports
-
-		cp -v ~/linux-src/drivers/irqchip/irq-pruss-intc.c ./drivers/irqchip/
-		cp -rv ~/linux-src/drivers/remoteproc/* ./drivers/remoteproc/
-		cp -v ~/linux-src/include/linux/remoteproc.h ./include/linux/remoteproc.h
-		cp -rv ~/linux-src/include/linux/remoteproc/* ./include/linux/remoteproc/
-		cp -v ~/linux-src/include/linux/pruss_driver.h ./include/linux/pruss_driver.h
 
 		post_backports
 		exit 2
@@ -580,7 +561,7 @@ soc
 packaging () {
 	#do_backport="enable"
 	if [ "x${do_backport}" = "xenable" ] ; then
-		backport_tag="v5.10.59"
+		backport_tag="v5.10.60"
 
 		subsystem="bindeb-pkg"
 		#regenerate="enable"
