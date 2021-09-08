848a1bd56b33035ae246f76d5034ad3f98ec532e
Robert Nelson <robertcnelson@gmail.com>
2021-09-07 19:50:15 -0500

    kernel v5.14 rebase with: device-tree/etc AUFS: https://github.com/sfjro/aufs5-standalone/commit/03a3ee3199ae847b9e5ac7596900c64fc17cfd5a BBDTBS: https://github.com/beagleboard/BeagleBoard-DeviceTrees/commit/b498ccccd153ec69a57b077887a53522e7472acd TI_AMX3_CM3: http://git.ti.com/gitweb/?p=processor-firmware/ti-amx3-cm3-pm-firmware.git;a=commit;h=fb484c5e54f2e31cf0a338d2927a06a2870bcc2c WPANUSB: https://github.com/statropy/wpanusb/commit/251f0167545bf2dcaa3cad991a59dbf5ab05490a WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/sforshee/wireless-regdb.git/commit/?id=47007d0169c4c6c855a3fc5333c142489a43e89e
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -355,6 +355,7 @@ next_dtbs () {
 }
 
 cleanup_dts_builds () {
+	rm -rf arch/arm/boot/dts/modules.order || true
 	rm -rf arch/arm/boot/dts/.*cmd || true
 	rm -rf arch/arm/boot/dts/.*tmp || true
 	rm -rf arch/arm/boot/dts/*dtb || true
@@ -396,6 +397,9 @@ beagleboard_dtbs () {
 
 		device="am335x-boneblack-uboot.dtb" ; dtb_makefile_append
 
+		device="am335x-bone-uboot-univ.dtb" ; dtb_makefile_append
+		device="am335x-boneblack-uboot-univ.dtb" ; dtb_makefile_append
+
 		${git_bin} add -f arch/arm/boot/dts/
 		${git_bin} add -f include/dt-bindings/
 		${git_bin} commit -a -m "Add BeagleBoard.org Device Tree Changes" -m "${https_repo}/tree/${branch}" -m "${https_repo}/commit/${git_hash}" -s
@@ -424,7 +428,7 @@ local_patch () {
 }
 
 #external_git
-#aufs
+aufs
 wpanusb
 #rt
 wireless_regdb
@@ -522,9 +526,6 @@ drivers () {
 }
 
 soc () {
-#	dir 'soc/imx/imx7'
-
-#	dir 'soc/ti/panda'
 	dir 'bootup_hacks'
 }
 
