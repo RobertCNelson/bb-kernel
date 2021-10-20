d9ab0e9cb6b7dfa28be305956ca492337d08c7d1
Robert Nelson <robertcnelson@gmail.com>
2021-10-20 13:58:48 -0500

    kernel v5.14.14 rebase with: device-tree/etc AUFS: https://github.com/sfjro/aufs5-standalone/commit/05607e3cb465c625e40d03871dac369ecd5a8b0e BBDTBS: https://github.com/beagleboard/BeagleBoard-DeviceTrees/commit/c192f53d2a6d4ab387f9b132a753b493f5ee2662 TI_AMX3_CM3: http://git.ti.com/gitweb/?p=processor-firmware/ti-amx3-cm3-pm-firmware.git;a=commit;h=fb484c5e54f2e31cf0a338d2927a06a2870bcc2c WPANUSB: https://github.com/statropy/wpanusb/commit/251f0167545bf2dcaa3cad991a59dbf5ab05490a WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/sforshee/wireless-regdb.git/commit/?id=47007d0169c4c6c855a3fc5333c142489a43e89e
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -38,10 +38,10 @@ toolchain="gcc_11_arm"
 
 #Kernel
 KERNEL_REL=5.14
-KERNEL_TAG=${KERNEL_REL}.13
+KERNEL_TAG=${KERNEL_REL}.14
 kernel_rt=".X-rtY"
 #Kernel Build
-BUILD=${build_prefix}14
+BUILD=${build_prefix}14.1
 
 #v5.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
