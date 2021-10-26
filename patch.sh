0fa6dd9b4557bbb6aae00e626ede815505a62516
Robert Nelson <robertcnelson@gmail.com>
2021-10-26 10:19:59 -0500

    kernel v5.15-rc7 rebase with: device-tree/etc AUFS: https://github.com/sfjro/aufs5-standalone/commit/03a3ee3199ae847b9e5ac7596900c64fc17cfd5a BBDTBS: https://github.com/beagleboard/BeagleBoard-DeviceTrees/commit/93bbdf59348fb29d55fc7d03eccbaebee2616aa8 TI_AMX3_CM3: http://git.ti.com/gitweb/?p=processor-firmware/ti-amx3-cm3-pm-firmware.git;a=commit;h=fb484c5e54f2e31cf0a338d2927a06a2870bcc2c WPANUSB: https://github.com/statropy/wpanusb/commit/251f0167545bf2dcaa3cad991a59dbf5ab05490a WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/sforshee/wireless-regdb.git/commit/?id=47007d0169c4c6c855a3fc5333c142489a43e89e
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -542,7 +542,7 @@ soc
 packaging () {
 	#do_backport="enable"
 	if [ "x${do_backport}" = "xenable" ] ; then
-		backport_tag="v5.10.74"
+		backport_tag="v5.10.75"
 
 		subsystem="bindeb-pkg"
 		#regenerate="enable"
--- version.sh
+++ version.sh
@@ -38,10 +38,10 @@ toolchain="gcc_11_arm"
 
 #Kernel
 KERNEL_REL=5.15
-KERNEL_TAG=${KERNEL_REL}-rc6
+KERNEL_TAG=${KERNEL_REL}-rc7
 kernel_rt=".X-rtY"
 #Kernel Build
-BUILD=${build_prefix}5
+BUILD=${build_prefix}5.1
 
 #v5.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
