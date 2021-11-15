540279f23ab209c6aba57a3f8d723ad4a399b267
Robert Nelson <robertcnelson@gmail.com>
2021-11-15 15:19:48 -0600

    kernel v5.4.154 rebase with rt: v5.4.154-rt65 wireguard/can_isotp/device-tree/etc AUFS: https://github.com/sfjro/aufs5-standalone/commit/995f07ca91f8e03f4686a36d1abd433d764a57ab BBDTBS: https://github.com/beagleboard/BeagleBoard-DeviceTrees/commit/4279432f763392597eb8b2aefc7752d9ec8eae56 CAN-ISOTP: https://github.com/hartkopp/can-isotp/commit/beb4650660179963a8ed5b5cbf2085cc1b34f608 RT: patch-5.4.154-rt65.patch.xz TI_AMX3_CM3: http://git.ti.com/gitweb/?p=processor-firmware/ti-amx3-cm3-pm-firmware.git;a=commit;h=fb484c5e54f2e31cf0a338d2927a06a2870bcc2c WIREGUARD: https://git.zx2c4.com/WireGuard/commit/edad0d6e99e5133b1e8e865d727a25fff6399cb4 WPANUSB: https://github.com/statropy/wpanusb/commit/251f0167545bf2dcaa3cad991a59dbf5ab05490a BCFSERIAL: https://github.com/statropy/bcfserial/commit/aded88429a8a00143596b41f4c1f50d9ae3d4069 WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/sforshee/wireless-regdb.git/commit/?id=47007d0169c4c6c855a3fc5333c142489a43e89e
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- repo_maintenance/push-bb.org.sh
+++ repo_maintenance/push-bb.org.sh
@@ -54,6 +54,10 @@ cat_files () {
 		cat ../patches/git/WPANUSB >> ${wfile}
 	fi
 
+	if [ -f ../patches/git/BCFSERIAL ] ; then
+		cat ../patches/git/BCFSERIAL >> ${wfile}
+	fi
+
 	if [ -f ../patches/git/WIRELESS_REGDB ] ; then
 		cat ../patches/git/WIRELESS_REGDB >> ${wfile}
 	fi
