6eb1e78d19ad6cfccb4688018e6aaee902304b62
Robert Nelson <robertcnelson@gmail.com>
2021-12-21 15:06:38 -0600

    config: renenable thumb2, bluetooth broken again
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -588,7 +588,7 @@ fixes
 packaging () {
 	#do_backport="enable"
 	if [ "x${do_backport}" = "xenable" ] ; then
-		backport_tag="v5.15.6"
+		backport_tag="v5.15.10"
 
 		subsystem="bindeb-pkg"
 		#regenerate="enable"
