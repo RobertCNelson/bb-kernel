4df77df77658ea612639880df2eb5166a7685722
Robert Nelson <robertcnelson@gmail.com>
2022-02-21 16:57:44 -0600

    kernel v5.16.10 rebase with: device-tree/etc AUFS: https://github.com/sfjro/aufs5-standalone/commit/a5c609156cb988491fc5da9dade523846664f3b1 BBDTBS: https://github.com/beagleboard/BeagleBoard-DeviceTrees/commit/0563a56276bbc31141976630f8e7a89d4fd4c4be TI_AMX3_CM3: http://git.ti.com/gitweb/?p=processor-firmware/ti-amx3-cm3-pm-firmware.git;a=commit;h=fb484c5e54f2e31cf0a338d2927a06a2870bcc2c WPANUSB: https://github.com/statropy/wpanusb/commit/251f0167545bf2dcaa3cad991a59dbf5ab05490a BCFSERIAL: https://github.com/statropy/bcfserial/commit/aded88429a8a00143596b41f4c1f50d9ae3d4069 WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/sforshee/wireless-regdb.git/commit/?id=4dac44b3705239cd874205efc47fecef1fa25276
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -596,7 +596,7 @@ fixes
 packaging () {
 	#do_backport="enable"
 	if [ "x${do_backport}" = "xenable" ] ; then
-		backport_tag="v5.16.9"
+		backport_tag="v5.16.10"
 
 		subsystem="bindeb-pkg"
 		#regenerate="enable"
--- version.sh
+++ version.sh
@@ -38,10 +38,10 @@ toolchain="gcc_11_arm"
 
 #Kernel
 KERNEL_REL=5.16
-KERNEL_TAG=${KERNEL_REL}.9
+KERNEL_TAG=${KERNEL_REL}.10
 kernel_rt=".2-rt19"
 #Kernel Build
-BUILD=${build_prefix}13
+BUILD=${build_prefix}13.1
 
 #v5.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
