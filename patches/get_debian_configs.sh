847ec7495d6ed91b6725eff6c74fa7c3bea7e766
Robert Nelson <robertcnelson@gmail.com>
2022-07-11 13:47:49 -0500

    debian: 5.18.0-2
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patches/get_debian_configs.sh
+++ patches/get_debian_configs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-abi="5.15.0-trunk"
-kernel="5.15.2-1~exp1"
+abi="5.18.0-2"
+kernel="5.18.5-1"
 
 debian_site="http://ftp.de.debian.org/debian/pool/main/l/linux"
 incoming_site="http://incoming.debian.org/debian-buildd/pool/main/l/linux"
