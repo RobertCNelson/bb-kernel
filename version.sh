48f96b6e98be982befe13a24d0abbf778185d7d1
Robert Nelson <robertcnelson@gmail.com>
2022-08-11 14:07:52 -0500

    scripts: resync all build scripts
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -8,25 +8,33 @@ build_prefix="-bone"
 branch_prefix="am33x-v"
 branch_postfix=""
 
+#https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/tree/Documentation/process/changes.rst?h=v4.12-rc1
 #arm
 KERNEL_ARCH=arm
-#toolchain="gcc_linaro_eabi_4_8"
-#toolchain="gcc_linaro_eabi_4_9"
-#toolchain="gcc_linaro_eabi_5"
-#toolchain="gcc_linaro_eabi_6"
-#toolchain="gcc_linaro_eabi_7"
-#toolchain="gcc_linaro_gnueabi_4_6"
-#toolchain="gcc_linaro_gnueabihf_4_7"
-#toolchain="gcc_linaro_gnueabihf_4_8"
-#toolchain="gcc_linaro_gnueabihf_4_9"
-#toolchain="gcc_linaro_gnueabihf_5"
-toolchain="gcc_linaro_gnueabihf_6"
-#toolchain="gcc_linaro_gnueabihf_7"
+DEBARCH=armhf
+toolchain="gcc_6_arm"
+#toolchain="gcc_7_arm"
+#toolchain="gcc_8_arm"
+#toolchain="gcc_9_arm"
+#toolchain="gcc_10_arm"
+#toolchain="gcc_11_arm"
 #arm64
 #KERNEL_ARCH=arm64
-#toolchain="gcc_linaro_aarch64_gnu_5"
-#toolchain="gcc_linaro_aarch64_gnu_6"
-#toolchain="gcc_linaro_aarch64_gnu_7"
+#DEBARCH=arm64
+#toolchain="gcc_6_aarch64"
+#toolchain="gcc_7_aarch64"
+#toolchain="gcc_8_aarch64"
+#toolchain="gcc_9_aarch64"
+#toolchain="gcc_10_aarch64"
+#toolchain="gcc_11_aarch64"
+#riscv64
+#KERNEL_ARCH=riscv
+#DEBARCH=riscv64
+#toolchain="gcc_7_riscv64"
+#toolchain="gcc_8_riscv64"
+#toolchain="gcc_9_riscv64"
+#toolchain="gcc_10_riscv64"
+#toolchain="gcc_11_riscv64"
 
 #Kernel
 KERNEL_REL=4.12
@@ -42,6 +50,5 @@ BUILD=${build_prefix}4
 #git branch
 BRANCH="${branch_prefix}${KERNEL_REL}${branch_postfix}"
 
-DISTRO=cross
-DEBARCH=armhf
+DISTRO=xross
 #
