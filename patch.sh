d4071c9b87073966b5e6ef5f0752d82596a9564f
Robert Nelson <robertcnelson@gmail.com>
2022-11-08 09:57:34 -0600

    scripts: resync all build scripts
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -610,3 +610,4 @@ packaging () {
 
 packaging
 echo "patch.sh ran successfully"
+#
--- repo_maintenance/push-bb.org.sh
+++ repo_maintenance/push-bb.org.sh
@@ -53,12 +53,17 @@ cat_files () {
 	if [ -f ../patches/git/WIRELESS_REGDB ] ; then
 		cat ../patches/git/WIRELESS_REGDB >> ${wfile}
 	fi
+
+	if [ -f ../patches/git/KSMBD ] ; then
+		cat ../patches/git/KSMBD >> ${wfile}
+	fi
 }
 
 DIR=$PWD
 git_bin=$(which git)
 
-repo="git@github.com:beagleboard/linux.git"
+repo_github="git@github.com:beagleboard/linux.git"
+repo_gitlab="git@git.beagleboard.org:beagleboard/linux.git"
 example="bb.org"
 compare="https://github.com/RobertCNelson/ti-linux-kernel/compare"
 
@@ -73,15 +78,18 @@ if [ -e ${DIR}/version.sh ]; then
 	make ARCH=${KERNEL_ARCH} savedefconfig
 	cp ${DIR}/KERNEL/defconfig ${DIR}/KERNEL/arch/${KERNEL_ARCH}/configs/${example}_defconfig
 	${git_bin} add arch/${KERNEL_ARCH}/configs/${example}_defconfig
+	#${git_bin} add arch/${KERNEL_ARCH}/configs/ti_sdk_am3x_release_defconfig
+	#${git_bin} add arch/${KERNEL_ARCH}/configs/ti_sdk_dra7x_release_defconfig
+	#${git_bin} add arch/${KERNEL_ARCH}/configs/ti_sdk_arm64_release_defconfig
 
-	if [ "x${ti_git_old_release}" = "x${ti_git_post}" ] ; then
+	if [ "x${ti_git_old_release}" = "x${ti_git_new_release}" ] ; then
 		echo "${KERNEL_TAG}${BUILD}" > ${wfile}
 		echo "${KERNEL_TAG}${BUILD} ${example}_defconfig" >> ${wfile}
 		cat_files
 	else
 		echo "${KERNEL_TAG}${BUILD}" > ${wfile}
 		echo "${KERNEL_TAG}${BUILD} ${example}_defconfig" >> ${wfile}
-		echo "${KERNEL_REL} TI Delta: ${compare}/${ti_git_old_release}...${ti_git_post}" >> ${wfile}
+		echo "${KERNEL_REL} TI Delta: ${compare}/${ti_git_old_release}...${ti_git_new_release}" >> ${wfile}
 		cat_files
 	fi
 	${git_bin} commit -a -F ${wfile} -s
@@ -89,7 +97,13 @@ if [ -e ${DIR}/version.sh ]; then
 	${git_bin} tag -a "${KERNEL_TAG}${BUILD}" -F ${wfile} -f
 
 	#push tag
-	${git_bin} push -f ${repo} "${KERNEL_TAG}${BUILD}"
+	echo "log: git: pushing tags..."
+
+	echo "log: git push -f ${repo_github} ${KERNEL_TAG}${BUILD}"
+	${git_bin} push -f ${repo_github} "${KERNEL_TAG}${BUILD}"
+
+	echo "log: git push -f ${repo_gitlab} ${KERNEL_TAG}${BUILD}"
+	${git_bin} push -f ${repo_gitlab} "${KERNEL_TAG}${BUILD}"
 
 	echo "debug: pushing ${bborg_branch}"
 
@@ -98,8 +112,13 @@ if [ -e ${DIR}/version.sh ]; then
 	${git_bin} branch -m v${KERNEL_TAG}${BUILD} ${bborg_branch}
 
 	#push branch
-	echo "log: git push -f ${repo} ${bborg_branch}"
-	${git_bin} push -f ${repo} ${bborg_branch}
+	echo "log: git: pushing branch..."
+
+	echo "log: git push -f ${repo_github} ${bborg_branch}"
+	${git_bin} push -f ${repo_github} ${bborg_branch}
+
+	echo "log: git push -f ${repo_gitlab} ${bborg_branch}"
+	${git_bin} push -f ${repo_gitlab} ${bborg_branch}
 
 	cd ${DIR}/
 fi
