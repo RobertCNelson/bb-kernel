0e31aef98ac5b6b1c0b61142c7e2aee17a860017
Robert Nelson <robertcnelson@gmail.com>
2022-12-27 09:40:42 -0600

    bump debian reference
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patches/get_debian_configs.sh
+++ patches/get_debian_configs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 abi="6.1.0-0"
-kernel="6.1.1-1~exp1"
+kernel="6.1.1-1~exp2"
 
 debian_site="http://ftp.de.debian.org/debian/pool/main/l/linux"
 incoming_site="http://incoming.debian.org/debian-buildd/pool/main/l/linux"
