4bf2cc9ee1efc414e844f7d8c671072c10bde606
Robert Nelson <robertcnelson@gmail.com>
2022-12-29 13:16:14 -0600

    scripts: resync all build scripts
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -2,21 +2,14 @@
 #
 ARCH=$(uname -m)
 
-if [ $(which nproc) ] ; then
-	CORES=$(nproc)
-else
-	CORES=1
-fi
-
-#Debian 7 (Wheezy): git version 1.7.10.4 and later needs "--no-edit"
-unset git_opts
-git_no_edit=$(LC_ALL=C git help pull | grep -m 1 -e "--no-edit" || true)
-if [ ! "x${git_no_edit}" = "x" ] ; then
-	git_opts="--no-edit"
-fi
-
 config="omap2plus_defconfig"
 
+build_prefix="-bone"
+branch_prefix="am33x-v"
+branch_postfix=""
+
+#arm
+KERNEL_ARCH=arm
 #toolchain="gcc_linaro_eabi_4_8"
 #toolchain="gcc_linaro_eabi_4_9"
 #toolchain="gcc_linaro_gnueabi_4_6"
@@ -24,16 +17,17 @@ config="omap2plus_defconfig"
 #toolchain="gcc_linaro_gnueabihf_4_8"
 toolchain="gcc_linaro_gnueabihf_4_9"
 
-#Kernel/Build
+#Kernel
 KERNEL_REL=3.17
 KERNEL_TAG=${KERNEL_REL}.2
-BUILD=bone5
+BUILD=${build_prefix}5
 
-#v3.X-rcX + upto SHA
+#v4.X-rcX + upto SHA
+#prev_KERNEL_SHA=""
 #KERNEL_SHA=""
 
 #git branch
-BRANCH="am33x-v3.17"
+BRANCH="${branch_prefix}${KERNEL_REL}${branch_postfix}"
 
 DISTRO=cross
 DEBARCH=armhf
