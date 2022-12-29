33287c8747ccb59f920fc524ab3b02ebf554fe08
Robert Nelson <robertcnelson@gmail.com>
2022-12-29 12:56:23 -0600

    scripts: resync all build scripts
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -4,24 +4,38 @@ ARCH=$(uname -m)
 
 config="omap2plus_defconfig"
 
+build_prefix="-bone"
+branch_prefix="am33x-v"
+branch_postfix=""
+
+#arm
+KERNEL_ARCH=arm
 #toolchain="gcc_linaro_eabi_4_8"
 #toolchain="gcc_linaro_eabi_4_9"
+#toolchain="gcc_linaro_eabi_5"
+#toolchain="gcc_linaro_eabi_6"
 #toolchain="gcc_linaro_gnueabi_4_6"
 #toolchain="gcc_linaro_gnueabihf_4_7"
 #toolchain="gcc_linaro_gnueabihf_4_8"
 toolchain="gcc_linaro_gnueabihf_4_9"
+#toolchain="gcc_linaro_gnueabihf_5"
+#toolchain="gcc_linaro_gnueabihf_6"
+#arm64
+#KERNEL_ARCH=arm64
+#toolchain="gcc_linaro_aarch64_gnu_5"
+#toolchain="gcc_linaro_aarch64_gnu_6"
 
-#Kernel/Build
+#Kernel
 KERNEL_REL=3.18
 KERNEL_TAG=${KERNEL_REL}.5
-BUILD=bone1
+BUILD=${build_prefix}1
 
-#v3.X-rcX + upto SHA
+#v4.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
 #KERNEL_SHA=""
 
 #git branch
-BRANCH="am33x-v3.18"
+BRANCH="${branch_prefix}${KERNEL_REL}${branch_postfix}"
 
 DISTRO=cross
 DEBARCH=armhf
