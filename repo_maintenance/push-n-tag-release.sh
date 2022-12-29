062091747e3d96377b00b78f53a0b4342c50d48b
Robert Nelson <robertcnelson@gmail.com>
2022-12-29 15:50:07 -0600

    scripts: resync all build scripts
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- repo_maintenance/push-n-tag-release.sh
+++ repo_maintenance/push-n-tag-release.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 #
-# Copyright (c) 2009-2017 Robert Nelson <robertcnelson@gmail.com>
+# Copyright (c) 2009-2019 Robert Nelson <robertcnelson@gmail.com>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -22,17 +22,65 @@
 
 #yeah, i'm getting lazy..
 
+wfile=$(mktemp /tmp/builder.XXXXXXXXX)
+echo "Working on temp $wfile ..."
+
+cat_files () {
+	if [ -f ./patches/git/AUFS ] ; then
+		cat ./patches/git/AUFS >> ${wfile}
+	fi
+
+	if [ -f ./patches/git/BBDTBS ] ; then
+		cat ./patches/git/BBDTBS >> ${wfile}
+	fi
+
+	if [ -f ./patches/git/CAN-ISOTP ] ; then
+		cat ./patches/git/CAN-ISOTP >> ${wfile}
+	fi
+
+	if [ -f ./patches/git/RT ] ; then
+		cat ./patches/git/RT >> ${wfile}
+	fi
+
+	if [ -f ./patches/git/TI_AMX3_CM3 ] ; then
+		cat ./patches/git/TI_AMX3_CM3 >> ${wfile}
+	fi
+
+	if [ -f ./patches/git/WIREGUARD ] ; then
+		cat ./patches/git/WIREGUARD >> ${wfile}
+	fi
+
+	if [ -f ./patches/git/WPANUSB ] ; then
+		cat ./patches/git/WPANUSB >> ${wfile}
+	fi
+
+	if [ -f ./patches/git/BCFSERIAL ] ; then
+		cat ./patches/git/BCFSERIAL >> ${wfile}
+	fi
+
+	if [ -f ./patches/git/WIRELESS_REGDB ] ; then
+		cat ./patches/git/WIRELESS_REGDB >> ${wfile}
+	fi
+
+	if [ -f ./patches/git/KSMBD ] ; then
+		cat ./patches/git/KSMBD >> ${wfile}
+	fi
+}
+
 DIR=$PWD
 git_bin=$(which git)
 
-repo="git@github.com:RobertCNelson/linux-stable-rcn-ee.git"
+repo="git@gitlab.gfnd.rcn-ee.org:production/linux-stable-rcn-ee.git"
 example="rcn-ee"
 
 if [ -e ${DIR}/version.sh ]; then
 	unset BRANCH
 	. ${DIR}/version.sh
 
-	${git_bin} commit -a -m "${KERNEL_TAG}${BUILD} release" -s
+	echo "${KERNEL_TAG}${BUILD} release" > ${wfile}
+	cat_files
+
+	${git_bin} commit -a -F ${wfile} -s
 	${git_bin} tag -a "${KERNEL_TAG}${BUILD}" -m "${KERNEL_TAG}${BUILD}" -f
 
 	${git_bin} push -f origin ${BRANCH}
@@ -46,7 +94,10 @@ if [ -e ${DIR}/version.sh ]; then
 	cp ${DIR}/KERNEL/defconfig ${DIR}/KERNEL/arch/${KERNEL_ARCH}/configs/${example}_defconfig
 	${git_bin} add arch/${KERNEL_ARCH}/configs/${example}_defconfig
 
-	${git_bin} commit -a -m "${KERNEL_TAG}${BUILD} ${example}_defconfig" -s
+	echo "${KERNEL_TAG}${BUILD} ${example}_defconfig" > ${wfile}
+	cat_files
+
+	${git_bin} commit -a -F ${wfile} -s
 	${git_bin} tag -a "${KERNEL_TAG}${BUILD}" -m "${KERNEL_TAG}${BUILD}" -f
 
 	#push tag
@@ -56,3 +107,5 @@ if [ -e ${DIR}/version.sh ]; then
 	cd ${DIR}/
 fi
 
+echo "Deleting $wfile ..."
+rm -f "$wfile"
