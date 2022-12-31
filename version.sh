e494c6df2efb95367dd7175606bef9430595a37b
Robert Nelson <robertcnelson@gmail.com>
2022-12-31 11:41:16 -0600

    scripts: resync all build scripts
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -2,19 +2,6 @@
 #
 ARCH=$(uname -m)
 
-if [ $(which nproc) ] ; then
-	CORES=$(nproc)
-else
-	CORES=1
-fi
-
-#Debian 7 (Wheezy): git version 1.7.10.4 and later needs "--no-edit"
-unset git_opts
-git_no_edit=$(LC_ALL=C git help pull | grep -m 1 -e "--no-edit" || true)
-if [ ! "x${git_no_edit}" = "x" ] ; then
-	git_opts="--no-edit"
-fi
-
 config="omap2plus_defconfig"
 
 build_prefix="-bone"
@@ -25,17 +12,26 @@ branch_postfix=""
 KERNEL_ARCH=arm
 #toolchain="gcc_linaro_eabi_4_8"
 #toolchain="gcc_linaro_eabi_4_9"
+#toolchain="gcc_linaro_eabi_5"
+#toolchain="gcc_linaro_eabi_6"
 #toolchain="gcc_linaro_gnueabi_4_6"
 #toolchain="gcc_linaro_gnueabihf_4_7"
 #toolchain="gcc_linaro_gnueabihf_4_8"
 toolchain="gcc_linaro_gnueabihf_4_9"
+#toolchain="gcc_linaro_gnueabihf_5"
+#toolchain="gcc_linaro_gnueabihf_6"
+#arm64
+#KERNEL_ARCH=arm64
+#toolchain="gcc_linaro_aarch64_gnu_5"
+#toolchain="gcc_linaro_aarch64_gnu_6"
 
 #Kernel
 KERNEL_REL=3.16
 KERNEL_TAG=${KERNEL_REL}.3
 BUILD=${build_prefix}6
 
-#v3.X-rcX + upto SHA
+#v4.X-rcX + upto SHA
+#prev_KERNEL_SHA=""
 #KERNEL_SHA=""
 
 #git branch
