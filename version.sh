07ddbbf872f42814828c4bc40c5938def6536122
Robert Nelson <robertcnelson@gmail.com>
2023-01-02 11:50:38 -0600

    scripts: resync all build scripts
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -2,19 +2,6 @@
 #
 ARCH=$(uname -m)
 
-if [ $(which nproc) ] ; then
-	CORES=$(nproc)
-else
-	CORES=1
-fi
-
-#Debian 7 (Wheezy): git version 1.7.10.4 and later needs "--no-edit"
-unset git_opts
-git_no_edit=$(LC_ALL=C git help pull | grep -m 1 -e "--no-edit" || true)
-if [ ! "x${git_no_edit}" = "x" ] ; then
-	git_opts="--no-edit"
-fi
-
 config="omap2plus_defconfig"
 
 build_prefix="-bone"
@@ -29,8 +16,8 @@ KERNEL_ARCH=arm
 #toolchain="gcc_linaro_eabi_6"
 #toolchain="gcc_linaro_gnueabi_4_6"
 #toolchain="gcc_linaro_gnueabihf_4_7"
-toolchain="gcc_linaro_gnueabihf_4_8"
-#toolchain="gcc_linaro_gnueabihf_4_9"
+#toolchain="gcc_linaro_gnueabihf_4_8"
+toolchain="gcc_linaro_gnueabihf_4_9"
 #toolchain="gcc_linaro_gnueabihf_5"
 #toolchain="gcc_linaro_gnueabihf_6"
 #arm64
