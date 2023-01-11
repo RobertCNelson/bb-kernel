973b4e2e6aeb93555e8dd8940e0a5ae682172f6f
Robert Nelson <robertcnelson@gmail.com>
2023-01-11 15:24:37 -0600

    kernel v5.15.86 rebase with: device-tree/etc AUFS: https://github.com/sfjro/aufs-standalone/commit/098702c45259a632b3879f67ba39204edc76d1c6 BBDTBS: https://git.beagleboard.org/beagleboard/BeagleBoard-DeviceTrees/-/commit/bbb697ae926113ca66c5b9039a3134948ce4a86e TI_AMX3_CM3: http://git.ti.com/gitweb/?p=processor-firmware/ti-amx3-cm3-pm-firmware.git;a=commit;h=fb484c5e54f2e31cf0a338d2927a06a2870bcc2c WPANUSB: https://git.beagleboard.org/beagleconnect/linux/wpanusb/-/commit/6aa9bf65b9d88a2c9a111e7b4aed03de2be9413d BCFSERIAL: https://git.beagleboard.org/beagleconnect/linux/bcfserial/-/commit/db467023bd136c97c2e13c3a8b9e41dbdfafbc66 WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/sforshee/wireless-regdb.git/commit/?id=fe05cc94e070c71486047002fc2edf68f5f2a07a
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -41,10 +41,10 @@ toolchain="gcc_11_arm"
 
 #Kernel
 KERNEL_REL=5.15
-KERNEL_TAG=${KERNEL_REL}.73
-kernel_rt=".73-rt52"
+KERNEL_TAG=${KERNEL_REL}.86
+kernel_rt=".86-rt56"
 #Kernel Build
-BUILD=${build_prefix}31
+BUILD=${build_prefix}31.1
 
 #v6.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
