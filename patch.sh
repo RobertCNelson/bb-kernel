a8f98dbdcb72521d37ed3e1438575642234ee601
Robert Nelson <robertcnelson@gmail.com>
2023-01-31 14:21:46 -0600

    kernel v5.15.86 rebase with: device-tree/etc AUFS: https://github.com/sfjro/aufs-standalone/commit/098702c45259a632b3879f67ba39204edc76d1c6 BBDTBS: https://git.beagleboard.org/beagleboard/BeagleBoard-DeviceTrees/-/commit/6ae8b01c8b5936a896341b479304bdd330f6eaf7 TI_AMX3_CM3: http://git.ti.com/gitweb/?p=processor-firmware/ti-amx3-cm3-pm-firmware.git;a=commit;h=fb484c5e54f2e31cf0a338d2927a06a2870bcc2c WPANUSB: https://git.beagleboard.org/beagleconnect/linux/wpanusb/-/commit/6aa9bf65b9d88a2c9a111e7b4aed03de2be9413d BCFSERIAL: https://git.beagleboard.org/beagleconnect/linux/bcfserial/-/commit/db467023bd136c97c2e13c3a8b9e41dbdfafbc66 WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/sforshee/wireless-regdb.git/commit/?id=fe05cc94e070c71486047002fc2edf68f5f2a07a
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -489,7 +489,7 @@ patch_backports (){
 }
 
 backports () {
-	backport_tag="v5.10.162"
+	backport_tag="v5.10.165"
 
 	subsystem="uio"
 	#regenerate="enable"
@@ -505,7 +505,7 @@ backports () {
 		dir 'drivers/ti/uio'
 	fi
 
-	backport_tag="v5.15.86"
+	backport_tag="v5.15.90"
 
 	subsystem="iio"
 	#regenerate="enable"
