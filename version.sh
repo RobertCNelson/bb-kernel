055f88313198474417a748720c8cf13180e4bb71
Robert Nelson <robertcnelson@gmail.com>
2023-05-11 14:37:03 -0500

    kernel v6.3.2-bone4.1 rebase external git projects BBDTBS: https://git.beagleboard.org/beagleboard/BeagleBoard-DeviceTrees/-/commit/17a9f0fd6c12cd4d1eccc4fb837c343995f30baf AUFS: https://github.com/sfjro/aufs-standalone/commit/7ce1f59feeab7cea9ac1ff1e2edebfe7040bbb3b WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/sforshee/wireless-regdb.git/commit/?id=43f81b4d0819ae1f9f6e31af7f563e9135aec9b6 TI_AMX3_CM3: http://git.ti.com/gitweb/?p=processor-firmware/ti-amx3-cm3-pm-firmware.git;a=commit;h=fb484c5e54f2e31cf0a338d2927a06a2870bcc2c
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -41,10 +41,10 @@ toolchain="gcc_12_arm"
 
 #Kernel
 KERNEL_REL=6.3
-KERNEL_TAG=${KERNEL_REL}.1
-kernel_rt="-rt11"
+KERNEL_TAG=${KERNEL_REL}.2
+kernel_rt=".1-rt13"
 #Kernel Build
-BUILD=${build_prefix}4
+BUILD=${build_prefix}4.1
 
 #v6.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
