fa618c0f322305c5fa840a10f6d49716b256fd10
Robert Nelson <robertcnelson@gmail.com>
2023-05-12 14:22:59 -0500

    4.19.280-bone-rt-r76 release BBDTBS: https://git.beagleboard.org/beagleboard/BeagleBoard-DeviceTrees/-/commit/a34d721dc050dbd882ad08c045f5c2443ce9b5d5 RT: patch-4.19.280-rt123.patch.xz AUFS: https://github.com/sfjro/aufs4-standalone/commit/1bb4caf8d7ee47acc49f05deb4da9d34884c44b2 WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/sforshee/wireless-regdb.git/commit/?id=43f81b4d0819ae1f9f6e31af7f563e9135aec9b6 TI_AMX3_CM3: http://git.ti.com/gitweb/?p=processor-firmware/ti-amx3-cm3-pm-firmware.git;a=commit;h=fb484c5e54f2e31cf0a338d2927a06a2870bcc2c CAN-ISOTP: https://github.com/hartkopp/can-isotp/commit/7626d0a0707391970080d493ce69638719938da7
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -44,7 +44,7 @@ KERNEL_REL=4.19
 KERNEL_TAG=${KERNEL_REL}.280
 kernel_rt=".280-rt123"
 #Kernel Build
-BUILD=${build_prefix}75.5
+BUILD=${build_prefix}76
 
 #v6.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
