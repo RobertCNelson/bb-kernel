3c40c617cce1459a5e2dfc3062372aa7583def4c
Robert Nelson <robertcnelson@gmail.com>
2023-05-31 11:10:19 -0500

    debian: 6.1.27-1
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patches/get_debian_configs.sh
+++ patches/get_debian_configs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-abi="6.1.0-0"
-kernel="6.1.2-1~exp1"
+abi="6.1.0-9"
+kernel="6.1.27-1"
 
 debian_site="http://ftp.de.debian.org/debian/pool/main/l/linux"
 incoming_site="http://incoming.debian.org/debian-buildd/pool/main/l/linux"
