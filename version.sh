f71ef71dcb45c3ea3afa0767d5a75520eaca4150
Robert Nelson <robertcnelson@gmail.com>
2023-07-14 17:55:15 -0500

    config: force CONFIG_MUSB_PIO_ONLY
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -47,7 +47,7 @@ KERNEL_REL=5.15
 KERNEL_TAG=${KERNEL_REL}.119
 kernel_rt=".119-rt65"
 #Kernel Build
-BUILD=${build_prefix}38.1
+BUILD=${build_prefix}38.2
 
 #v6.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
