8cfe2ae78770ad89fb5db0fe64157eb797e53705
Robert Nelson <robertcnelson@gmail.com>
2023-07-14 17:57:22 -0500

    config: force CONFIG_MUSB_PIO_ONLY
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/config-hot-fix.sh
+++ tools/config-hot-fix.sh
@@ -155,9 +155,21 @@ config="CONFIG_W1_SLAVE_DS2433_CRC" ; config_enable
 config="CONFIG_UIO_PDRV_GENIRQ" ; config_module
 config="CONFIG_UIO_PRUSS" ; config_module
 
+#2022.12.25: still totally broken..
+#[   26.460634] tps65217-charger tps65217-charger: DMA mask not set
+#[   26.581296] genirq: Flags mismatch irq 53. 00002000 (tps65217-charger) vs. 00002000 (vbus)
+#[   26.739119] tps65217-charger tps65217-charger: Unable to register irq 53 err -16
+#[   26.842097] tps65217-charger: probe of tps65217-charger failed with error -16
+config="CONFIG_CHARGER_TPS65217" ; config_disable
+
 #2023.07.10
 config="CONFIG_KERNEL_LZO" ; config_disable
 config="CONFIG_KERNEL_LZ4" ; config_enable
 config="CONFIG_GCC_PLUGINS" ; config_disable
+
+#2023.07.14
+# MUSB DMA mode
+config="CONFIG_MUSB_PIO_ONLY" ; config_enable
+config="CONFIG_USB_TI_CPPI41_DMA" ; config_disable
 
 cd ${DIR}/
