999620b7ef66c045ed77338ed8239f054ecf5f0f
Robert Nelson <robertcnelson@gmail.com>
2023-11-18 13:14:03 -0600

    scripts: resync all build scripts
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -18,6 +18,8 @@ DEBARCH=armhf
 toolchain="gcc_9_arm"
 #toolchain="gcc_10_arm"
 #toolchain="gcc_11_arm"
+#toolchain="gcc_12_arm"
+#toolchain="gcc_13_arm"
 #arm64
 #KERNEL_ARCH=arm64
 #DEBARCH=arm64
@@ -27,6 +29,8 @@ toolchain="gcc_9_arm"
 #toolchain="gcc_9_aarch64"
 #toolchain="gcc_10_aarch64"
 #toolchain="gcc_11_aarch64"
+#toolchain="gcc_12_aarch64"
+#toolchain="gcc_13_aarch64"
 #riscv64
 #KERNEL_ARCH=riscv
 #DEBARCH=riscv64
@@ -35,15 +39,17 @@ toolchain="gcc_9_arm"
 #toolchain="gcc_9_riscv64"
 #toolchain="gcc_10_riscv64"
 #toolchain="gcc_11_riscv64"
+#toolchain="gcc_12_riscv64"
+#toolchain="gcc_13_riscv64"
 
 #Kernel
 KERNEL_REL=5.6
 KERNEL_TAG=${KERNEL_REL}.19
 kernel_rt=".X-rtY"
 #Kernel Build
-BUILD=${build_prefix}27
+BUILD=${build_prefix}27.1
 
-#v5.X-rcX + upto SHA
+#v6.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
 #KERNEL_SHA=""
 
