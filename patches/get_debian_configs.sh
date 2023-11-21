37ae1821431e8ba920984679372518d442caf87e
Robert Nelson <robertcnelson@gmail.com>
2023-11-21 14:15:26 -0600

    drop: aufs
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patches/get_debian_configs.sh
+++ patches/get_debian_configs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-abi="6.1.0-1"
-kernel="6.1.4-1"
+abi="6.1.0-13"
+kernel="6.1.55-1"
 
 debian_site="http://ftp.de.debian.org/debian/pool/main/l/linux"
 incoming_site="http://incoming.debian.org/debian-buildd/pool/main/l/linux"
@@ -22,11 +22,6 @@ dl_deb () {
 
 dpkg_arch="armhf"
 image="armmp"
-unsigned=""
-dl_deb
-
-dpkg_arch="armhf"
-image="armmp-lpae"
 unsigned=""
 dl_deb
 
