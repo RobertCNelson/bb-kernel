77a2c5b05676aa90944e745e4894ca15ff2d6c07
Robert Nelson <robertcnelson@gmail.com>
2023-12-11 18:28:40 -0600

    kernel v6.7-rc5-bone4.1 rebase external git projects BBDTBS: https://git.beagleboard.org/beagleboard/BeagleBoard-DeviceTrees/-/commit/dee2e608f1109d5298851ecf54fa865a2152f49b WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/sforshee/wireless-regdb.git/commit/?id=37dcea0e6e5effb4228fe385e906edba3cbee389 TI_AMX3_CM3: http://git.ti.com/gitweb/?p=processor-firmware/ti-amx3-cm3-pm-firmware.git;a=commit;h=fb484c5e54f2e31cf0a338d2927a06a2870bcc2c
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -323,7 +323,7 @@ patch_backports () {
 }
 
 backports () {
-	backport_tag="v5.10.202"
+	backport_tag="v5.10.203"
 
 	subsystem="uio"
 	#regenerate="enable"
@@ -352,7 +352,7 @@ packaging () {
 	echo "Update: package scripts"
 	#do_backport="enable"
 	if [ "x${do_backport}" = "xenable" ] ; then
-		backport_tag="v6.6.4"
+		backport_tag="v6.6.6"
 
 		subsystem="bindeb-pkg"
 		#regenerate="enable"
