2e31ceab308d8f8256dd65f74499bae4ad94e7bc
Robert Nelson <robertcnelson@gmail.com>
2023-12-16 14:37:29 -0600

    scripts: resync all build scripts
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patches/get_debian_configs.sh
+++ patches/get_debian_configs.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-abi="6.1.0-13"
-kernel="6.1.55-1"
+#
+abi="6.1.0-16"
+kernel="6.1.67-1"
+#
 
-debian_site="http://ftp.de.debian.org/debian/pool/main/l/linux"
+debian_site="http://ftp.us.debian.org/debian/pool/main/l/linux"
 incoming_site="http://incoming.debian.org/debian-buildd/pool/main/l/linux"
 
 dl_deb () {
@@ -15,7 +17,7 @@ dl_deb () {
 
 	if [ -f ./dl/linux-image-${abi}-${image}${unsigned}_${kernel}_${dpkg_arch}.deb ] ; then
 		dpkg -x ./dl/linux-image-${abi}-${image}${unsigned}_${kernel}_${dpkg_arch}.deb ./dl/tmp/
-		cp -v ./dl/tmp/boot/config-${abi}-${image} ./debian-${image}
+		cp -v ./dl/tmp/boot/config-${abi}-${image} ./debian
 		rm -rf ./dl/tmp/ || true
 	fi
 }
