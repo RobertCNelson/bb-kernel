584c8f737092893e515654b4fba0bc49c6ed56a6
Robert Nelson <robertcnelson@gmail.com>
2024-03-21 14:48:01 -0500

    5.10.212-bone-rt-r78 release BBDTBS: https://openbeagle.org/beagleboard/BeagleBoard-DeviceTrees/-/commit/c6fe9f89aab61626a7f0128bcce9c41a74ade37c RT: patch-5.10.212-rt104.patch.xz WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/wens/wireless-regdb.git/commit/?id=454130065a5857bc0c534d5c52ed17a97b704c36 TI_AMX3_CM3: http://git.ti.com/gitweb/?p=processor-firmware/ti-amx3-cm3-pm-firmware.git;a=commit;h=fb484c5e54f2e31cf0a338d2927a06a2870bcc2c WPANUSB: https://openbeagle.org/beagleconnect/linux/wpanusb/-/commit/6aa9bf65b9d88a2c9a111e7b4aed03de2be9413d BCFSERIAL: https://openbeagle.org/beagleconnect/linux/bcfserial/-/commit/db467023bd136c97c2e13c3a8b9e41dbdfafbc66
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -47,7 +47,7 @@ KERNEL_REL=5.10
 KERNEL_TAG=${KERNEL_REL}.212
 kernel_rt=".212-rt104"
 #Kernel Build
-BUILD=${build_prefix}77.1
+BUILD=${build_prefix}78
 
 #v6.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
