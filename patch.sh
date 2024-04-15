d948d6cf2eaf9c1de6c730881067351a63b5c507
Robert Nelson <robertcnelson@gmail.com>
2024-04-15 13:19:44 -0500

    kernel v6.9-rc4-bone3.1 rebase external git projects BBDTBS: https://openbeagle.org/beagleboard/BeagleBoard-DeviceTrees/-/commit/d2d84d70bbdb7b111c69c5f068a4eaa82969ffcc WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/wens/wireless-regdb.git/commit/?id=73529a882f83afa26402b18680025334b0215d92 TI_AMX3_CM3: http://git.ti.com/gitweb/?p=processor-firmware/ti-amx3-cm3-pm-firmware.git;a=commit;h=fb484c5e54f2e31cf0a338d2927a06a2870bcc2c WPANUSB: https://openbeagle.org/beagleconnect/linux/wpanusb/-/commit/6aa9bf65b9d88a2c9a111e7b4aed03de2be9413d
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -469,7 +469,7 @@ patch_backports () {
 }
 
 backports () {
-	backport_tag="v5.10.214"
+	backport_tag="v5.10.215"
 
 	subsystem="uio"
 	#regenerate="enable"
@@ -513,7 +513,7 @@ packaging () {
 	echo "Update: package scripts"
 	#do_backport="enable"
 	if [ "x${do_backport}" = "xenable" ] ; then
-		backport_tag="v6.8.1"
+		backport_tag="v6.8.6"
 
 		subsystem="bindeb-pkg"
 		#regenerate="enable"
--- version.sh
+++ version.sh
@@ -44,10 +44,10 @@ toolchain="gcc_13_arm"
 
 #Kernel
 KERNEL_REL=6.9
-KERNEL_TAG=${KERNEL_REL}-rc2
+KERNEL_TAG=${KERNEL_REL}-rc4
 kernel_rt="-rt7"
 #Kernel Build
-BUILD=${build_prefix}3
+BUILD=${build_prefix}3.1
 
 #v6.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
