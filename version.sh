a7760a93f47cc1998871d0accfc02f1a1b44a5d3
Robert Nelson <robertcnelson@gmail.com>
2024-07-01 16:32:04 -0500

    kernel v6.8.12-bone14.1 rebase external git projects BBDTBS: https://openbeagle.org/beagleboard/BeagleBoard-DeviceTrees/-/commit/8c4a59291581e6177b9ffc031db805c1a2a140ba WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/wens/wireless-regdb.git/commit/?id=87941e40f422dc3deaa7f1e4019a1b226d1f11f7 WPANUSB: https://openbeagle.org/beagleconnect/linux/wpanusb/-/commit/6aa9bf65b9d88a2c9a111e7b4aed03de2be9413d
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -47,10 +47,10 @@ toolchain="gcc_13_arm"
 
 #Kernel
 KERNEL_REL=6.8
-KERNEL_TAG=${KERNEL_REL}.8
+KERNEL_TAG=${KERNEL_REL}.12
 kernel_rt=".2-rt11"
 #Kernel Build
-BUILD=${build_prefix}14
+BUILD=${build_prefix}14.1
 
 #v6.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
