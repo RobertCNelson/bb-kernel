2526c6d88ddcdbc10e541fcade7601fbd26d330c
Robert Nelson <robertcnelson@gmail.com>
2024-08-30 10:30:23 -0500

    bump rt
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -52,7 +52,7 @@ linux_stable_repo="https://kernel.googlesource.com/pub/scm/linux/kernel/git/stab
 KERNEL_REL=6.11
 KERNEL_TAG=${KERNEL_REL}-rc5
 #https://mirrors.edge.kernel.org/pub/linux/kernel/projects/rt/6.11/
-kernel_rt="-rc3-rt3"
+kernel_rt="-rc5-rt5"
 #Kernel Build
 BUILD=${build_prefix}3
 
