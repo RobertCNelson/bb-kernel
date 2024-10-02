0450a5c4be00682bfa98238c9556648ef5104db6
Robert Nelson <robertcnelson@gmail.com>
2024-10-02 17:07:11 -0500

    6.10.12-bone12 release BBDTBS: https://openbeagle.org/beagleboard/BeagleBoard-DeviceTrees/-/commit/2cb131b69adea3899f71aeef2a3c0eef1f92f7a5 WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/wens/wireless-regdb.git/commit/?id=8e3d27cc7d6fb99f8f1a1b38ea5b97bfa81aa179 WPANUSB: https://openbeagle.org/beagleconnect/linux/wpanusb/-/commit/6aa9bf65b9d88a2c9a111e7b4aed03de2be9413d
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -54,7 +54,7 @@ KERNEL_TAG=${KERNEL_REL}.12
 #https://mirrors.edge.kernel.org/pub/linux/kernel/projects/rt/6.10/
 kernel_rt=".2-rt14"
 #Kernel Build
-BUILD=${build_prefix}11.1
+BUILD=${build_prefix}12
 
 #v6.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
