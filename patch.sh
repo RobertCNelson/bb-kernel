86819480d8271ef9abfceff7a6b40dff2c068f21
Robert Nelson <robertcnelson@gmail.com>
2024-10-03 11:46:47 -0500

    kernel v6.6.52-bone18.1 rebase external git projects BBDTBS: https://openbeagle.org/beagleboard/BeagleBoard-DeviceTrees/-/commit/b4236622bbfb540b988de10488207913ae7452e5 WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/wens/wireless-regdb.git/commit/?id=8e3d27cc7d6fb99f8f1a1b38ea5b97bfa81aa179 WPANUSB: https://openbeagle.org/beagleconnect/linux/wpanusb/-/commit/6aa9bf65b9d88a2c9a111e7b4aed03de2be9413d
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -240,9 +240,9 @@ arm_dtb_makefile_append () {
 }
 
 arm_dtbo_makefile_append () {
-	if [ -f ../${work_dir}/src/arm/overlays/${device}.dts ] ; then
+	if [ -f ../${work_dir}/src/arm/overlays/${device}.dtso ] ; then
 		sed -i -e 's:am335x-boneblack.dtb \\:am335x-boneblack.dtb \\\n\t'$device'.dtbo \\:g' arch/arm/boot/dts/ti/omap/Makefile
-		cp -v ../${work_dir}/src/arm/overlays/${device}.dts arch/arm/boot/dts/ti/omap/${device}.dtso
+		cp -v ../${work_dir}/src/arm/overlays/${device}.dtso arch/arm/boot/dts/ti/omap/${device}.dtso
 	else
 		echo "Missing [${device}]"
 	fi
@@ -253,9 +253,9 @@ k3_dtb_makefile_append () {
 }
 
 k3_dtbo_makefile_append () {
-	if [ -f ../${work_dir}/src/arm64/overlays/${device}.dts ] ; then
+	if [ -f ../${work_dir}/src/arm64/overlays/${device}.dtso ] ; then
 		echo "dtb-\$(CONFIG_ARCH_K3) += $device.dtbo" >> arch/arm64/boot/dts/ti/Makefile
-		cp -v ../${work_dir}/src/arm64/overlays/${device}.dts arch/arm64/boot/dts/ti/${device}.dtso
+		cp -v ../${work_dir}/src/arm64/overlays/${device}.dtso arch/arm64/boot/dts/ti/${device}.dtso
 		sed -i -e 's:ti/k3-:k3-:g' arch/arm64/boot/dts/ti/${device}.dtso
 	else
 		echo "Missing [${device}]"
--- version.sh
+++ version.sh
@@ -50,11 +50,11 @@ linux_repo="https://kernel.googlesource.com/pub/scm/linux/kernel/git/torvalds/li
 linux_stable_repo="https://kernel.googlesource.com/pub/scm/linux/kernel/git/stable/linux.git"
 #
 KERNEL_REL=6.6
-KERNEL_TAG=${KERNEL_REL}.49
+KERNEL_TAG=${KERNEL_REL}.52
 #https://mirrors.edge.kernel.org/pub/linux/kernel/projects/rt/6.6/
-kernel_rt=".49-rt41"
+kernel_rt=".52-rt43"
 #Kernel Build
-BUILD=${build_prefix}18
+BUILD=${build_prefix}18.1
 
 #v6.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
