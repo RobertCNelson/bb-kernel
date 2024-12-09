7bdebec9bd7d34506e47c96a2c34e439e3ebc814
Robert Nelson <robertcnelson@gmail.com>
2024-12-09 15:50:44 -0600

    kernel v6.1.119-bone-rt-r33.1 rebase external git projects and rt: v6.1.119-rt45 BBDTBS: https://openbeagle.org/beagleboard/BeagleBoard-DeviceTrees/-/commit/c89d4f7adfc91b5615451f5437a4054cc13fbc58 RT: patch-6.1.119-rt45.patch.xz WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/wens/wireless-regdb.git/commit/?id=f67f40d009199ee75c906f1ac898b38be981a7f7 BCFSERIAL: https://openbeagle.org/beagleconnect/linux/bcfserial/-/commit/db467023bd136c97c2e13c3a8b9e41dbdfafbc66
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -50,11 +50,11 @@ linux_repo="https://kernel.googlesource.com/pub/scm/linux/kernel/git/torvalds/li
 linux_stable_repo="https://kernel.googlesource.com/pub/scm/linux/kernel/git/stable/linux.git"
 #
 KERNEL_REL=6.1
-KERNEL_TAG=${KERNEL_REL}.102
+KERNEL_TAG=${KERNEL_REL}.119
 #https://mirrors.edge.kernel.org/pub/linux/kernel/projects/rt/6.1/
-kernel_rt=".102-rt37"
+kernel_rt=".119-rt45"
 #Kernel Build
-BUILD=${build_prefix}33
+BUILD=${build_prefix}33.1
 
 #v6.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
