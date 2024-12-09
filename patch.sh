28e17597cb76d24f21b7f60c3850c37389e40739
Robert Nelson <robertcnelson@gmail.com>
2024-12-09 11:40:55 -0600

    kernel v6.13-rc2-bone1.1 rebase external git projects BBDTBS: https://openbeagle.org/beagleboard/BeagleBoard-DeviceTrees/-/commit/bacdafd944771de281563cbe61385ee90d82b9d6 WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/wens/wireless-regdb.git/commit/?id=f67f40d009199ee75c906f1ac898b38be981a7f7
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -96,14 +96,6 @@ cherrypick () {
 	num=$(($num+1))
 }
 
-copy_mainline_driver () {
-	#regenerate="enable"
-	if [ "x${regenerate}" = "xenable" ] ; then
-		cp -v ./drivers/mmc/core/quirks.h ../patches/mainline/mmc/
-		exit 2
-	fi
-}
-
 external_git () {
 	git_tag=""
 	echo "pulling: ${git_tag}"
@@ -342,7 +334,6 @@ local_patch () {
 	${git} "${DIR}/patches/dir/0001-patch.patch"
 }
 
-copy_mainline_driver
 #external_git
 #mainline_patches
 rt
@@ -440,23 +431,6 @@ backports
 drivers
 
 packaging () {
-	echo "Update: package scripts"
-	#do_backport="enable"
-	if [ "x${do_backport}" = "xenable" ] ; then
-		backport_tag="v6.8.9"
-
-		subsystem="bindeb-pkg"
-		#regenerate="enable"
-		if [ "x${regenerate}" = "xenable" ] ; then
-			pre_backports
-
-			cp -v ~/linux-src/scripts/package/* ./scripts/package/
-
-			post_backports
-		else
-			patch_backports
-		fi
-	fi
 	${git} "${DIR}/patches/backports/bindeb-pkg/0002-builddeb-Install-our-dtbs-under-boot-dtbs-version.patch"
 }
 
