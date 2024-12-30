0c198cc1f8497e4c86c9bf571c2de6bdff83d0e1
Robert Nelson <robertcnelson@gmail.com>
2024-12-30 12:17:58 -0600

    debian: 6.12.6
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patches/get_debian_configs.sh
+++ patches/get_debian_configs.sh
@@ -3,11 +3,11 @@
 #
 #https://packages.debian.org/source/sid/linux
 #
-#abi="6.10.6"
-#kernel="6.10.6-1"
+abi="6.12.6"
+kernel="6.12.6-1"
 #
-abi="6.12-rc6"
-kernel="6.12~rc6-1~exp1"
+#abi="6.12-rc6"
+#kernel="6.12~rc6-1~exp1"
 #
 
 mirror_site="http://192.168.1.10/debian/pool/main/l/linux"
