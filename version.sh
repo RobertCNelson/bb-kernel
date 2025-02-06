d8558326c8eb894f1a515e87777815ee4f6ba719
Robert Nelson <robertcnelson@gmail.com>
2025-02-06 15:58:55 -0600

    kernel v6.4.16-bone24.1 rebase external git projects BBDTBS: https://openbeagle.org/beagleboard/BeagleBoard-DeviceTrees/-/commit/77d658bc8e89324c70f1532f80e94c863bb8b3ea WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/wens/wireless-regdb.git/commit/?id=f67f40d009199ee75c906f1ac898b38be981a7f7 WPANUSB: https://openbeagle.org/beagleconnect/linux/wpanusb/-/commit/6aa9bf65b9d88a2c9a111e7b4aed03de2be9413d BCFSERIAL: https://openbeagle.org/beagleconnect/linux/bcfserial/-/commit/db467023bd136c97c2e13c3a8b9e41dbdfafbc66
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -46,12 +46,15 @@ toolchain="gcc_13_arm"
 #toolchain="gcc_14_riscv64"
 
 #Kernel
+linux_repo="https://kernel.googlesource.com/pub/scm/linux/kernel/git/torvalds/linux.git"
+linux_stable_repo="https://kernel.googlesource.com/pub/scm/linux/kernel/git/stable/linux.git"
+#
 KERNEL_REL=6.4
 KERNEL_TAG=${KERNEL_REL}.16
 #https://mirrors.edge.kernel.org/pub/linux/kernel/projects/rt/6.4/
 kernel_rt=".6-rt8"
 #Kernel Build
-BUILD=${build_prefix}24
+BUILD=${build_prefix}24.1
 
 #v6.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
