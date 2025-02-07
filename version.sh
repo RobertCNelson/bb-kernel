a76e3861f13d10acf208b3b31093aaa4e840b7c1
Robert Nelson <robertcnelson@gmail.com>
2025-02-07 17:21:55 -0600

    5.15.177-bone-rt-r43 release BBDTBS: https://openbeagle.org/beagleboard/BeagleBoard-DeviceTrees/-/commit/a30b759b8906a324fcecbb28b1f233850119d4ed RT: patch-5.15.177-rt83.patch.xz WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/wens/wireless-regdb.git/commit/?id=f67f40d009199ee75c906f1ac898b38be981a7f7
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -54,7 +54,7 @@ KERNEL_TAG=${KERNEL_REL}.177
 #https://mirrors.edge.kernel.org/pub/linux/kernel/projects/rt/5.15/
 kernel_rt=".177-rt83"
 #Kernel Build
-BUILD=${build_prefix}42.1
+BUILD=${build_prefix}43
 
 #v6.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
