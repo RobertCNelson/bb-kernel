4df6a20c638d38d4c60259b32a1926a5c767d1c4
Robert Nelson <robertcnelson@gmail.com>
2025-02-12 11:45:07 -0600

    scripts: resync all build scripts
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/host_det.sh
+++ tools/host_det.sh
@@ -457,6 +457,11 @@ debian_regs () {
 			#http://packages.linuxmint.com/index.php
 			deb_distro="noble"
 			;;
+		xia)
+			#22.1
+			#http://packages.linuxmint.com/index.php
+			deb_distro="noble"
+			;;
 		esac
 
 		#Devuan: Compatibility Matrix
@@ -485,18 +490,23 @@ debian_regs () {
 			#14 forky: https://wiki.debian.org/DebianForky
 			deb_distro="sid"
 			;;
+		duke)
+			#15 duke: https://wiki.debian.org/DebianDuke
+			deb_distro="sid"
+			;;
 		esac
 
 		#https://wiki.ubuntu.com/Releases
 		unset error_unknown_deb_distro
 		case "${deb_distro}" in
-		buster|bullseye|bookworm|trixie|forky|sid)
+		buster|bullseye|bookworm|trixie|forky|duke|sid)
 			#https://wiki.debian.org/LTS
 			#10 buster: 2024-06-30 https://wiki.debian.org/DebianBuster
 			#11 bullseye: 2026 https://wiki.debian.org/DebianBullseye
 			#12 bookworm: https://wiki.debian.org/DebianBookworm
 			#13 trixie: https://wiki.debian.org/DebianTrixie
 			#14 forky: https://wiki.debian.org/DebianForky
+			#15 duke: https://wiki.debian.org/DebianDuke
 			unset warn_eol_distro
 			;;
 		squeeze|wheezy|jessie|stretch)
@@ -508,15 +518,15 @@ debian_regs () {
 			warn_eol_distro=1
 			stop_pkg_search=1
 			;;
-		focal|jammy|mantic|noble)
+		focal|jammy|noble|oracular|plucky)
 			#20.04 focal: (EOL: April 2025) lts: focal -> jammy
 			#22.04 jammy: (EOL: April 2027) lts: jammy -> noble
-			#23.10 mantic: (EOL: July 2024)
 			#24.04 noble: (EOL: June 2029) lts: noble -> xyz
 			#24.10 oracular: (EOL: July 2025)
+			#25.04 plucky: (EOL: April 2025)
 			unset warn_eol_distro
 			;;
-		hardy|lucid|maverick|natty|oneiric|precise|quantal|raring|saucy|trusty|utopic|vivid|wily|xenial|yakkety|zesty|artful|bionic|cosmic|disco|eoan|groovy|hirsute|impish|kinetic|lunar)
+		hardy|lucid|maverick|natty|oneiric|precise|quantal|raring|saucy|trusty|utopic|vivid|wily|xenial|yakkety|zesty|artful|bionic|cosmic|disco|eoan|groovy|hirsute|impish|kinetic|lunar|mantic)
 			#8.04 hardy: (EOL: May 2013) lts: hardy -> lucid
 			#10.04 lucid: (EOL: April 2015) lts: lucid -> precise
 			#10.10 maverick: (EOL: April 10, 2012)
@@ -543,6 +553,7 @@ debian_regs () {
 			#21.10 impish: (EOL: July 2022)
 			#22.10 kinetic: (EOL: July 2023)
 			#23.04 lunar: (EOL: January 2024)
+			#23.10 mantic: (EOL: July 2024)
 			warn_eol_distro=1
 			stop_pkg_search=1
 			;;
