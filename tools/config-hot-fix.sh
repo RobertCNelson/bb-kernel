30792dcb47cec506ad1677a73f52760e58644b88
Robert Nelson <robertcnelson@gmail.com>
2025-04-10 09:18:01 -0500

    ci: build with both debian-12-amd64/debian-13-amd64
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/config-hot-fix.sh
+++ tools/config-hot-fix.sh
@@ -223,9 +223,22 @@ config="CONFIG_USB_TI_CPPI41_DMA" ; config_disable
 #removed in 6.7-rc1
 ./scripts/config --disable CONFIG_DEV_APPLETALK
 
-#09.02.00.005
+#TI delta 09.01.00.004:
+./scripts/config --enable CONFIG_APERTURE_HELPERS
+./scripts/config --enable CONFIG_FB_CFB_FILLRECT
+./scripts/config --enable CONFIG_FB_CFB_COPYAREA
+./scripts/config --enable CONFIG_FB_CFB_IMAGEBLIT
+./scripts/config --enable CONFIG_FB_SIMPLE
+./scripts/config --module CONFIG_TI_EQEP
+
 ./scripts/config --module CONFIG_VIDEO_TI_VIP
 ./scripts/config --module CONFIG_VIDEO_OV1063X
+./scripts/config --module CONFIG_VIDEO_OV2312
+./scripts/config --module CONFIG_VIDEO_OV5640
+./scripts/config --module CONFIG_VIDEO_OV5645
+./scripts/config --module CONFIG_VIDEO_IMX219
+./scripts/config --module CONFIG_VIDEO_IMX390
+./scripts/config --module CONFIG_VIDEO_OX05B1S
 
 #enable SPI/W1
 ./scripts/config --enable CONFIG_SPI_OMAP24XX
@@ -254,8 +267,8 @@ config="CONFIG_USB_TI_CPPI41_DMA" ; config_disable
 ./scripts/config --module CONFIG_RPMSG_PRU
 ./scripts/config --enable CONFIG_RPMSG_VIRTIO
 
-#TI: 10.00.04
-./scripts/config --enable CONFIG_FB_SIMPLE
+#TI: 10.01.01
+./scripts/config --module CONFIG_OMAP2PLUS_MBOX
 
 #new in v6.12.x
 ./scripts/config --enable CONFIG_RPMB
