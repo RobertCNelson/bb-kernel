4025103b280baa33f5308fd04bb770d0223822f9
Robert Nelson <robertcnelson@gmail.com>
2025-04-15 09:49:52 -0500

    scripts: resync all build scripts
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- repo_maintenance/push-n-tag-release.sh
+++ repo_maintenance/push-n-tag-release.sh
@@ -17,20 +17,12 @@ cat_files () {
 	if [ -f ./patches/external/git/WIRELESS_REGDB ] ; then
 		cat ./patches/external/git/WIRELESS_REGDB >> ${wfile}
 	fi
-
-	if [ -f ./patches/external/git/WPANUSB ] ; then
-		cat ./patches/external/git/WPANUSB >> ${wfile}
-	fi
-
-	if [ -f ./patches/external/git/BCFSERIAL ] ; then
-		cat ./patches/external/git/BCFSERIAL >> ${wfile}
-	fi
 }
 
 DIR=$PWD
 git_bin=$(which git)
 
-repo="git@gitlab.gfnd.rcn-ee.org:production/linux-stable-rcn-ee.git"
+repo="ssh://git@forgejo.gfnd.rcn-ee.org:222/Production/linux-stable-rcn-ee.git"
 example="rcn-ee"
 
 if [ -e ${DIR}/version.sh ]; then
