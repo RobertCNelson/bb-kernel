2cde6f676739408bcb561b1554f3ffd22501687a
Robert Nelson <robertcnelson@gmail.com>
2025-04-25 20:21:27 -0500

    test: it66122
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -465,6 +465,9 @@ drivers () {
 	dir 'drivers/fb_ssd1306'
 	dir 'drivers/sdhci-omap'
 
+	dir 'drivers/pre-ite-it66121'
+	dir 'drivers/ite-it66121'
+
 	dir 'external/ti-amx3-cm3-pm-firmware'
 
 	dir 'drivers/fixes/mmc'
