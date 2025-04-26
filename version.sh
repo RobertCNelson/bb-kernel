f650d06af642f0f158d35e232cf852da3b945d9f
Robert Nelson <robertcnelson@gmail.com>
2025-04-26 10:40:21 -0500

    test: it66122
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -49,7 +49,7 @@ KERNEL_TAG=${KERNEL_REL}.11
 #https://mirrors.edge.kernel.org/pub/linux/kernel/projects/rt/6.11/
 kernel_rt="-rt7"
 #Kernel Build
-BUILD=${build_prefix}11.1
+BUILD=${build_prefix}11.2
 
 #v6.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
