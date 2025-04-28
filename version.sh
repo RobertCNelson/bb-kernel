49c94f13fc377873956bbe609e3044ba7f21b877
Robert Nelson <robertcnelson@gmail.com>
2025-04-28 16:37:46 -0500

    cleanup: it66121
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -49,7 +49,7 @@ KERNEL_TAG=${KERNEL_REL}.24
 #https://mirrors.edge.kernel.org/pub/linux/kernel/projects/rt/6.12/
 kernel_rt=".16-rt9"
 #Kernel Build
-BUILD=${build_prefix}21
+BUILD=${build_prefix}21.1
 
 #v6.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
