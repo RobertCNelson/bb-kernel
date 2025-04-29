d1c88247763496c41086dd883344de7ef257e731
Robert Nelson <robertcnelson@gmail.com>
2025-04-29 14:26:05 -0500

    ci: wire up
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -40,6 +40,9 @@ toolchain="gcc_6_arm"
 #toolchain="gcc_12_riscv64"
 
 #Kernel
+linux_repo="https://kernel.googlesource.com/pub/scm/linux/kernel/git/torvalds/linux.git"
+linux_stable_repo="https://kernel.googlesource.com/pub/scm/linux/kernel/git/stable/linux.git"
+#
 KERNEL_REL=4.9
 KERNEL_TAG=${KERNEL_REL}.178
 kernel_rt=".178-rt131"
