b900ca9054ffd8512a5679b81f1565a748109698
Robert Nelson <robertcnelson@gmail.com>
2025-04-29 09:21:48 -0500

    6.5.13-bone20 release BBDTBS: https://openbeagle.org/beagleboard/BeagleBoard-DeviceTrees/-/commit/cb68d99ce2a6e707df9106f8104919018a938780 WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/wens/wireless-regdb.git/commit/?id=6953f19b96f4bf21b22de5c039e372bb33e2c96b
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -451,8 +451,8 @@ backports () {
 		post_backports
 	else
 		patch_backports
-		#v6.10
-		#git format-patch -19 drivers/gpu/drm/bridge/ite-it66121.c
+		#v6.15
+		#git format-patch -25 drivers/gpu/drm/bridge/ite-it66121.c
 		#
 		${git} "${DIR}/patches/mainline/ite-it66121/0001-drm-bridge-it66121-Convert-to-i2c-s-.probe_new.patch"
 		${git} "${DIR}/patches/mainline/ite-it66121/0002-drm-bridge-it66121-Use-devm_regulator_bulk_get_enabl.patch"
@@ -482,6 +482,18 @@ backports () {
 		#v6.10.x+
 		${git} "${DIR}/patches/mainline/ite-it66121/0018-drm-bridge-ite66121-Register-HPD-interrupt-handler-o.patch"
 		#${git} "${DIR}/patches/mainline/ite-it66121/0019-drm-bridge-it66121-Remove-a-duplicated-invoke-of-of_.patch"
+
+		#v6.13.x+
+		#${git} "${DIR}/patches/mainline/ite-it66121/0020-drm-bridge-ite-it66121-Drop-hdmi_avi_infoframe_init-.patch"
+
+		#v6.14.x+
+		#${git} "${DIR}/patches/mainline/ite-it66121/0021-drm-bridge-ite-it66121-use-eld_mutex-to-protect-acce.patch"
+		#${git} "${DIR}/patches/mainline/ite-it66121/0022-drm-Use-of_property_present-for-non-boolean-properti.patch"
+		#${git} "${DIR}/patches/mainline/ite-it66121/0023-ASoC-hdmi-codec-move-no_capture_mute-to-struct-hdmi_.patch"
+
+		#v6.15.x+
+		#${git} "${DIR}/patches/mainline/ite-it66121/0024-drm-bridge-Pass-full-state-to-atomic_enable.patch"
+		#${git} "${DIR}/patches/mainline/ite-it66121/0025-drm-bridge-Pass-full-state-to-atomic_disable.patch"
 	fi
 
 	backport_tag="rpi-6.5.y"
--- version.sh
+++ version.sh
@@ -49,7 +49,7 @@ KERNEL_TAG=${KERNEL_REL}.13
 #https://mirrors.edge.kernel.org/pub/linux/kernel/projects/rt/6.5/
 kernel_rt=".2-rt8"
 #Kernel Build
-BUILD=${build_prefix}19.3
+BUILD=${build_prefix}20
 
 #v6.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
