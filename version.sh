7b155944055310179a93a6f777db38e611d2d1e4
Robert Nelson <robertcnelson@gmail.com>
2025-04-29 19:10:00 -0500

    scripts: resync all build scripts
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -40,6 +40,9 @@ toolchain="gcc_8_arm"
 #toolchain="gcc_12_riscv64"
 
 #Kernel
+linux_repo="https://kernel.googlesource.com/pub/scm/linux/kernel/git/torvalds/linux.git"
+linux_stable_repo="https://kernel.googlesource.com/pub/scm/linux/kernel/git/stable/linux.git"
+#
 KERNEL_REL=5.0
 KERNEL_TAG=${KERNEL_REL}.21
 kernel_rt=".X-rtY"
