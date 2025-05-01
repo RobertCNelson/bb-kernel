3e8d6c7ecf1d5f7d2753dd3df970dedf5d39310b
Robert Nelson <robertcnelson@gmail.com>
2025-05-01 15:25:33 -0500

    ci: cleanup and fix build
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- scripts/git.sh
+++ scripts/git.sh
@@ -247,9 +247,10 @@ git_minor=$(LC_ALL=C ${git_bin} --version | awk '{print $3}' | cut -d. -f2)
 git_sub=$(LC_ALL=C ${git_bin} --version | awk '{print $3}' | cut -d. -f3)
 
 #debian Stable:
-#https://packages.debian.org/stretch/git -> 2.11.0
-#https://packages.debian.org/buster/git -> 2.20.1
-#https://packages.debian.org/bullseye/git -> 2.30.2
+#https://packages.debian.org/stretch/git (9) -> 2.11.0
+#https://packages.debian.org/buster/git (10) -> 2.20.1
+#https://packages.debian.org/bullseye/git (11) -> 2.30.2
+#https://packages.debian.org/bookworm/git (12) -> 2.39.5
 #https://packages.ubuntu.com/bionic/git (18.04) -> 2.17.1
 #https://packages.ubuntu.com/focal/git (20.04) -> 2.25.1
 #https://packages.ubuntu.com/jammy/git (22.04) -> 2.34.1
