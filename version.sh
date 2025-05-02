2414e5289968af8ad4b766518c249355dd5db23c
Robert Nelson <robertcnelson@gmail.com>
2025-05-02 14:55:17 -0500

    it66121: merge drm_connector-switch-to-edid_read-callba
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -49,7 +49,7 @@ KERNEL_TAG=${KERNEL_REL}.26
 #https://mirrors.edge.kernel.org/pub/linux/kernel/projects/rt/6.12/
 kernel_rt=".16-rt9"
 #Kernel Build
-BUILD=${build_prefix}22.1
+BUILD=${build_prefix}22.2
 
 #v6.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
