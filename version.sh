7b925775d223e685c608148bfd2906fd09348153
Robert Nelson <robertcnelson@gmail.com>
2025-05-06 18:54:57 -0500

    6.6.87-bone-rt-r27 release BBDTBS: https://openbeagle.org/beagleboard/BeagleBoard-DeviceTrees/-/commit/248814d995de773082b624299b2ba543b15ec66d RT: patch-6.6.87-rt54.patch.xz WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/wens/wireless-regdb.git/commit/?id=6953f19b96f4bf21b22de5c039e372bb33e2c96b
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -49,7 +49,7 @@ KERNEL_TAG=${KERNEL_REL}.87
 #https://mirrors.edge.kernel.org/pub/linux/kernel/projects/rt/6.6/
 kernel_rt=".87-rt54"
 #Kernel Build
-BUILD=${build_prefix}26.1
+BUILD=${build_prefix}27
 
 #v6.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
