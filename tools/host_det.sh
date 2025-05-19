9280ea37374a7e1f944fbeb40e000478ea634371
Robert Nelson <robertcnelson@gmail.com>
2025-05-19 16:47:15 -0500

    scripts: resync all build scripts
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/host_det.sh
+++ tools/host_det.sh
@@ -323,6 +323,11 @@ debian_regs () {
 			#http://packages.linuxmint.com/index.php
 			deb_distro="bookworm"
 			;;
+		gigi)
+			#LMDE 7
+			#http://packages.linuxmint.com/index.php
+			deb_distro="trixie"
+			;;
 		debian)
 			deb_distro="jessie"
 			;;
@@ -461,6 +466,11 @@ debian_regs () {
 			;;
 		xia)
 			#22.1
+			#http://packages.linuxmint.com/index.php
+			deb_distro="noble"
+			;;
+		zara)
+			#22.2
 			#http://packages.linuxmint.com/index.php
 			deb_distro="noble"
 			;;
