1765d4236fdd7ce0a26bbb75e07654b4266fd84d
Robert Nelson <robertcnelson@gmail.com>
2025-05-27 11:42:43 -0500

    6.15-bone13 release BBDTBS: https://openbeagle.org/beagleboard/BeagleBoard-DeviceTrees/-/commit/89554f8dd8db3eca16d6c4ffa5ee1cd3428ee462 WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/wens/wireless-regdb.git/commit/?id=6953f19b96f4bf21b22de5c039e372bb33e2c96b
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -55,7 +55,7 @@ KERNEL_TAG=${KERNEL_REL}
 #https://mirrors.edge.kernel.org/pub/linux/kernel/projects/rt/6.15/
 kernel_rt="-rc1-rt1"
 #Kernel Build
-BUILD=${build_prefix}12.1
+BUILD=${build_prefix}13
 
 #v6.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
