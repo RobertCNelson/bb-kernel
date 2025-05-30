8d2d1c1bb56a16ed2a4baa6091ccc89e9de409f6
Robert Nelson <robertcnelson@gmail.com>
2025-05-29 20:37:03 -0500

    6.12.28-bone-rt-r27 release BBDTBS: https://github.com/beagleboard/BeagleBoard-DeviceTrees/commit/2955281d5b940d184ada72d668f3cf13408ddf13 RT: patch-6.12.28-rt10.patch.xz WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/wens/wireless-regdb.git/commit/?id=6953f19b96f4bf21b22de5c039e372bb33e2c96b
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -55,7 +55,7 @@ KERNEL_TAG=${KERNEL_REL}.28
 #https://mirrors.edge.kernel.org/pub/linux/kernel/projects/rt/6.12/
 kernel_rt=".28-rt10"
 #Kernel Build
-BUILD=${build_prefix}26.2
+BUILD=${build_prefix}27
 
 #v6.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
