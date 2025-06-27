2869dfe317634340ad556242faaf54d83c713aaf
Robert Nelson <robertcnelson@gmail.com>
2025-06-27 13:17:27 -0500

    backport: tps65219 for eco
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -447,6 +447,25 @@ post_rpibackports () {
 }
 
 backports () {
+	backport_tag="v6.16-rc3"
+
+	subsystem="tps65219"
+	#regenerate="enable"
+	if [ "x${regenerate}" = "xenable" ] ; then
+		pre_backports
+
+		cp -v ~/linux-src/drivers/input/misc/tps65219-pwrbutton.c ./drivers/input/misc/
+		cp -v ~/linux-src/drivers/mfd/tps65219.c ./drivers/mfd/
+		cp -v ~/linux-src/drivers/gpio/gpio-tps65219.c ./drivers/gpio/
+		cp -v ~/linux-src/drivers/regulator/tps65219-regulator.c ./drivers/regulator/
+		cp -v ~/linux-src/Documentation/devicetree/bindings/regulator/ti,tps65219.yaml ./Documentation/devicetree/bindings/regulator/
+		cp -v ~/linux-src/include/linux/mfd/tps65219.h ./include/linux/mfd/
+
+		post_backports
+	else
+		patch_backports
+	fi
+
 	backport_tag="v6.2"
 
 	subsystem="it66121"
@@ -526,6 +545,8 @@ drivers () {
 	dir 'drivers/it66122'
 
 	dir 'external/ti-amx3-cm3-pm-firmware'
+
+	dir 'fixes'
 }
 
 ###
