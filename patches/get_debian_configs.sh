6a8052aa2eea2ab29f600eb94977a8c08428dcfc
Robert Nelson <robertcnelson@gmail.com>
2025-07-07 15:53:13 -0500

    kernel v6.16-rc5-bone6.1 rebase external git projects BBDTBS: https://github.com/beagleboard/BeagleBoard-DeviceTrees/commit/f16fe6cb5ca734bdd83d937a4f925ad633ea3884 WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/wens/wireless-regdb.git/commit/?id=6953f19b96f4bf21b22de5c039e372bb33e2c96b
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patches/get_debian_configs.sh
+++ patches/get_debian_configs.sh
@@ -3,11 +3,11 @@
 #
 #https://packages.debian.org/source/sid/linux
 #
-#abi="6.12.33"
-#kernel="6.12.33-1"
+#abi="6.12.35"
+#kernel="6.12.35-1"
 #
 abi="6.15"
-kernel="6.15.4-1~exp1"
+kernel="6.15.5-1~exp1"
 #
 #abi="6.15-rc7"
 #kernel="6.15~rc7-1~exp1"
