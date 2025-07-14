fd81286d012bc03aae78d9643b96c96fc4388f14
Robert Nelson <robertcnelson@gmail.com>
2025-07-14 16:13:47 -0500

    kernel v6.16-rc6-bone7.2 rebase external git projects BBDTBS: https://github.com/beagleboard/BeagleBoard-DeviceTrees/commit/9d9f8208e3293e35f2c704720d65541b33aebccf WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/wens/wireless-regdb.git/commit/?id=e05260afb7203dd0a7ba6faf9a30c86faf19a24e
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patches/get_debian_configs.sh
+++ patches/get_debian_configs.sh
@@ -7,7 +7,7 @@
 #kernel="6.12.35-1"
 #
 abi="6.15"
-kernel="6.15.5-1~exp1"
+kernel="6.15.6-1~exp1"
 #
 #abi="6.15-rc7"
 #kernel="6.15~rc7-1~exp1"
--- version.sh
+++ version.sh
@@ -51,11 +51,11 @@ linux_repo="https://kernel.googlesource.com/pub/scm/linux/kernel/git/torvalds/li
 linux_stable_repo="https://kernel.googlesource.com/pub/scm/linux/kernel/git/stable/linux.git"
 #
 KERNEL_REL=6.16
-KERNEL_TAG=${KERNEL_REL}-rc5
+KERNEL_TAG=${KERNEL_REL}-rc6
 #https://mirrors.edge.kernel.org/pub/linux/kernel/projects/rt/6.16/
 kernel_rt="-rt2"
 #Kernel Build
-BUILD=${build_prefix}7.1
+BUILD=${build_prefix}7.2
 
 #v6.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
