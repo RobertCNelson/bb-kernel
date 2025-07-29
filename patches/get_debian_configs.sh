0a3c8dea908a92a9e841273e09a2875b25458a62
Robert Nelson <robertcnelson@gmail.com>
2025-07-29 16:10:17 -0500

    scripts: resync all build scripts
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patches/get_debian_configs.sh
+++ patches/get_debian_configs.sh
@@ -3,8 +3,8 @@
 #
 #https://packages.debian.org/source/trixie/linux
 #
-abi="6.12.35+deb13"
-kernel="6.12.35-1"
+abi="6.12.38+deb13"
+kernel="6.12.38-1"
 #
 
 mirror_site="http://192.168.1.10/debian/pool/main/l/linux"
@@ -24,7 +24,7 @@ dl_deb () {
 
 	if [ -f ./dl/linux-image-${abi}-${image}${unsigned}_${kernel}_${dpkg_arch}.deb ] ; then
 		dpkg -x ./dl/linux-image-${abi}-${image}${unsigned}_${kernel}_${dpkg_arch}.deb ./dl/tmp/
-		cp -v ./dl/tmp/boot/config-${abi}-${image} ./debian
+		cp -v ./dl/tmp/boot/config-${abi}-${image} ./debian.config
 		rm -rf ./dl/tmp/ || true
 	fi
 }
