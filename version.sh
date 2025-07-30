3aa368bd9bf495c08c7aaa7770ed6f676357ef04
Robert Nelson <robertcnelson@gmail.com>
2025-07-30 09:54:02 -0500

    config: convert to new debian base with diff
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -55,7 +55,7 @@ KERNEL_TAG=${KERNEL_REL}.36
 #https://mirrors.edge.kernel.org/pub/linux/kernel/projects/rt/6.12/
 kernel_rt=".28-rt10"
 #Kernel Build
-BUILD=${build_prefix}29
+BUILD=${build_prefix}29.1
 
 #v6.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
