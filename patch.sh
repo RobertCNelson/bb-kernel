1188b957b1eebcd1b5ddf2d09cfb899f2f3a8564
Robert Nelson <robertcnelson@gmail.com>
2025-09-02 17:57:11 -0500

    kernel v6.13.12-bone22.2 rebase external git projects BBDTBS: https://github.com/beagleboard/BeagleBoard-DeviceTrees/commit/004a84d8df4b5836655e9df1616d9e090d1ba4c9 WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/wens/wireless-regdb.git/commit/?id=e05260afb7203dd0a7ba6faf9a30c86faf19a24e
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -276,29 +276,30 @@ beagleboard_dtbs () {
 		cp -v ../${work_dir}/src/arm64/ti/*.h arch/arm64/boot/dts/ti/
 		cp -vr ../${work_dir}/include/dt-bindings/* ./include/dt-bindings/
 
-		device="AM335X-PRU-UIO-00A0" ; arm_dtbo_makefile_append
 		device="BB-ADC-00A0" ; arm_dtbo_makefile_append
 		device="BB-BBBW-WL1835-00A0" ; arm_dtbo_makefile_append
 		device="BB-BBGG-WL1835-00A0" ; arm_dtbo_makefile_append
 		device="BB-BBGW-WL1835-00A0" ; arm_dtbo_makefile_append
-
 		device="BB-BONE-eMMC1-01-00A0" ; arm_dtbo_makefile_append
-
+		device="BB-HDMI-IT66121-00A0" ; arm_dtbo_makefile_append
+		device="BB-HDMI-TDA998x-00A0" ; arm_dtbo_makefile_append
+		device="BB-NHDMI-IT66121-00A0" ; arm_dtbo_makefile_append
+		device="BB-NHDMI-TDA998x-00A0" ; arm_dtbo_makefile_append
+		device="BBORG_COMMS-00A2" ; arm_dtbo_makefile_append
+		device="BBORG_FAN-A000" ; arm_dtbo_makefile_append
+		device="BBORG_RELAY-00A2" ; arm_dtbo_makefile_append
+		device="BB-SPIDEV0-00A0" ; arm_dtbo_makefile_append
+		device="BB-SPIDEV1-00A0" ; arm_dtbo_makefile_append
 		device="BB-UART1-00A0" ; arm_dtbo_makefile_append
 		device="BB-UART2-00A0" ; arm_dtbo_makefile_append
 		device="BB-UART4-00A0" ; arm_dtbo_makefile_append
-
-		device="BBORG_COMMS-00A2" ; arm_dtbo_makefile_append
-		device="BBORG_FAN-A000" ; arm_dtbo_makefile_append
-
 		device="BONE-ADC" ; arm_dtbo_makefile_append
+		device="BONE-LED-P9-42" ; arm_dtbo_makefile_append
+		device="M-BB-BBG-00A0" ; arm_dtbo_makefile_append
+		device="M-BB-BBGG-00A0" ; arm_dtbo_makefile_append
 
 		device="am335x-boneblack-uboot.dtb" ; arm_dtb_makefile_append
 		device="am335x-boneblack-revd.dtb" ; arm_dtb_makefile_append
-
-		device="BONE-I2C1" ; k3_dtbo_makefile_append
-		device="BONE-I2C2" ; k3_dtbo_makefile_append
-		device="BONE-I2C3" ; k3_dtbo_makefile_append
 
 		device="k3-am6232-pocketbeagle2.dtb" ; k3_dtb_makefile_append
 
@@ -502,66 +503,8 @@ backports () {
 		cp -v ~/linux-src/drivers/gpu/drm/bridge/ite-it66121.c ./drivers/gpu/drm/bridge/
 
 		post_backports
-	else
-		#patch_backports
-		#v6.15
-		#git format-patch -25 drivers/gpu/drm/bridge/ite-it66121.c
-		#
-
-		#v6.1.x
-		#${git} "${DIR}/patches/mainline/ite-it66121/0001-drm-bridge-it66121-Convert-to-i2c-s-.probe_new.patch"
-		#${git} "${DIR}/patches/mainline/ite-it66121/0002-drm-bridge-it66121-Use-devm_regulator_bulk_get_enabl.patch"
-		#${git} "${DIR}/patches/mainline/ite-it66121/0003-drm-bridge-it66121-Use-regmap_noinc_read.patch"
-		#${git} "${DIR}/patches/mainline/ite-it66121/0004-drm-bridge-it66121-Write-AVI-infoframe-with-regmap_b.patch"
-		#${git} "${DIR}/patches/mainline/ite-it66121/0005-drm-bridge-it66121-Fix-wait-for-DDC-ready.patch"
-		#${git} "${DIR}/patches/mainline/ite-it66121/0006-drm-bridge-it66121-Don-t-use-DDC-error-IRQs.patch"
-		#${git} "${DIR}/patches/mainline/ite-it66121/0007-drm-bridge-it66121-Don-t-clear-DDC-FIFO-twice.patch"
-		#${git} "${DIR}/patches/mainline/ite-it66121/0008-drm-bridge-it66121-Set-DDC-preamble-only-once-before.patch"
-		#${git} "${DIR}/patches/mainline/ite-it66121/0009-drm-bridge-it66121-Move-VID-PID-to-new-it66121_chip_.patch"
-		#${git} "${DIR}/patches/mainline/ite-it66121/0010-drm-bridge-it66121-Add-support-for-the-IT6610.patch"
-		#${git} "${DIR}/patches/mainline/ite-it66121/0011-drm-bridge-Remove-unnecessary-include-statements-for.patch"
-
-		#v6.2.x
-		#v6.3.x
-		#v6.4.x
-		#v6.5.x+... (not v6.1.x/v6.2.x/v6.3.x/v6.4.x)
-		#${git} "${DIR}/patches/mainline/ite-it66121/0012-drm-Switch-i2c-drivers-back-to-use-.probe.patch"
-
-		#v6.5.x
-		#v6.6.x
-		#v6.7.x+
-		#${git} "${DIR}/patches/mainline/ite-it66121/0013-drm-bridge-it66121-Extend-match-support-for-OF-table.patch"
-		#v6.1.x-lts... (not v6.2.x/v6.3.x/v6.4.x)
-		#${git} "${DIR}/patches/mainline/ite-it66121/0014-drm-bridge-it66121-Simplify-probe.patch"
-		#${git} "${DIR}/patches/mainline/ite-it66121/0015-drm-bridge-it66121-Fix-invalid-connector-dereference.patch"
-		#${git} "${DIR}/patches/mainline/ite-it66121/0016-drm-bridge-it66121-get_edid-callback-must-not-return.patch"
-
-		#v6.7.x
-		#v6.8.x
-		#v6.9.x+ (disable)
-		#${git} "${DIR}/patches/mainline/ite-it66121/0017-drm-bridge-it66121-switch-to-edid_read-callback.patch"
-
-		#v6.9.x
-		#v6.10.x+
-		#${git} "${DIR}/patches/mainline/ite-it66121/0018-drm-bridge-ite66121-Register-HPD-interrupt-handler-o.patch"
-		#${git} "${DIR}/patches/mainline/ite-it66121/0019-drm-bridge-it66121-Remove-a-duplicated-invoke-of-of_.patch"
-
-		#v6.10.x
-		#v6.11.x
-		#v6.12.x
-		#v6.13.x+
-		#${git} "${DIR}/patches/mainline/ite-it66121/0020-drm-bridge-ite-it66121-Drop-hdmi_avi_infoframe_init-.patch"
-
-		#v6.13.x
-		#v6.14.x+
-		#${git} "${DIR}/patches/mainline/ite-it66121/0021-drm-bridge-ite-it66121-use-eld_mutex-to-protect-acce.patch"
-		${git} "${DIR}/patches/mainline/ite-it66121/0022-drm-Use-of_property_present-for-non-boolean-properti.patch"
-		#(not v6.13.x)
-		#${git} "${DIR}/patches/mainline/ite-it66121/0023-ASoC-hdmi-codec-move-no_capture_mute-to-struct-hdmi_.patch"
-
-		#v6.15.x+
-		#${git} "${DIR}/patches/mainline/ite-it66121/0024-drm-bridge-Pass-full-state-to-atomic_enable.patch"
-		#${git} "${DIR}/patches/mainline/ite-it66121/0025-drm-bridge-Pass-full-state-to-atomic_disable.patch"
+	#else
+		patch_backports
 	fi
 
 	backport_tag="rpi-6.13.y"
