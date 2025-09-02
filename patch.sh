096a159edc31395b343693968edcfbbabcf9eb51
Robert Nelson <robertcnelson@gmail.com>
2025-09-02 14:55:01 -0500

    ite-it66121: revd lights up again
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -524,8 +524,8 @@ backports () {
 drivers () {
 	dir 'branding/boris'
 
-#	dir 'drivers/it66121_drm_connector'
-#	dir 'drivers/it66122'
+	dir 'drivers/it66121_drm_connector'
+	dir 'drivers/it66122'
 
 	dir 'external/ti-amx3-cm3-pm-firmware'
 }
