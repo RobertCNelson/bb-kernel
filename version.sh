ec6ec8dd2caf0f8ecc7dfa6c4ba3836d9f28bead
Robert Nelson <robertcnelson@gmail.com>
2025-09-02 12:59:59 -0500

    tps65219: backport v6.16.4
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -55,7 +55,7 @@ KERNEL_TAG=${KERNEL_REL}.10
 #https://mirrors.edge.kernel.org/pub/linux/kernel/projects/rt/6.15/
 kernel_rt="-rt2"
 #Kernel Build
-BUILD=${build_prefix}26.2
+BUILD=${build_prefix}26.3
 
 #v6.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
