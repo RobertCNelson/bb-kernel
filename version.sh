e08d3a296143c9ad56c7c99c60883ea5d919ff4c
Robert Nelson <robertcnelson@gmail.com>
2025-09-04 15:24:34 -0500

    it66121: v6.3.x
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -55,7 +55,7 @@ KERNEL_TAG=${KERNEL_REL}.99
 #https://mirrors.edge.kernel.org/pub/linux/kernel/projects/rt/6.6/
 kernel_rt=".99-rt58"
 #Kernel Build
-BUILD=${build_prefix}29.1
+BUILD=${build_prefix}29.2
 
 #v6.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
