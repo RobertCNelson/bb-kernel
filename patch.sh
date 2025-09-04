a0afedbe1b002ac0c0d945c00564c8c30e25e007
Robert Nelson <robertcnelson@gmail.com>
2025-09-04 12:27:36 -0500

    kernel v6.16.5-bone15.1 rebase external git projects BBDTBS: https://github.com/beagleboard/BeagleBoard-DeviceTrees/commit/26ded64e2a8480af1957c537313e7f9f6c61a43a WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/wens/wireless-regdb.git/commit/?id=e05260afb7203dd0a7ba6faf9a30c86faf19a24e
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -276,6 +276,8 @@ beagleboard_dtbs () {
 		cp -v ../${work_dir}/src/arm64/ti/*.h arch/arm64/boot/dts/ti/
 		cp -vr ../${work_dir}/include/dt-bindings/* ./include/dt-bindings/
 
+		#ls src/arm/overlays/ | grep dtso
+
 		device="BB-ADC-00A0" ; arm_dtbo_makefile_append
 		device="BB-BBBW-WL1835-00A0" ; arm_dtbo_makefile_append
 		device="BB-BBGG-WL1835-00A0" ; arm_dtbo_makefile_append
@@ -283,6 +285,11 @@ beagleboard_dtbs () {
 		device="BB-BONE-eMMC1-01-00A0" ; arm_dtbo_makefile_append
 		device="BB-HDMI-IT66121-00A0" ; arm_dtbo_makefile_append
 		device="BB-HDMI-TDA998x-00A0" ; arm_dtbo_makefile_append
+		device="BB-I2C1-MCP7940X-00A0" ; arm_dtbo_makefile_append
+		device="BB-I2C1-RTC-DS3231" ; arm_dtbo_makefile_append
+		device="BB-I2C1-RTC-PCF8563" ; arm_dtbo_makefile_append
+		device="BB-I2C2-BME680" ; arm_dtbo_makefile_append
+		device="BB-I2C2-MPU6050" ; arm_dtbo_makefile_append
 		device="BB-NHDMI-IT66121-00A0" ; arm_dtbo_makefile_append
 		device="BB-NHDMI-TDA998x-00A0" ; arm_dtbo_makefile_append
 		device="BBORG_COMMS-00A2" ; arm_dtbo_makefile_append
@@ -305,10 +312,11 @@ beagleboard_dtbs () {
 
 		#ls src/arm64/overlays/ | grep pocketbeagle2
 
+		device="k3-am6232-pocketbeagle2-techlab-cape" ; k3_dtbo_makefile_append
 		device="k3-am62-pocketbeagle2-ardupilot-cape" ; k3_dtbo_makefile_append
 		device="k3-am62-pocketbeagle2-leds-off" ; k3_dtbo_makefile_append
+		device="k3-am62-pocketbeagle2-mspm0swd" ; k3_dtbo_makefile_append
 		device="k3-am62-pocketbeagle2-techlab-cape" ; k3_dtbo_makefile_append
-		device="k3-am6232-pocketbeagle2-techlab-cape" ; k3_dtbo_makefile_append
 
 		#ls src/arm64/overlays/ | grep beagleplay
 
