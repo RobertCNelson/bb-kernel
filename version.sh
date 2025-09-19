4c1051f81592933f8aff65940cd14ae9f52170d5
Robert Nelson <robertcnelson@gmail.com>
2025-09-19 13:21:30 -0500

    6.12.45-bone-rt-r35 release BBDTBS: https://github.com/beagleboard/BeagleBoard-DeviceTrees/commit/dde7fd99569b2a939114f9fff45878069cc031b5 RT: patch-6.12.43-rt12.patch.xz WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/wens/wireless-regdb.git/commit/?id=e05260afb7203dd0a7ba6faf9a30c86faf19a24e
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -55,7 +55,7 @@ KERNEL_TAG=${KERNEL_REL}.45
 #https://mirrors.edge.kernel.org/pub/linux/kernel/projects/rt/6.12/
 kernel_rt=".43-rt12"
 #Kernel Build
-BUILD=${build_prefix}34.1
+BUILD=${build_prefix}35
 
 #v6.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
