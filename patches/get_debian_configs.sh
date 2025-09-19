c073459585fe61452f29e52aa419588d051b6389
Robert Nelson <robertcnelson@gmail.com>
2025-09-19 16:51:36 -0500

    scripts: resync all build scripts
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patches/get_debian_configs.sh
+++ patches/get_debian_configs.sh
@@ -3,12 +3,13 @@
 #
 #https://packages.debian.org/source/bookworm/linux
 #
-abi="6.1.0-18"
-kernel="6.1.76-1"
+abi="6.1.0-39"
+kernel="6.1.148-1"
 #
 
 mirror_site="http://192.168.1.10/debian/pool/main/l/linux"
-debian_site="http://ftp.us.debian.org/debian/pool/main/l/linux"
+debian_site="http://deb.debian.org/debian/pool/main/l/linux"
+debian_security_site="http://deb.debian.org/debian-security/pool/main/l/linux"
 incoming_site="http://incoming.debian.org/debian-buildd/pool/main/l/linux"
 
 dl_deb () {
@@ -22,9 +23,13 @@ dl_deb () {
 		wget -c --directory-prefix=./dl/ ${incoming_site}/linux-image-${abi}-${image}${unsigned}_${kernel}_${dpkg_arch}.deb
 	fi
 
+	if [ ! -f ./dl/linux-image-${abi}-${image}${unsigned}_${kernel}_${dpkg_arch}.deb ] ; then
+		wget -c --directory-prefix=./dl/ ${debian_security_site}/linux-image-${abi}-${image}${unsigned}_${kernel}_${dpkg_arch}.deb
+	fi
+
 	if [ -f ./dl/linux-image-${abi}-${image}${unsigned}_${kernel}_${dpkg_arch}.deb ] ; then
 		dpkg -x ./dl/linux-image-${abi}-${image}${unsigned}_${kernel}_${dpkg_arch}.deb ./dl/tmp/
-		cp -v ./dl/tmp/boot/config-${abi}-${image} ./debian
+		cp -v ./dl/tmp/boot/config-${abi}-${image} ./debian.config
 		rm -rf ./dl/tmp/ || true
 	fi
 }
