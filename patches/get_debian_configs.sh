a94e294d7b32226d579f99084d395613b21d2388
Robert Nelson <robertcnelson@gmail.com>
2025-09-23 10:55:28 -0500

    scripts: resync all build scripts
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patches/get_debian_configs.sh
+++ patches/get_debian_configs.sh
@@ -3,12 +3,13 @@
 #
 #https://packages.debian.org/source/bullseye/linux
 #
-abi="5.10.0-28"
-kernel="5.10.209-2"
+abi="5.10.0-35"
+kernel="5.10.237-1"
 #
 
 mirror_site="http://192.168.1.10/debian/pool/main/l/linux"
-debian_site="http://ftp.us.debian.org/debian/pool/main/l/linux"
+debian_site="http://deb.debian.org/debian/pool/main/l/linux"
+debian_security_site="http://deb.debian.org/debian-security/pool/main/l/linux"
 incoming_site="http://incoming.debian.org/debian-buildd/pool/main/l/linux"
 
 dl_deb () {
@@ -22,9 +23,13 @@ dl_deb () {
 		wget -c --directory-prefix=./dl/ ${incoming_site}/linux-image-${abi}-${image}${unsigned}_${kernel}_${dpkg_arch}.deb
 	fi
 
+	if [ ! -f ./dl/linux-image-${abi}-${image}${unsigned}_${kernel}_${dpkg_arch}.deb ] ; then
+		wget -c --directory-prefix=./dl/ ${debian_security_site}/linux-image-${abi}-${image}${unsigned}_${kernel}_${dpkg_arch}.deb
+	fi
+
 	if [ -f ./dl/linux-image-${abi}-${image}${unsigned}_${kernel}_${dpkg_arch}.deb ] ; then
 		dpkg -x ./dl/linux-image-${abi}-${image}${unsigned}_${kernel}_${dpkg_arch}.deb ./dl/tmp/
-		cp -v ./dl/tmp/boot/config-${abi}-${image} ./debian
+		cp -v ./dl/tmp/boot/config-${abi}-${image} ./debian.config
 		rm -rf ./dl/tmp/ || true
 	fi
 }
--- tools/config-hot-fix.sh
+++ tools/config-hot-fix.sh
@@ -116,78 +116,78 @@ config="CONFIG_PRU_REMOTEPROC" ; config_module
 
 #abi="5.13.0-trunk"
 #kernel="5.13.9-1~exp2"
-config="CONFIG_BPF_UNPRIV_DEFAULT_OFF" ; config_enable
-config="CONFIG_CGROUP_MISC" ; config_enable
-config="CONFIG_RESET_ATTACK_MITIGATION" ; config_enable
+./scripts/config --enable CONFIG_BPF_UNPRIV_DEFAULT_OFF
+./scripts/config --enable CONFIG_CGROUP_MISC
+./scripts/config --enable CONFIG_RESET_ATTACK_MITIGATION
 
 #LIBCOMPOSITE built-in finally works... ;)
-config="CONFIG_USB_LIBCOMPOSITE" ; config_enable
-config="CONFIG_USB_F_ACM" ; config_enable
-config="CONFIG_USB_F_SS_LB" ; config_enable
-config="CONFIG_USB_U_SERIAL" ; config_enable
-config="CONFIG_USB_U_ETHER" ; config_enable
-config="CONFIG_USB_U_AUDIO" ; config_enable
-config="CONFIG_USB_F_SERIAL" ; config_enable
-config="CONFIG_USB_F_OBEX" ; config_enable
-config="CONFIG_USB_F_NCM" ; config_enable
-config="CONFIG_USB_F_ECM" ; config_enable
-config="CONFIG_USB_F_PHONET" ; config_module
-config="CONFIG_USB_F_EEM" ; config_enable
-config="CONFIG_USB_F_SUBSET" ; config_enable
-config="CONFIG_USB_F_RNDIS" ; config_enable
-config="CONFIG_USB_F_MASS_STORAGE" ; config_enable
-config="CONFIG_USB_F_FS" ; config_enable
-config="CONFIG_USB_F_UAC1" ; config_enable
-config="CONFIG_USB_F_UAC2" ; config_enable
-config="CONFIG_USB_F_UVC" ; config_module
-config="CONFIG_USB_F_MIDI" ; config_enable
-config="CONFIG_USB_F_HID" ; config_enable
-config="CONFIG_USB_F_PRINTER" ; config_enable
-config="CONFIG_USB_F_TCM" ; config_module
-config="CONFIG_USB_CONFIGFS" ; config_enable
-config="CONFIG_USB_CONFIGFS_SERIAL" ; config_enable
-config="CONFIG_USB_CONFIGFS_ACM" ; config_enable
-config="CONFIG_USB_CONFIGFS_OBEX" ; config_enable
-config="CONFIG_USB_CONFIGFS_NCM" ; config_enable
-config="CONFIG_USB_CONFIGFS_ECM" ; config_enable
-config="CONFIG_USB_CONFIGFS_ECM_SUBSET" ; config_enable
-config="CONFIG_USB_CONFIGFS_RNDIS" ; config_enable
-config="CONFIG_USB_CONFIGFS_EEM" ; config_enable
-config="CONFIG_USB_CONFIGFS_PHONET" ; config_enable
-config="CONFIG_USB_CONFIGFS_MASS_STORAGE" ; config_enable
-config="CONFIG_USB_CONFIGFS_F_LB_SS" ; config_enable
-config="CONFIG_USB_CONFIGFS_F_FS" ; config_enable
-config="CONFIG_USB_CONFIGFS_F_UAC1" ; config_enable
-config="CONFIG_USB_CONFIGFS_F_UAC2" ; config_enable
-config="CONFIG_USB_CONFIGFS_F_MIDI" ; config_enable
-config="CONFIG_USB_CONFIGFS_F_HID" ; config_enable
-config="CONFIG_USB_CONFIGFS_F_UVC" ; config_enable
-config="CONFIG_USB_CONFIGFS_F_PRINTER" ; config_enable
+./scripts/config --enable CONFIG_USB_LIBCOMPOSITE
+./scripts/config --enable CONFIG_USB_F_ACM
+./scripts/config --enable CONFIG_USB_F_SS_LB
+./scripts/config --enable CONFIG_USB_U_SERIAL
+./scripts/config --enable CONFIG_USB_U_ETHER
+./scripts/config --enable CONFIG_USB_U_AUDIO
+./scripts/config --enable CONFIG_USB_F_SERIAL
+./scripts/config --enable CONFIG_USB_F_OBEX
+./scripts/config --enable CONFIG_USB_F_NCM
+./scripts/config --enable CONFIG_USB_F_ECM
+./scripts/config --module CONFIG_USB_F_PHONET
+./scripts/config --enable CONFIG_USB_F_EEM
+./scripts/config --enable CONFIG_USB_F_SUBSET
+./scripts/config --enable CONFIG_USB_F_RNDIS
+./scripts/config --enable CONFIG_USB_F_MASS_STORAGE
+./scripts/config --enable CONFIG_USB_F_FS
+./scripts/config --enable CONFIG_USB_F_UAC1
+./scripts/config --enable CONFIG_USB_F_UAC2
+./scripts/config --module CONFIG_USB_F_UVC
+./scripts/config --enable CONFIG_USB_F_MIDI
+./scripts/config --enable CONFIG_USB_F_HID
+./scripts/config --enable CONFIG_USB_F_PRINTER
+./scripts/config --module CONFIG_USB_F_TCM
+./scripts/config --enable CONFIG_USB_CONFIGFS
+./scripts/config --enable CONFIG_USB_CONFIGFS_SERIAL
+./scripts/config --enable CONFIG_USB_CONFIGFS_ACM
+./scripts/config --enable CONFIG_USB_CONFIGFS_OBEX
+./scripts/config --enable CONFIG_USB_CONFIGFS_NCM
+./scripts/config --enable CONFIG_USB_CONFIGFS_ECM
+./scripts/config --enable CONFIG_USB_CONFIGFS_ECM_SUBSET
+./scripts/config --enable CONFIG_USB_CONFIGFS_RNDIS
+./scripts/config --enable CONFIG_USB_CONFIGFS_EEM
+./scripts/config --enable CONFIG_USB_CONFIGFS_PHONET
+./scripts/config --enable CONFIG_USB_CONFIGFS_MASS_STORAGE
+./scripts/config --enable CONFIG_USB_CONFIGFS_F_LB_SS
+./scripts/config --enable CONFIG_USB_CONFIGFS_F_FS
+./scripts/config --enable CONFIG_USB_CONFIGFS_F_UAC1
+./scripts/config --enable CONFIG_USB_CONFIGFS_F_UAC2
+./scripts/config --enable CONFIG_USB_CONFIGFS_F_MIDI
+./scripts/config --enable CONFIG_USB_CONFIGFS_F_HID
+./scripts/config --enable CONFIG_USB_CONFIGFS_F_UVC
+./scripts/config --enable CONFIG_USB_CONFIGFS_F_PRINTER
 
 #2022.03.01 fix W1, needs to be a module now...
-config="CONFIG_W1" ; config_enable
-config="CONFIG_W1_MASTER_GPIO" ; config_module
-config="CONFIG_W1_SLAVE_DS2430" ; config_module
-config="CONFIG_W1_SLAVE_DS2433_CRC" ; config_enable
+./scripts/config --enable CONFIG_W1
+./scripts/config --module CONFIG_W1_MASTER_GPIO
+./scripts/config --module CONFIG_W1_SLAVE_DS2430
+./scripts/config --enable CONFIG_W1_SLAVE_DS2433_CRC
 
 #2022.03.18 Re-Enable UIO PRUSS
-config="CONFIG_UIO_PDRV_GENIRQ" ; config_module
-config="CONFIG_UIO_PRUSS" ; config_module
+./scripts/config --module CONFIG_UIO_PDRV_GENIRQ
+./scripts/config --module CONFIG_UIO_PRUSS
 
 #2022.12.25: still totally broken..
 #[   26.460634] tps65217-charger tps65217-charger: DMA mask not set
 #[   26.581296] genirq: Flags mismatch irq 53. 00002000 (tps65217-charger) vs. 00002000 (vbus)
 #[   26.739119] tps65217-charger tps65217-charger: Unable to register irq 53 err -16
 #[   26.842097] tps65217-charger: probe of tps65217-charger failed with error -16
-config="CONFIG_CHARGER_TPS65217" ; config_disable
+./scripts/config --disable CONFIG_CHARGER_TPS65217
 
 #2023.07.10
-config="CONFIG_GCC_PLUGINS" ; config_disable
+./scripts/config --disable CONFIG_GCC_PLUGINS
 
 #2023.07.14
 # MUSB DMA mode
-config="CONFIG_MUSB_PIO_ONLY" ; config_enable
-config="CONFIG_USB_TI_CPPI41_DMA" ; config_disable
+./scripts/config --enable CONFIG_MUSB_PIO_ONLY
+./scripts/config --disable CONFIG_USB_TI_CPPI41_DMA
 
 # We recommend to turn off Real-Time group scheduling in the
 # kernel when using systemd. RT group scheduling effectively
