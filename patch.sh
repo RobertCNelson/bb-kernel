7fe8b5260d2045914d195cdedd4a43f7bfe04cdd
Robert Nelson <robertcnelson@gmail.com>
2025-10-16 13:49:21 -0500

    kernel v6.17.3-bone10.1 rebase external git projects BBDTBS: https://github.com/beagleboard/BeagleBoard-DeviceTrees/commit/82882860919be2dbb44a88ea72bcb57dcb4552ba WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/wens/wireless-regdb.git/tag/?h=master-2025-10-07
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -308,6 +308,7 @@ beagleboard_dtbs () {
 
 		device="k3-am6232-pocketbeagle2-techlab-cape" ; k3_dtbo_makefile_append
 		device="k3-am62-pocketbeagle2-ardupilot-cape" ; k3_dtbo_makefile_append
+		device="k3-am62-pocketbeagle2-led-all" ; k3_dtbo_makefile_append
 		device="k3-am62-pocketbeagle2-leds-off" ; k3_dtbo_makefile_append
 		device="k3-am62-pocketbeagle2-mspm0swd" ; k3_dtbo_makefile_append
 		device="k3-am62-pocketbeagle2-techlab-cape" ; k3_dtbo_makefile_append
@@ -474,7 +475,7 @@ post_rpibackports () {
 }
 
 backports () {
-	backport_tag="v6.17.1"
+	backport_tag="v6.18-rc1"
 
 	subsystem="tps65219"
 	#regenerate="enable"
@@ -493,7 +494,7 @@ backports () {
 		patch_backports
 	fi
 
-	backport_tag="v6.17.1"
+	backport_tag="v6.18-rc1"
 
 	subsystem="it66121"
 	#regenerate="enable"
--- version.sh
+++ version.sh
@@ -54,11 +54,11 @@ linux_repo="https://kernel.googlesource.com/pub/scm/linux/kernel/git/torvalds/li
 linux_stable_repo="https://kernel.googlesource.com/pub/scm/linux/kernel/git/stable/linux.git"
 #
 KERNEL_REL=6.17
-KERNEL_TAG=${KERNEL_REL}.2
+KERNEL_TAG=${KERNEL_REL}.3
 #https://mirrors.edge.kernel.org/pub/linux/kernel/projects/rt/6.17/
 kernel_rt=".1-rt5"
 #Kernel Build
-BUILD=${build_prefix}10
+BUILD=${build_prefix}10.1
 
 #v6.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
