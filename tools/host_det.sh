490de12b316b8ee1b027f103d689740e8148081f
Robert Nelson <robertcnelson@gmail.com>
2025-10-16 17:34:11 -0500

    kernel v6.6.110-bone-rt-r32.1 rebase external git projects and rt: v6.6.110-rt62 BBDTBS: https://github.com/beagleboard/BeagleBoard-DeviceTrees/commit/254759f61c902e0193c42cc7e63929ede617b92d RT: patch-6.6.110-rt62.patch.xz WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/wens/wireless-regdb.git/tag/?h=master-2025-10-07
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/host_det.sh
+++ tools/host_det.sh
@@ -533,11 +533,12 @@ debian_regs () {
 			warn_eol_distro=1
 			stop_pkg_search=1
 			;;
-		jammy|noble|plucky|questing)
+		jammy|noble|plucky|questing|resolute)
 			#22.04 jammy: (EOL: April 2027) lts: jammy -> noble
-			#24.04 noble: (EOL: June 2029) lts: noble -> xyz
+			#24.04 noble: (EOL: April 2029) lts: noble -> resolute
 			#25.04 plucky: (EOL: January 2026)
 			#25.10 questing: (EOL: July 2026)
+			#26.04 resolute: (EOL: April 2031) lts: resolute -> xyz
 			unset warn_eol_distro
 			;;
 		hardy|lucid|maverick|natty|oneiric|precise|quantal|raring|saucy|trusty|utopic|vivid|wily|xenial|yakkety|zesty|artful|bionic|cosmic|disco|eoan|focal|groovy|hirsute|impish|kinetic|lunar|mantic|oracular)
