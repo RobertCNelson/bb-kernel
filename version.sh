98e6fccf98ff63eea7b0a13f7acfe6d08075628d
Robert Nelson <robertcnelson@gmail.com>
2025-10-19 19:50:13 -0500

    kernel v6.12.49-bone-rt-r37.1 rebase external git projects and rt: v6.12.49-rt13 BBDTBS: https://github.com/beagleboard/BeagleBoard-DeviceTrees/commit/dae1e7cbd3530363ffcae008d613907082f725ea RT: patch-6.12.49-rt13.patch.xz WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/wens/wireless-regdb.git/tag/?h=master-2025-10-07
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -54,11 +54,11 @@ linux_repo="https://kernel.googlesource.com/pub/scm/linux/kernel/git/torvalds/li
 linux_stable_repo="https://kernel.googlesource.com/pub/scm/linux/kernel/git/stable/linux.git"
 #
 KERNEL_REL=6.12
-KERNEL_TAG=${KERNEL_REL}.45
+KERNEL_TAG=${KERNEL_REL}.49
 #https://mirrors.edge.kernel.org/pub/linux/kernel/projects/rt/6.12/
-kernel_rt=".43-rt12"
+kernel_rt=".49-rt13"
 #Kernel Build
-BUILD=${build_prefix}37
+BUILD=${build_prefix}37.1
 
 #v6.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
