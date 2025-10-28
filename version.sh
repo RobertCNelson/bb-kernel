bbe52b84b02be5ef7ac08927d2d3ec67c49a8ad2
Robert Nelson <robertcnelson@gmail.com>
2025-10-28 14:38:41 -0500

    config: SND_SOC_PCM186X
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -58,7 +58,7 @@ KERNEL_TAG=${KERNEL_REL}.5
 #https://mirrors.edge.kernel.org/pub/linux/kernel/projects/rt/6.17/
 kernel_rt=".5-rt7"
 #Kernel Build
-BUILD=${build_prefix}13.1
+BUILD=${build_prefix}13.2
 
 #v6.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
