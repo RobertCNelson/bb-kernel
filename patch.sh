11baaa2440a5185ada8cbbd4ae4eb1ad0e501328
Robert Nelson <robertcnelson@gmail.com>
2025-12-05 13:47:56 -0600

    it66121_drm_connector: switch to bootlins rebase
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -533,7 +533,7 @@ backports () {
 drivers () {
 	dir 'branding/boris'
 
-	dir 'drivers/it66121_drm_connector'
+	dir 'drivers/it66121_drm_connector_v2'
 	dir 'drivers/it66122'
 
 	dir 'external/ti-amx3-cm3-pm-firmware'
