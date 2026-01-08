30eb1621157ae79d8fab4fb12399566aa656d000
Robert Nelson <robertcnelson@gmail.com>
2026-01-08 11:29:55 -0600

    config: RUST
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -59,7 +59,7 @@ KERNEL_TAG=${KERNEL_REL}.4
 #https://mirrors.edge.kernel.org/pub/linux/kernel/projects/rt/6.18/
 kernel_rt="-rc4-rt3"
 #Kernel Build
-BUILD=${build_prefix}13.1
+BUILD=${build_prefix}13.2
 
 #v6.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
