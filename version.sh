75fbab27cbcecfbdb112ae51f1957cb235860e22
Robert Nelson <robertcnelson@gmail.com>
2026-01-19 19:49:21 -0600

    6.12.66-bone44 release BBDTBS: https://github.com/beagleboard/BeagleBoard-DeviceTrees/commit/7cf15d7e062804025927622f8a1e7afde058f65c WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/wens/wireless-regdb.git/tag/?h=master-2025-10-07
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -59,7 +59,7 @@ KERNEL_TAG=${KERNEL_REL}.66
 #https://mirrors.edge.kernel.org/pub/linux/kernel/projects/rt/6.12/
 kernel_rt=".66-rt15"
 #Kernel Build
-BUILD=${build_prefix}43.1
+BUILD=${build_prefix}44
 
 #v6.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
