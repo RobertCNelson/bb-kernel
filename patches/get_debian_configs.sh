586a84621703a38f946b50999643bd4e16a73d5e
Robert Nelson <robertcnelson@gmail.com>
2026-01-23 14:21:09 -0600

    kernel v6.12.67-bone-rt-r44.1 rebase external git projects and rt: v6.12.66-rt15 BBDTBS: https://github.com/beagleboard/BeagleBoard-DeviceTrees/commit/39c11ddc6e73bca4d15deb1a5df882622480f372 RT: patch-6.12.66-rt15.patch.xz WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/wens/wireless-regdb.git/tag/?h=master-2025-10-07
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patches/get_debian_configs.sh
+++ patches/get_debian_configs.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+# SPDX-FileCopyrightText: Robert Nelson <robertcnelson@gmail.com>
+#
+# SPDX-License-Identifier: MIT
+
 #
 #https://packages.debian.org/source/trixie/linux
 #
-abi="6.12.63+deb13"
+abi="6.12"
 kernel="6.12.63-1"
 #
 
@@ -13,30 +17,36 @@ debian_security_site="http://deb.debian.org/debian-security/pool/main/l/linux"
 incoming_site="http://incoming.debian.org/debian-buildd/pool/main/l/linux"
 
 dl_deb () {
-	wget -c --directory-prefix=./dl/ ${mirror_site}/linux-image-${abi}-${image}${unsigned}_${kernel}_${dpkg_arch}.deb
-
-	if [ ! -f ./dl/linux-image-${abi}-${image}${unsigned}_${kernel}_${dpkg_arch}.deb ] ; then
-		wget -c --directory-prefix=./dl/ ${debian_site}/linux-image-${abi}-${image}${unsigned}_${kernel}_${dpkg_arch}.deb
+	if [ ! -f ./dl/linux-config-${abi}_${kernel}_${dpkg_arch}.deb ] ; then
+		wget -cnv --directory-prefix=./dl/ ${mirror_site}/linux-config-${abi}_${kernel}_${dpkg_arch}.deb
 	fi
 
-	if [ ! -f ./dl/linux-image-${abi}-${image}${unsigned}_${kernel}_${dpkg_arch}.deb ] ; then
-		wget -c --directory-prefix=./dl/ ${incoming_site}/linux-image-${abi}-${image}${unsigned}_${kernel}_${dpkg_arch}.deb
+	if [ ! -f ./dl/linux-config-${abi}_${kernel}_${dpkg_arch}.deb ] ; then
+		wget -cnv --directory-prefix=./dl/ ${debian_site}/linux-config-${abi}_${kernel}_${dpkg_arch}.deb
 	fi
 
-	if [ ! -f ./dl/linux-image-${abi}-${image}${unsigned}_${kernel}_${dpkg_arch}.deb ] ; then
-		wget -c --directory-prefix=./dl/ ${debian_security_site}/linux-image-${abi}-${image}${unsigned}_${kernel}_${dpkg_arch}.deb
+	if [ ! -f ./dl/linux-config-${abi}_${kernel}_${dpkg_arch}.deb ] ; then
+		wget -cnv --directory-prefix=./dl/ ${incoming_site}/linux-config-${abi}_${kernel}_${dpkg_arch}.deb
 	fi
 
-	if [ -f ./dl/linux-image-${abi}-${image}${unsigned}_${kernel}_${dpkg_arch}.deb ] ; then
-		dpkg -x ./dl/linux-image-${abi}-${image}${unsigned}_${kernel}_${dpkg_arch}.deb ./dl/tmp/
-		cp -v ./dl/tmp/boot/config-${abi}-${image} ./debian.config
+	if [ ! -f ./dl/linux-config-${abi}_${kernel}_${dpkg_arch}.deb ] ; then
+		wget -cnv --directory-prefix=./dl/ ${debian_security_site}/linux-config-${abi}_${kernel}_${dpkg_arch}.deb
+	fi
+
+	if [ -f ./dl/linux-config-${abi}_${kernel}_${dpkg_arch}.deb ] ; then
+		dpkg -x ./dl/linux-config-${abi}_${kernel}_${dpkg_arch}.deb ./dl/tmp/
+		if [ -f ./dl/tmp/usr/src/linux-config-${abi}/config.${dpkg_arch}_${config}.xz ] ; then
+			xzcat ./dl/tmp/usr/src/linux-config-${abi}/config.${dpkg_arch}_${config}.xz > ./debian.config
+		else
+			tree ./dl/tmp/usr/src/linux-config-${abi}/
+			exit 2
+		fi
 		rm -rf ./dl/tmp/ || true
 	fi
 }
 
 dpkg_arch="armhf"
-image="rt-armmp"
-unsigned=""
+config="rt_armmp"
 dl_deb
 
 rm -rf ./dl/ || true
