ce19edb5963702b38a12b1baddf48f12f5268a51
Robert Nelson <robertcnelson@gmail.com>
2026-01-25 18:53:33 -0600

    kernel v6.19-rc7-bone5.1 rebase external git projects BBDTBS: https://github.com/beagleboard/BeagleBoard-DeviceTrees/commit/55b77ae0711b0e8c958aaf7a5c3b56e4cbfe52f7 WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/wens/wireless-regdb.git/tag/?h=master-2025-10-07
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patches/get_debian_configs.sh
+++ patches/get_debian_configs.sh
@@ -14,7 +14,7 @@
 #kernel="6.18.2-1~exp1"
 #
 abi="6.19"
-kernel="6.19~rc5-1~exp1"
+kernel="6.19~rc6-1~exp1"
 #
 
 mirror_site="http://192.168.1.10/debian/pool/main/l/linux"
@@ -24,35 +24,38 @@ incoming_site="http://incoming.debian.org/debian-buildd/pool/main/l/linux"
 
 dl_deb () {
 	if [ ! -f ./dl/linux-config-${abi}_${kernel}_${dpkg_arch}.deb ] ; then
-		wget -cnv --directory-prefix=./dl/ ${mirror_site}/linux-config-${abi}_${kernel}_${dpkg_arch}.deb
+		wget -cq --directory-prefix=./dl/ ${mirror_site}/linux-config-${abi}_${kernel}_${dpkg_arch}.deb
 	fi
 
 	if [ ! -f ./dl/linux-config-${abi}_${kernel}_${dpkg_arch}.deb ] ; then
-		wget -cnv --directory-prefix=./dl/ ${debian_site}/linux-config-${abi}_${kernel}_${dpkg_arch}.deb
+		wget -cq --directory-prefix=./dl/ ${debian_site}/linux-config-${abi}_${kernel}_${dpkg_arch}.deb
 	fi
 
 	if [ ! -f ./dl/linux-config-${abi}_${kernel}_${dpkg_arch}.deb ] ; then
-		wget -cnv --directory-prefix=./dl/ ${incoming_site}/linux-config-${abi}_${kernel}_${dpkg_arch}.deb
+		wget -cq --directory-prefix=./dl/ ${incoming_site}/linux-config-${abi}_${kernel}_${dpkg_arch}.deb
 	fi
 
 	if [ ! -f ./dl/linux-config-${abi}_${kernel}_${dpkg_arch}.deb ] ; then
-		wget -cnv --directory-prefix=./dl/ ${debian_security_site}/linux-config-${abi}_${kernel}_${dpkg_arch}.deb
+		wget -cq --directory-prefix=./dl/ ${debian_security_site}/linux-config-${abi}_${kernel}_${dpkg_arch}.deb
 	fi
 
 	if [ -f ./dl/linux-config-${abi}_${kernel}_${dpkg_arch}.deb ] ; then
+		echo "[linux-config-${abi}_${kernel}_${dpkg_arch}.deb]"
 		dpkg -x ./dl/linux-config-${abi}_${kernel}_${dpkg_arch}.deb ./dl/tmp/
-		if [ -f ./dl/tmp/usr/src/linux-config-${abi}/config.${dpkg_arch}_none_${config}.xz ] ; then
-			xzcat ./dl/tmp/usr/src/linux-config-${abi}/config.${dpkg_arch}_none_${config}.xz > ./debian.config
+		if [ -f ./dl/tmp/usr/src/linux-config-${abi}/config.${dpkg_arch}_${config}.xz ] ; then
+			xzcat ./dl/tmp/usr/src/linux-config-${abi}/config.${dpkg_arch}_${config}.xz > ./debian.config
 		else
 			tree ./dl/tmp/usr/src/linux-config-${abi}/
 			exit 2
 		fi
 		rm -rf ./dl/tmp/ || true
+	else
+		echo "[linux-config-${abi}_${kernel}_${dpkg_arch}.deb] NOT BUILT YET"
 	fi
 }
 
 dpkg_arch="armhf"
-config="armmp"
+config="none_armmp"
 dl_deb
 
 rm -rf ./dl/ || true
