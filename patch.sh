8a80105b2e4b275e8b03c85e3d5c6bacce603515
Robert Nelson <robertcnelson@gmail.com>
2026-02-27 11:49:36 -0600

    kernel v6.12.74-bone48.1 rebase external git projects BBDTBS: https://github.com/beagleboard/BeagleBoard-DeviceTrees/commit/c72911b090b341c62a30fd899e596ac8a1dd4e2c WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/wens/wireless-regdb.git/tag/?h=master-2026-02-04
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -268,6 +268,7 @@ beagleboard_dtbs () {
 		#exit 2
 
 		device="AM335X-PRU-UIO-00A0" ; arm_dtbo_makefile_append
+		device="AM3359-PWM012" ; arm_dtbo_makefile_append
 		device="BB-ADC-00A0" ; arm_dtbo_makefile_append
 		device="BB-BBBW-WL1835-00A0" ; arm_dtbo_makefile_append
 		device="BB-BBGG-WL1835-00A0" ; arm_dtbo_makefile_append
