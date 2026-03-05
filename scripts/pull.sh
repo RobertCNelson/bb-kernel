c7f905ae671c9a939040f4a41fc219a0b437a4be
Robert Nelson <robertcnelson@gmail.com>
2026-03-05 10:52:10 -0600

    scripts: resync all build scripts
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ scripts/pull.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# SPDX-FileCopyrightText: Robert Nelson <robertcnelson@gmail.com>
+#
+# SPDX-License-Identifier: MIT
+
+git pull --no-edit https://github.com/RobertCNelson/bb-kernel.git am33x-rt-v5.15
+git pull --no-edit https://gitlab.com/RobertCNelson/bb-kernel.git am33x-rt-v5.15
+
