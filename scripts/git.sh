00ac52dea495a944fc06ddbab69f4a8f919fec08
Robert Nelson <robertcnelson@gmail.com>
2026-03-05 10:32:28 -0600

    kernel v6.1.164-bone-rt-r44.1 rebase external git projects and rt: v6.1.164-rt60 BBDTBS: https://github.com/beagleboard/BeagleBoard-DeviceTrees/commit/7aca0c91d8df0882554df7c4706640e16ee7c986 RT: patch-6.1.164-rt60.patch.xz WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/wens/wireless-regdb.git/tag/?h=master-2026-02-04 BCFSERIAL: https://openbeagle.org/beagleconnect/linux/bcfserial/-/commit/db467023bd136c97c2e13c3a8b9e41dbdfafbc66
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- scripts/git.sh
+++ scripts/git.sh
@@ -29,11 +29,19 @@ git_kernel_stable () {
 	fi
 }
 
+git_kernel_org_stable_tag_backup () {
+	#We want to hit git.kernel.org last for least bandwidth hit...
+	backup_stable_repo="https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git"
+	echo "-----------------------------"
+	echo "scripts/git: fetching v${KERNEL_TAG} from: ${backup_stable_repo}"
+	${git_bin} fetch "${backup_stable_repo}" tag v${KERNEL_TAG} --no-tags
+}
+
 git_kernel_stable_tag_backup () {
 	backup_stable_repo="https://kernel.googlesource.com/pub/scm/linux/kernel/git/torvalds/linux.git"
 	echo "-----------------------------"
 	echo "scripts/git: fetching v${KERNEL_TAG} from: ${backup_stable_repo}"
-	${git_bin} fetch "${backup_stable_repo}" tag v${KERNEL_TAG} --no-tags
+	${git_bin} fetch "${backup_stable_repo}" tag v${KERNEL_TAG} --no-tags || git_kernel_org_stable_tag_backup
 }
 
 git_kernel_stable_tag () {
