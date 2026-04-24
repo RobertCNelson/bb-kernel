25c37e133809d29e508d58dd8bca61e7e4580b18
Robert Nelson <robertcnelson@gmail.com>
2026-04-24 15:56:20 -0500

    ci: disable CONFIG_CC33XX
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -547,8 +547,8 @@ drivers () {
 	dir 'drivers/wkup_m3_ipc'
 	dir 'external/ti-amx3-cm3-pm-firmware'
 
-	dir 'drivers/ti-cc33xx-1.0.2.10'
-	dir 'drivers/cc33xx-fixes'
+#	dir 'drivers/ti-cc33xx-1.0.2.10'
+#	dir 'drivers/cc33xx-fixes'
 }
 
 ###
