5437dc540d888797031900ae81fd5eee195b94e1
Robert Nelson <robertcnelson@gmail.com>
2026-04-24 14:29:29 -0500

    WIRELESS_REGDB="2026-03-18"
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- scripts/pull.sh
+++ scripts/pull.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-git pull --no-edit https://github.com/RobertCNelson/bb-kernel.git am33x-v6.3
-git pull --no-edit https://gitlab.com/RobertCNelson/bb-kernel.git am33x-v6.3
+# SPDX-FileCopyrightText: Robert Nelson <robertcnelson@gmail.com>
+#
+# SPDX-License-Identifier: MIT
+
+git pull --no-edit https://github.com/RobertCNelson/bb-kernel.git am33x-v6.2
+git pull --no-edit https://gitlab.com/RobertCNelson/bb-kernel.git am33x-v6.2
 
