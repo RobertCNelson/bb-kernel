add0bb336c049033332d814e1db9403b9dd8e818
Robert Nelson <robertcnelson@gmail.com>
2026-04-25 11:38:13 -0500

    ci: build test CC33XX
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -65,7 +65,7 @@ KERNEL_TAG=${KERNEL_REL}.11
 #https://mirrors.edge.kernel.org/pub/linux/kernel/projects/rt/6.15/
 kernel_rt="-rt2"
 #Kernel Build
-BUILD=${build_prefix}32.1
+BUILD=${build_prefix}32.2
 
 #v6.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
