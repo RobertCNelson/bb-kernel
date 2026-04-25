1c56d75d60550f5e783b160e194c235e08cc2384
Robert Nelson <robertcnelson@gmail.com>
2026-04-25 09:49:25 -0500

    cc33xx: merge 1.0.2.10 sdk
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -590,6 +590,8 @@ drivers () {
 	echo "dir: drivers/cc33xx/v5_20241107"
 	#b4 am https://lore.kernel.org/linux-wireless/20241107125209.1736277-1-michael.nemanov@ti.com/
 	${git} "${DIR}/patches/drivers/cc33xx/v5_20241107/v5_20241107_michael_nemanov_wifi_cc33xx_add_driver_for_new_ti_cc33xx_wireless_device_family.mbx"
+
+	dir 'drivers/cc33xx/1.0.2.10'
 }
 
 ###
