fd60369ee9c580fc30d3950775c453dbb2a473c3
Robert Nelson <robertcnelson@gmail.com>
2026-04-25 12:06:31 -0500

    ci: disable CONFIG_CC33XX
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -65,7 +65,7 @@ KERNEL_TAG=${KERNEL_REL}.12
 #https://mirrors.edge.kernel.org/pub/linux/kernel/projects/rt/6.16/
 kernel_rt="-rt2"
 #Kernel Build
-BUILD=${build_prefix}26.2
+BUILD=${build_prefix}26.3
 
 #v6.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
