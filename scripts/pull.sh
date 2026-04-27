beb2f3035f50719fff9b4685f2a0296c3864b936
Robert Nelson <robertcnelson@gmail.com>
2026-04-26 19:02:39 -0500

    resync: debian 7.0.1-1~exp1
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- scripts/pull.sh
+++ scripts/pull.sh
@@ -4,6 +4,6 @@
 #
 # SPDX-License-Identifier: MIT
 
-git pull --no-edit https://github.com/RobertCNelson/bb-kernel.git am33x-v6.19
-git pull --no-edit https://gitlab.com/RobertCNelson/bb-kernel.git am33x-v6.19
+git pull --no-edit https://github.com/RobertCNelson/bb-kernel.git am33x-v7.0
+git pull --no-edit https://gitlab.com/RobertCNelson/bb-kernel.git am33x-v7.0
 
