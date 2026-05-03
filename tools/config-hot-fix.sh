aa394e18da393b38a59e7d54d3a66b1ea9112e86
Robert Nelson <robertcnelson@gmail.com>
2026-05-03 18:17:26 -0500

    kernel v7.1-rc2-bone1.1 rebase external git projects BBDTBS: https://github.com/beagleboard/BeagleBoard-DeviceTrees/commit/ab42071f865ba1a788271be0b7d0288b0da43b79 WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/wens/wireless-regdb.git/tag/?h=master-2026-03-18
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/config-hot-fix.sh
+++ tools/config-hot-fix.sh
@@ -15,11 +15,13 @@ if [ -f ${DIR}/KERNEL/Makefile ] ; then
 
 	cp -v "${DIR}/patches/debian.config" .config
 	cp -v "${DIR}/patches/beagle.config" beagle.config
+	cp -v "${DIR}/patches/configs_removed_on_mainline.config" configs_removed_on_mainline.config
 	make ARCH=${KERNEL_ARCH} CROSS_COMPILE="${CC}" olddefconfig
-	ARCH=${KERNEL_ARCH} ./scripts/kconfig/merge_config.sh -m -r .config beagle.config
+	ARCH=${KERNEL_ARCH} ./scripts/kconfig/merge_config.sh -m -r .config beagle.config configs_removed_on_mainline.config
 	make ARCH=${KERNEL_ARCH} CROSS_COMPILE="${CC}" olddefconfig
 	cp -v .config "${DIR}/patches/defconfig"
 	rm beagle.config || true
+	rm configs_removed_on_mainline.config || true
 
 	cd ${DIR}/
 fi
