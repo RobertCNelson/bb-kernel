1fd1c32d5f805c7be1738dcd87f2e629352b995b
Robert Nelson <robertcnelson@gmail.com>
2026-05-05 12:39:16 -0500

    kernel v6.6.137-bone-rt-r42.2 rebase external git projects and rt: v6.6.135-rt74 BBDTBS: https://github.com/beagleboard/BeagleBoard-DeviceTrees/commit/959a79d903152e6b92088d05ff9906935a227bb3 RT: patch-6.6.135-rt74.patch.xz WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/wens/wireless-regdb.git/tag/?h=master-2026-03-18
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -113,6 +113,7 @@ rt () {
 			xzcat patch-${rt_patch}.patch.xz | patch -p1 || rt_cleanup
 			rm -f patch-${rt_patch}.patch.xz
 			rm -f localversion-rt
+			rm -f net/core/skbuff.c.orig
 			${git_bin} add .
 			${git_bin} commit -a -m 'merge: CONFIG_PREEMPT_RT Patch Set' -m "patch-${rt_patch}.patch.xz" -s
 			${git_bin} format-patch -1 -o ../patches/external/rt/
--- tools/config-hot-fix.sh
+++ tools/config-hot-fix.sh
@@ -408,4 +408,22 @@ cd ${DIR}/KERNEL/
 
 ./scripts/config --module CONFIG_MWIFIEX_SDIO
 
+#v7.1-rc1
+
+./scripts/config --disable CONFIG_ATM_CLIP
+./scripts/config --disable CONFIG_ATM_LANE
+
+./scripts/config --disable CONFIG_HAMRADIO
+
+./scripts/config --disable CONFIG_CAIF
+
+./scripts/config --disable CONFIG_ATM_DUMMY
+./scripts/config --disable CONFIG_ATM_NICSTAR
+./scripts/config --disable CONFIG_ATM_IA
+./scripts/config --disable CONFIG_ATM_FORE200E
+
+./scripts/config --disable CONFIG_NET_VENDOR_ALTEON
+
+./scripts/config --disable CONFIG_ISDN
+
 cd ${DIR}/
