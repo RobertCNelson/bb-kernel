48680e6d65288bb8104d1d9560678bbfbba9f124
Robert Nelson <robertcnelson@gmail.com>
2026-05-05 13:46:13 -0500

    kernel v6.1.170-bone47.6 rebase external git projects BBDTBS: https://github.com/beagleboard/BeagleBoard-DeviceTrees/commit/a3228a4f4633c0c7da79c17ccaba08e1c268516f WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/wens/wireless-regdb.git/tag/?h=master-2026-03-18 BCFSERIAL: https://openbeagle.org/beagleconnect/linux/bcfserial/-/commit/db467023bd136c97c2e13c3a8b9e41dbdfafbc66
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/config-hot-fix.sh
+++ tools/config-hot-fix.sh
@@ -411,11 +411,31 @@ cd ${DIR}/KERNEL/
 ./scripts/config --module CONFIG_LEDS_CLASS_FLASH
 ./scripts/config --module CONFIG_GREYBUS_LIGHT
 
+./scripts/config --module CONFIG_MWIFIEX_SDIO
+
 #CC33XX
 ./scripts/config --module CONFIG_CC33XX
 ./scripts/config --disable CONFIG_CC33XX_SPI
 ./scripts/config --module CONFIG_CC33XX_SDIO
 
 ./scripts/config --module CONFIG_BT_TI_UART
+
+#v7.1-rc1
+
+./scripts/config --disable CONFIG_ATM_CLIP
+./scripts/config --disable CONFIG_ATM_LANE
+
+./scripts/config --disable CONFIG_HAMRADIO
+
+./scripts/config --disable CONFIG_CAIF
+
+./scripts/config --disable CONFIG_ATM_DUMMY
+./scripts/config --disable CONFIG_ATM_NICSTAR
+./scripts/config --disable CONFIG_ATM_IA
+./scripts/config --disable CONFIG_ATM_FORE200E
+
+./scripts/config --disable CONFIG_NET_VENDOR_ALTEON
+
+./scripts/config --disable CONFIG_ISDN
 
 cd ${DIR}/
