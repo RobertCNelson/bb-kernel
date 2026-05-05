65155575df079d7b6359b7b5639ec6831609e841
Robert Nelson <robertcnelson@gmail.com>
2026-05-05 15:00:51 -0500

    5.15.204-bone-rt-r49 release BBDTBS: https://github.com/beagleboard/BeagleBoard-DeviceTrees/commit/55fefa1431fbdeaa7643b328f56f04fb779e067c RT: patch-5.15.202-rt94.patch.xz WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/wens/wireless-regdb.git/tag/?h=master-2026-03-18
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -65,7 +65,7 @@ KERNEL_TAG=${KERNEL_REL}.204
 #https://mirrors.edge.kernel.org/pub/linux/kernel/projects/rt/5.15/
 kernel_rt=".202-rt94"
 #Kernel Build
-BUILD=${build_prefix}48.2
+BUILD=${build_prefix}49
 
 #v6.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
