4418056ff371b8958f3952fd009acc1b85728ed7
Robert Nelson <robertcnelson@gmail.com>
2026-05-09 13:20:44 -0500

    scripts: resync all build scripts
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -65,7 +65,7 @@ KERNEL_TAG=${KERNEL_REL}.25
 #https://mirrors.edge.kernel.org/pub/linux/kernel/projects/rt/x.y/
 kernel_rt=".X-rtY"
 #Kernel Build
-BUILD=${build_prefix}30
+BUILD=${build_prefix}30.1
 
 #v6.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
