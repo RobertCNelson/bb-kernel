d6257482f970f496d6b475105f0ed35ef6d3b2e6
Robert Nelson <robertcnelson@gmail.com>
2026-05-12 11:38:18 -0500

    config: retest CONFIG_VLAN_8021Q
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -65,7 +65,7 @@ KERNEL_TAG=${KERNEL_REL}.29
 #https://mirrors.edge.kernel.org/pub/linux/kernel/projects/rt/x.y/
 kernel_rt=".X-rtY"
 #Kernel Build
-BUILD=${build_prefix}32.1
+BUILD=${build_prefix}32.2
 
 #v6.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
