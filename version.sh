863044913a7dd86ee8e8f6af6593b8312f8a4cc4
Robert Nelson <robertcnelson@gmail.com>
2026-05-12 15:12:08 -0500

    config: disable rust
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -65,11 +65,7 @@ KERNEL_TAG=${KERNEL_REL}-rc3
 #https://mirrors.edge.kernel.org/pub/linux/kernel/projects/rt/x.y/
 kernel_rt=".X-rtY"
 #Kernel Build
-BUILD=${build_prefix}3
-
-#v6.X-rcX + upto SHA
-#prev_KERNEL_SHA=""
-#KERNEL_SHA=""
+BUILD=${build_prefix}3.1
 
 #git branch
 BRANCH="${branch_prefix}${KERNEL_REL}${branch_postfix}"
