a47325c40ce676f680f093bf2d0557b790edf13e
Robert Nelson <robertcnelson@gmail.com>
2026-05-14 09:40:48 -0500

    scripts: resync all build scripts
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -29,6 +29,7 @@ DEBARCH=armhf
 #toolchain="gcc_13_arm"
 toolchain="gcc_14_arm"
 #toolchain="gcc_15_arm"
+#toolchain="gcc_16_arm"
 #arm64
 #KERNEL_ARCH=arm64
 #DEBARCH=arm64
@@ -40,6 +41,7 @@ toolchain="gcc_14_arm"
 #toolchain="gcc_13_aarch64"
 #toolchain="gcc_14_aarch64"
 #toolchain="gcc_15_aarch64"
+#toolchain="gcc_16_aarch64"
 #riscv64
 #KERNEL_ARCH=riscv
 #DEBARCH=riscv64
@@ -51,6 +53,7 @@ toolchain="gcc_14_arm"
 #toolchain="gcc_13_riscv64"
 #toolchain="gcc_14_riscv64"
 #toolchain="gcc_15_riscv64"
+#toolchain="gcc_16_riscv64"
 
 #Wireless:
 #https://git.kernel.org/pub/scm/linux/kernel/git/wens/wireless-regdb.git
@@ -65,11 +68,7 @@ KERNEL_TAG=${KERNEL_REL}.87
 #https://mirrors.edge.kernel.org/pub/linux/kernel/projects/rt/6.12/
 kernel_rt=".79-rt17"
 #Kernel Build
-BUILD=${build_prefix}56
-
-#v6.X-rcX + upto SHA
-#prev_KERNEL_SHA=""
-#KERNEL_SHA=""
+BUILD=${build_prefix}56.1
 
 #git branch
 BRANCH="${branch_prefix}${KERNEL_REL}${branch_postfix}"
