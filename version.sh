e61b14c53092d6ad9703757d726a462ca841d586
Robert Nelson <robertcnelson@gmail.com>
2026-05-16 12:39:30 -0500

    config: cleanup
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -68,7 +68,7 @@ KERNEL_TAG=${KERNEL_REL}.89
 #https://mirrors.edge.kernel.org/pub/linux/kernel/projects/rt/6.12/
 kernel_rt=".79-rt17"
 #Kernel Build
-BUILD=${build_prefix}58
+BUILD=${build_prefix}58.1
 
 #git branch
 BRANCH="${branch_prefix}${KERNEL_REL}${branch_postfix}"
