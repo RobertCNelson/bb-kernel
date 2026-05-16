ae8680ab20887395dceb77d365b5c3046d834a38
Robert Nelson <robertcnelson@gmail.com>
2026-05-15 21:37:23 -0500

    6.6.139-bone-rt-r45 release BBDTBS: https://github.com/beagleboard/BeagleBoard-DeviceTrees/commit/96d562b2df971eeb37505065e386ded9ef6d1d43 RT: patch-6.6.135-rt74.patch.xz WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/wens/wireless-regdb.git/tag/?h=master-2026-03-18
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -68,7 +68,7 @@ KERNEL_TAG=${KERNEL_REL}.139
 #https://mirrors.edge.kernel.org/pub/linux/kernel/projects/rt/6.6/
 kernel_rt=".135-rt74"
 #Kernel Build
-BUILD=${build_prefix}44.1
+BUILD=${build_prefix}45
 
 #git branch
 BRANCH="${branch_prefix}${KERNEL_REL}${branch_postfix}"
