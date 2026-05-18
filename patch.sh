d6841a6927ae3421730ee0302360b3bd9dc8c99a
Robert Nelson <robertcnelson@gmail.com>
2026-05-18 13:12:25 -0500

    kernel v6.6.140-bone-rt-r45.1 rebase external git projects and rt: v6.6.135-rt74 BBDTBS: https://github.com/beagleboard/BeagleBoard-DeviceTrees/commit/96d562b2df971eeb37505065e386ded9ef6d1d43 RT: patch-6.6.135-rt74.patch.xz WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/wens/wireless-regdb.git/tag/?h=master-2026-03-18
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -115,6 +115,7 @@ rt () {
 			rm -f localversion-rt
 			rm -f net/core/skbuff.c.orig
 			rm -f include/linux/sched.h.orig
+			rm -f kernel/locking/rtmutex.c.orig
 			${git_bin} add .
 			${git_bin} commit -a -m 'merge: CONFIG_PREEMPT_RT Patch Set' -m "patch-${rt_patch}.patch.xz" -s
 			${git_bin} format-patch -1 -o ../patches/external/rt/
--- version.sh
+++ version.sh
@@ -64,11 +64,11 @@ linux_repo="https://kernel.googlesource.com/pub/scm/linux/kernel/git/torvalds/li
 linux_stable_repo="https://kernel.googlesource.com/pub/scm/linux/kernel/git/stable/linux.git"
 #
 KERNEL_REL=6.6
-KERNEL_TAG=${KERNEL_REL}.139
+KERNEL_TAG=${KERNEL_REL}.140
 #https://mirrors.edge.kernel.org/pub/linux/kernel/projects/rt/6.6/
 kernel_rt=".135-rt74"
 #Kernel Build
-BUILD=${build_prefix}45
+BUILD=${build_prefix}45.1
 
 #git branch
 BRANCH="${branch_prefix}${KERNEL_REL}${branch_postfix}"
