1c7a6c6c7f3cc4aac5a2935cb7273c9dcb24c13f
Robert Nelson <robertcnelson@gmail.com>
2026-05-18 12:27:52 -0500

    kernel v6.12.90-bone58.2 rebase external git projects BBDTBS: https://github.com/beagleboard/BeagleBoard-DeviceTrees/commit/1a07d4f1f86be62cc73dd9fe712c8f1f76c3828d WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/wens/wireless-regdb.git/tag/?h=master-2026-03-18
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -112,9 +112,6 @@ rt () {
 			xzcat patch-${rt_patch}.patch.xz | patch -p1 || rt_cleanup
 			rm -f patch-${rt_patch}.patch.xz
 			rm -f localversion-rt
-			rm -f kernel/sched/fair.c.orig
-			rm -f include/linux/sched.h.orig
-			rm -f kernel/locking/rtmutex.c.orig
 			${git_bin} add .
 			${git_bin} commit -a -m 'merge: CONFIG_PREEMPT_RT Patch Set' -m "patch-${rt_patch}.patch.xz" -s
 			${git_bin} format-patch -1 -o ../patches/external/rt/
