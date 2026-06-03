79fadfc6d8d262bf4139f3ca0faaea3fe2d3cbf0
Robert Nelson <robertcnelson@gmail.com>
2026-06-03 10:51:58 -0500

    kernel v6.12.92-bone60.1 rebase external git projects BBDTBS: https://github.com/beagleboard/BeagleBoard-DeviceTrees/commit/7596cc893befd208787e7edc689433af7eff2c84 WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/wens/wireless-regdb.git/tag/?h=master-2026-05-30
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/get_debian_configs.sh
+++ tools/get_debian_configs.sh
@@ -8,20 +8,20 @@
 #https://packages.debian.org/source/sid/linux
 #
 bookworm_kernel_branch="6.1"
-bookworm_kernel_tag="6.1.172-1"
+bookworm_kernel_tag="6.1.174-1"
 #
 trixie_kernel_branch="6.12"
-trixie_kernel_tag="6.12.90-1"
+trixie_kernel_tag="6.12.90-2"
 #
 forky_kernel_branch="7.0"
-forky_kernel_tag="7.0.7-1"
+forky_kernel_tag="7.0.10-1"
 #
 sid_kernel_branch="7.0"
-sid_kernel_tag="7.0.9-1"
+sid_kernel_tag="7.0.10-1"
 #
 exp_kernel_branch="7.1"
 #exp_kernel_tag="7.0.1-1~exp1"
-exp_kernel_tag="7.1~rc4-1~exp2"
+exp_kernel_tag="7.1~rc5-1~exp1"
 #
 
 mirror_site="http://192.168.1.10/debian/pool/main/l/linux"
--- version.sh
+++ version.sh
@@ -57,18 +57,18 @@ toolchain="gcc_14_arm"
 
 #Wireless:
 #https://git.kernel.org/pub/scm/linux/kernel/git/wens/wireless-regdb.git
-WIRELESS_REGDB="2026-03-18"
+WIRELESS_REGDB="2026-05-30"
 
 #Kernel
 linux_repo="https://kernel.googlesource.com/pub/scm/linux/kernel/git/torvalds/linux.git"
 linux_stable_repo="https://kernel.googlesource.com/pub/scm/linux/kernel/git/stable/linux.git"
 #
 KERNEL_REL=6.12
-KERNEL_TAG=${KERNEL_REL}.91
+KERNEL_TAG=${KERNEL_REL}.92
 #https://mirrors.edge.kernel.org/pub/linux/kernel/projects/rt/6.12/
 kernel_rt=".89-rt18"
 #Kernel Build
-BUILD=${build_prefix}60
+BUILD=${build_prefix}60.1
 
 #git branch
 BRANCH="${branch_prefix}${KERNEL_REL}${branch_postfix}"
