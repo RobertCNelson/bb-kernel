b81f9222066d85c9cffdc2dd8f30303cbdbc4ef3
Robert Nelson <robertcnelson@gmail.com>
2026-07-29 12:28:19 -0500

    kernel v6.6.146-bone49.1 rebase external git projects BBDTBS: https://github.com/beagleboard/BeagleBoard-DeviceTrees/commit/a095094762d019c87721802de1eea96d3c0c9a72 WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/wens/wireless-regdb.git/tag/?h=master-2026-05-30
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/get_debian_configs.sh
+++ tools/get_debian_configs.sh
@@ -14,11 +14,11 @@ forky_kernel_branch="7.1"
 forky_kernel_tag="7.1.3-1"
 #
 sid_kernel_branch="7.1"
-sid_kernel_tag="7.1.4-1"
+sid_kernel_tag="7.1.5-1"
 #
 exp_kernel_branch="7.2"
 #exp_kernel_tag="7.1.2-1~exp1"
-exp_kernel_tag="7.2~rc3-1~exp1"
+exp_kernel_tag="7.2~rc5-1~exp1"
 #
 
 mirror_site="http://192.168.1.10/debian/pool/main/l/linux"
