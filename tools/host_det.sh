fbfb140a9de9060ea626c4f73dbdd0bd28d554e4
Robert Nelson <robertcnelson@gmail.com>
2026-08-19 14:56:35 -0500

    ci: tools/host_det.sh drop broken xz check
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/host_det.sh
+++ tools/host_det.sh
@@ -681,27 +681,6 @@ fi
 
 ARCH=$(uname -m)
 
-echo "-----------------------------"
-unset NEEDS_COMMAND
-check_for_command () {
-	if ! which "$1" >/dev/null 2>&1 ; then
-		echo "You're missing command $1"
-		NEEDS_COMMAND=1
-	else
-		version=$(LC_ALL=C $1 $2 | head -n 1)
-		echo "$1: $version"
-	fi
-}
-
-unset NEEDS_COMMAND
-check_for_command xz --version
-
-if [ "${NEEDS_COMMAND}" ] ; then
-	echo "Please install missing commands"
-	echo "-----------------------------"
-	exit 2
-fi
-
 case "$BUILD_HOST" in
     redhat*)
 	    redhat_reqs || error "Failed dependency check"
