4ac80ea7a2027408e12c22b3a7b08f0ccc87d24d
Robert Nelson <robertcnelson@gmail.com>
2026-08-20 18:28:34 -0500

    7.2-bone10 release BBDTBS: https://github.com/beagleboard/BeagleBoard-DeviceTrees/commit/a1462a96b673e705b22beb7a1102d8fa11599d19 WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/wens/wireless-regdb.git/tag/?h=master-2026-05-30
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -68,7 +68,7 @@ KERNEL_TAG=${KERNEL_REL}
 #https://mirrors.edge.kernel.org/pub/linux/kernel/projects/rt/x.y/
 kernel_rt=".X-rtY"
 #Kernel Build
-BUILD=${build_prefix}9.4
+BUILD=${build_prefix}10
 
 #git branch
 BRANCH="${branch_prefix}${KERNEL_REL}${branch_postfix}"
