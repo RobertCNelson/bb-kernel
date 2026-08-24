d35363593d05dbc0c5431865712d3d5dbf09d5a8
Robert Nelson <robertcnelson@gmail.com>
2026-08-24 08:51:42 -0500

    scripts: resync all build scripts
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/ci-dt-schema.sh
+++ tools/ci-dt-schema.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 pip3 install --break-system-packages --root-user-action=ignore git+https://forgejo.gfnd.rcn-ee.org:3000/mirror/dt-schema.git@main
 dt-validate --version
