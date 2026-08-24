d3beede0123e3358357ce053d32852ae78945ff9
Robert Nelson <robertcnelson@gmail.com>
2026-08-24 11:46:36 -0500

    scripts: resync all build scripts
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/ci-dt-schema.sh
+++ tools/ci-dt-schema.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -x
 
-pip3 install --break-system-packages --root-user-action=ignore git+https://forgejo.gfnd.rcn-ee.org:3000/mirror/dt-schema.git@main
-dt-validate --version
-sed -i -e 's:#DTBS_CHECK=1:DTBS_CHECK=1:g' system.sh.gitlab
+pipx install git+https://forgejo.gfnd.rcn-ee.org:3000/mirror/dt-schema.git@main
+
+if dt-validate --version; then
+	sed -i -e 's:#DTBS_CHECK=1:DTBS_CHECK=1:g' system.sh.gitlab
+else
+	echo "Validation FAILED. Skipping system.sh.gitlab update."
+fi
