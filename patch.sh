3fb1fe84204985184bcc7bca0bdf4b325229fd3c
Robert Nelson <robertcnelson@gmail.com>
2026-09-08 11:32:30 -0500

    kernel v7.2.4-bone12.1 rebase external git projects BBDTBS: https://github.com/beagleboard/BeagleBoard-DeviceTrees/commit/f7e9c19835eef48325d47c6c9cd1c835fdcfde14 WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/wens/wireless-regdb.git/tag/?h=master-2026-09-03
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -296,6 +296,7 @@ beagleboard_dtbs () {
 		device="BB-HDMI-IT66122-00A0" ; arm_dtbo_makefile_append
 		device="BB-HDMI-TDA998x-00A0" ; arm_dtbo_makefile_append
 		device="BB-I2C1-00A0" ; arm_dtbo_makefile_append
+		device="BB-I2C1-ADS1015-00A0" ; arm_dtbo_makefile_append
 		device="BB-I2C1-FAST-00A0" ; arm_dtbo_makefile_append
 		device="BB-I2C1-MCP7940X-00A0" ; arm_dtbo_makefile_append
 		device="BB-I2C1-RTC-DS3231" ; arm_dtbo_makefile_append
@@ -514,7 +515,7 @@ backports () {
 
 	dir 'drivers/ti/uio_revert'
 
-	backport_tag="v6.6.154"
+	backport_tag="v6.6.156"
 
 	subsystem="uio"
 	#regenerate="enable"
--- scripts/pull.sh
+++ scripts/pull.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # SPDX-FileCopyrightText: Robert Nelson <robertcnelson@gmail.com>
-#
 # SPDX-License-Identifier: MIT
 
-git pull --no-edit https://github.com/RobertCNelson/bb-kernel.git am33x-v7.2
-git pull --no-edit https://gitlab.com/RobertCNelson/bb-kernel.git am33x-v7.2
+BRANCH="am33x-v7.2"
+REPO="bb-kernel"
 
+git pull --no-edit https://github.com/RobertCNelson/${REPO}.git ${BRANCH}
+git pull --no-edit https://gitlab.com/RobertCNelson/${REPO}.git ${BRANCH}
