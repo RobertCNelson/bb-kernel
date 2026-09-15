aa81bdc25afb4a32c737b0c812d8e5e49e5d9c50
Robert Nelson <robertcnelson@gmail.com>
2026-09-15 08:42:42 -0500

    kernel v6.6.156-bone52.1 rebase external git projects BBDTBS: https://github.com/beagleboard/BeagleBoard-DeviceTrees/commit/673295321130592541fb7b5f27b8776afd04e3df WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/wens/wireless-regdb.git/tag/?h=master-2026-09-03
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 # SPDX-FileCopyrightText: Robert Nelson <robertcnelson@gmail.com>
-#
 # SPDX-License-Identifier: MIT
 
 #
@@ -57,18 +56,18 @@ toolchain="gcc_13_arm"
 
 #Wireless:
 #https://git.kernel.org/pub/scm/linux/kernel/git/wens/wireless-regdb.git
-WIRELESS_REGDB="2026-05-30"
+WIRELESS_REGDB="2026-09-03"
 
 #Kernel
 linux_repo="https://kernel.googlesource.com/pub/scm/linux/kernel/git/torvalds/linux.git"
 linux_stable_repo="https://kernel.googlesource.com/pub/scm/linux/kernel/git/stable/linux.git"
 #
 KERNEL_REL=6.6
-KERNEL_TAG=${KERNEL_REL}.151
+KERNEL_TAG=${KERNEL_REL}.156
 #https://mirrors.edge.kernel.org/pub/linux/kernel/projects/rt/6.6/
-kernel_rt=".151-rt78"
+kernel_rt=".156-rt79"
 #Kernel Build
-BUILD=${build_prefix}52
+BUILD=${build_prefix}52.1
 
 #git branch
 BRANCH="${branch_prefix}${KERNEL_REL}${branch_postfix}"
