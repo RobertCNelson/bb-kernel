43dadbc032075a82409fcce4553111af0dfbdb21
Robert Nelson <robertcnelson@gmail.com>
2026-09-15 11:42:02 -0500

    kernel v5.15.216-bone-rt-r51.1 rebase external git projects and rt: v5.15.216-rt98 BBDTBS: https://github.com/beagleboard/BeagleBoard-DeviceTrees/commit/79560609c9992bfa84149fe9032af57ce052e373 RT: patch-5.15.216-rt98.patch.xz WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/wens/wireless-regdb.git/tag/?h=master-2026-09-03 BCFSERIAL: https://git.beagleboard.org/beagleconnect/linux/bcfserial/-/commit/db467023bd136c97c2e13c3a8b9e41dbdfafbc66
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 # SPDX-FileCopyrightText: Robert Nelson <robertcnelson@gmail.com>
-#
 # SPDX-License-Identifier: MIT
 
 #
@@ -57,18 +56,18 @@ toolchain="gcc_11_arm"
 
 #Wireless:
 #https://git.kernel.org/pub/scm/linux/kernel/git/wens/wireless-regdb.git
-WIRELESS_REGDB="2026-03-18"
+WIRELESS_REGDB="2026-09-03"
 
 #Kernel
 linux_repo="https://kernel.googlesource.com/pub/scm/linux/kernel/git/torvalds/linux.git"
 linux_stable_repo="https://kernel.googlesource.com/pub/scm/linux/kernel/git/stable/linux.git"
 #
 KERNEL_REL=5.15
-KERNEL_TAG=${KERNEL_REL}.207
+KERNEL_TAG=${KERNEL_REL}.216
 #https://mirrors.edge.kernel.org/pub/linux/kernel/projects/rt/5.15/
-kernel_rt=".206-rt95"
+kernel_rt=".216-rt98"
 #Kernel Build
-BUILD=${build_prefix}51
+BUILD=${build_prefix}51.1
 
 #git branch
 BRANCH="${branch_prefix}${KERNEL_REL}${branch_postfix}"
