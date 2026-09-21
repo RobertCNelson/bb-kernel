5af255d02546537328ecfad5ed854a946793126c
Robert Nelson <robertcnelson@gmail.com>
2026-09-21 14:11:04 -0500

    kernel v7.3-rc4-bone3.1 rebase external git projects BBDTBS: https://github.com/beagleboard/BeagleBoard-DeviceTrees/commit/93fdb0092fa5effe49a7f3b09f9236ff836e09ff WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/wens/wireless-regdb.git/tag/?h=master-2026-09-03
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/get_debian_configs.sh
+++ tools/get_debian_configs.sh
@@ -10,8 +10,8 @@
 forky_kernel_branch="7.1"
 forky_kernel_tag="7.1.13-1"
 #
-sid_kernel_branch="7.1"
-sid_kernel_tag="7.1.13-1"
+sid_kernel_branch="7.2"
+sid_kernel_tag="7.2.6-1"
 #
 exp_kernel_branch="7.2"
 exp_kernel_tag="7.2.3-1~exp1"
@@ -61,11 +61,11 @@ dl_distro () {
 	dl_deb
 }
 
-#kernel_branch="${sid_kernel_branch}"
-#kernel_tag="${sid_kernel_tag}"
+kernel_branch="${sid_kernel_branch}"
+kernel_tag="${sid_kernel_tag}"
 
-kernel_branch="${exp_kernel_branch}"
-kernel_tag="${exp_kernel_tag}"
+#kernel_branch="${exp_kernel_branch}"
+#kernel_tag="${exp_kernel_tag}"
 dl_distro
 
 rm -rf ./dl/ || true
